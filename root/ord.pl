20db31373a0563a28324c410568f6ae4a62bb977
ericosur <ericosur@gmail.com>
2012-04-07 21:55:48 +0800

    add pod description to ord.pl
    Former-commit-id: 8a1ab478e776b5888a5ac893e20df0d4492bef87

--- root/ord.pl
+++ root/ord.pl
@@ -1,5 +1,13 @@
 #!/usr/bin/perl
-# just want to know the 0x0d 0x0a sequence
-# 2006/12/27 by ericosur
+=pod
+
+=head1 DESCRIPTION
+
+just want to know the 0x0d 0x0a sequence
+2006/12/27 by ericosur
+
+=cut
 
 print ord("\r\n");
+print "\n";
+
