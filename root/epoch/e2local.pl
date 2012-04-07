1b99400406afa12c21c8da5edbf676cf5f9a5aef
ericosur <ericosur@gmail.com>
2012-04-07 18:16:21 +0800

    move epoch related script to its own folder and revised
    Former-commit-id: 531517fb1a8935f833736cc620a7f45a731aa81a

--- root/epoch/e2local.pl
+++ root/epoch/e2local.pl
@@ -5,12 +5,13 @@
 
 use strict;
 use Time::Local;
-use 5.010;
+#use 5.010;
 
-my $epoch = 1305617116;
+#my $epoch = 1305617116;
+my $epoch = `date +%s`;
 
 my ($ss,$mm,$hh,$mdd,$mmm,$year,$wday,$yday,$isdst) = localtime($epoch);
 
-printf("epoch: %d\n", $epoch);
+printf("current epoch: %d\n", $epoch);
 printf("%04d-%d-%02d\t",($year+1900),($mmm+1),$mdd);
 printf("%02d:%02d:%02d\n", $hh, $mm, $ss);
--- root/epoch/local.pl
+++ root/epoch/local.pl
@@ -5,15 +5,17 @@
 use strict;
 use Time::Local;
 
+# get time from localtime
 my ($sec,$min,$hours,$mday,$month,$year,$wday,$yday,$isdst) = localtime;
-
+# get time using timelocal
 my $time = timelocal($sec,$min,$hours,$mday,$month,$year+1900);
 
-print "timelocal = ", $time, "\n";
-print "time = ", time(), "\n";	# same as ''date +%s''
 #my ($year, $month, $mday, $hours, $min, $sec) = (2009, 4, 15, 16, 0, 0);
 foreach my $ii (localtime)  {
 	print $ii,"\t";
 }
 print "\n";
+print "timelocal = ", $time, "\n";
+print "time = ", time(), "\n";	# same as ''date +%s''
+print 'date +%s: ' . `date +%s` . "\n";
 
