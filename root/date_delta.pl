ac6bf78b54cc1a0202454ab341a641257a721588
ericosur <ericosur@gmail.com>
2012-04-07 17:34:25 +0800

    revised date_delta.pl for more friendly
    Former-commit-id: 917617ed74fdfb9fe70ef41670121a654cde6e37

--- root/date_delta.pl
+++ root/date_delta.pl
@@ -3,6 +3,24 @@ use 5.010;
 
 use Date::Calc qw(Delta_Days);
 
-my $days = Delta_Days(2011,1,1,2011,6,17);
+#
+# get the current date/time to compose filename
+#
+sub get_date
+{
+        my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime;
+        my $date = sprintf "%04d-%02d-%02d", $year+1900, $mon + 1, $mday;
+        return ($year+1900, $mon+1, $mday);
+}
 
-say $days;
+sub main()
+{
+	my ($yy, $mm, $dd) = get_date();
+	my ($syy,$smm,$sdd) = (2012,2,10);
+#	printf("%s-%s-%s\n", $yy,$mm,$dd);
+	my $days = Delta_Days(2012,2,10,$yy,$mm,$dd);
+	printf("date delta = %d since %d-%d-%d\n", $days, $syy, $smm, $sdd);
+}
+
+main;
+
