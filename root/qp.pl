9d0aded1b42baa76c59eba5440ee4f85ae8463d2
ericosur <ericosur@gmail.com>
2012-04-18 23:16:28 +0800

    review qp.pl and add main()
    Former-commit-id: d316aa12f61fbc1ce73ee6b4c53cce9ebc3c6775

--- root/qp.pl
+++ root/qp.pl
@@ -10,23 +10,28 @@
 use strict;
 use MIME::QuotedPrint;
 
-my $infile;
-my $outfile;
-
-($infile, $outfile) = @ARGV;
-die "$0 <infile> <outfile>\n" unless ($infile && $outfile);
-
-printf "infile = [%s], outfile = [%s]\n", $infile, $outfile;
-
-open my $inf, "< $infile" or die "open error\n";
-open my $otf, "> $outfile" or die "write error\n";
-
-while (<$inf>)
+sub main()
 {
-	my $tmp = encode_qp($_);
-	print $otf "$tmp";
+	my $infile;
+	my $outfile;
+
+	($infile, $outfile) = @ARGV;
+	die "$0 <infile> <outfile>\n" unless ($infile && $outfile);
+
+	printf "infile = [%s], outfile = [%s]\n", $infile, $outfile;
+
+	open my $inf, "< $infile" or die "open error\n";
+	open my $otf, "> $outfile" or die "write error\n";
+
+	while (<$inf>)
+	{
+		my $tmp = encode_qp($_);
+		print $otf "$tmp";
+	}
+
+	close $inf;
+	close $otf;
 }
 
-close $inf;
-close $otf;
+main;
 
