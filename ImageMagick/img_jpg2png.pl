0501e9f05fb0ffebe5d823ad7eb740c884fce239
ericosur <ericosur@gmail.com>
2013-10-31 14:59:18 +0800

    update img_jpg2png.pl, revised
    Former-commit-id: d49f8a0618bf2a7ed83769b24e5de1f571291078

--- ImageMagick/img_jpg2png.pl
+++ ImageMagick/img_jpg2png.pl
@@ -1,39 +1,45 @@
 #!/usr/bin/perl
-#!/bin/perl
 
 #
 # 2006/12/27 by ericosur
 #
+# try to use Image::Magick to convert jpg to png
+#
 
-
+use strict;
 use Image::Magick;
 
-if (not $ARGV[0])
+sub main()
 {
-	print "please specify a jpeg file\n";
-	exit;
+	if (not $ARGV[0])
+	{
+		print "please specify a jpeg file\n";
+		exit;
+	}
+
+	my $img_file = $ARGV[0];
+	my $out_file = $img_file;
+	$out_file =~ s/jpg/png/i;
+
+	print $img_file, ' ', $out_file, "\n";
+
+	my $img = Image::Magick->new();
+	my $rc = $img->Read($img_file);
+	warn $rc if $rc;
+	$rc = $img->Write($out_file);
+	warn $rc if $rc;
+	#
+	# draw a image - test
+	#
+	$out_file = q(whiteGrad.png);
+	@$img = ();
+	$img->Set(size => '1600x1200');
+	$rc = $img->Read('gradient:#000000-#3f3f3f');
+	warn $rc if $rc;
+	# $img->Rotate(degrees => -90);
+	print "write $out_file...\n";
+	$rc = $img->Write($out_file);
+	warn $rc if $rc;
 }
 
-
-my $img_file = $ARGV[0];
-my $out_file = $img_file;
-$out_file =~ s/jpg/png/i;
-
-print $img_file, ' ', $out_file, "\n";
-
-my $img = Image::Magick->new();
-my $rc = $img->Read($img_file);
-warn $rc if $rc;
-$rc = $img->Write($out_file);
-warn $rc if $rc;
-
-#
-# draw a image - test
-#
-@$img = ();
-$img->Set(size => '100x600');
-$rc = $img->Read('gradient:#000000-#3f3f3f');
-warn $rc if $rc;
-# $img->Rotate(degrees => -90);
-$rc = $img->Write('whiteTile.bmp');
-warn $rc if $rc;
+main;
