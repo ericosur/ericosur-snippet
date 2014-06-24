401f4fd8d04d8b34040c416cc2bb1027bd53b7a6
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2014-06-24 09:31:29 +0800

    add bubblesort.c for testing various compilers
    Former-commit-id: f58874e3ea7d12079b89a0214b0d75ded28bc268

---
+++ C/bubblesort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void bubblesort(int val[], int n)
+{
+	int i, j, tmp;
+
+	for (i=0; i<(n-1); i++)  {
+	    for (j=0; j<(n-c-1); j++)  {
+	        if ( val[j] > val[j+1] ) {
+	            tmp = val[j];
+	            val[j] = val[j+1];
+	            val[j+1] = tmp;
+	        }
+	    }
+	}
+}
+
+void show(int val[], int n)
+{
+	int i;
+
+	for (i=0; i<n; i++) {
+	    printf("%d  ", val[i]);
+	}
+	printf("\n");
+}
+
+int main()
+{
+	int vv[] = {43, 57, 97, 91, 79, 3, 11, 29, 73, 83, 91, 29};
+	int nn = sizeof(vv)/sizeof(int);
+
+	show(vv, nn);
+	bubblesort(vv, nn);
+	show(vv, nn);
+
+	return 0;
+}
