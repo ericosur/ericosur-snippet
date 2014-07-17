7509c95668f539f84dbaa05984565ad8df4db317
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2014-07-17 09:44:35 +0800

    add seeif.c to test compiler default size
    Former-commit-id: a182cac6b70400cb0eb276a422020836b0a134fe

---
+++ C/seeif.c
@@ -0,0 +1,35 @@
+/*
+   seeif.c: to see value stored in memory is big endian or little endian
+*/
+#include <stdio.h>
+#include <string.h>
+#include <memory.h>
+#include <stdlib.h>
+
+#include <time.h>
+
+#define STRLEN 10
+
+int main()
+{
+    int v = 0xdeadbeef;
+    unsigned char s[STRLEN] = {0};
+    int i;
+
+    memcpy(s, &v, sizeof(v));
+    for (i=0; i<sizeof(v); i++) {
+        printf("%02x ", s[i]);
+    }
+    printf("\n");
+
+    unsigned char s2[] = {0xef, 0xbe, 0xad, 0xde};
+    memcpy(&v, s2, 4);
+    printf("v = %x\n", v);
+
+    time_t t;
+    printf("sizeof(time_t) = %d\n", sizeof(time_t));
+
+    printf("time: %d\n", time(NULL));
+
+    return 0;
+}
