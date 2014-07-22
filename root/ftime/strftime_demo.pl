2c6ef1d6e381396c99dfd1a91b8f069ddf61dfa6
ericosur <ericosur@mba>
2014-07-22 10:38:01 +0800

    update strftime_demo.pl: refine wording
    Former-commit-id: e597cb27e7e6179f19324914fbee1664b4fd089b

--- root/ftime/strftime_demo.pl
+++ root/ftime/strftime_demo.pl
@@ -19,10 +19,14 @@ use POSIX qw(strftime);
 my $local_str = strftime "%a %b %e %H:%M:%S %Y", localtime;
 # or for GMT formatted appropriately for your locale:
 my $gm_str = strftime "%a %b %e %H:%M:%S %Y", gmtime;
+# julian date
+my $doy = strftime "%j", localtime(time);
 
-printf "local: %s\ngm: %s\n", $local_str, $gm_str;
+printf "localtime:\n%s\n   gmtime:\n%s\n", $local_str, $gm_str;
+printf "day of year:\n%s\n", $doy;
 
 #
 # print ''Sun Mar 16 20:47:57 2008''
 #
+print "scalar localtime:\n";
 print scalar localtime,"\n";
