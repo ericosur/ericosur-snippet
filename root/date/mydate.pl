c3c962d1fbabda1a640e65bd2d6268fd0a4e1890
ericosur <ericosur@mba>
2014-07-22 10:37:34 +0800

    update mydate.pl for grouping into demo()
    Former-commit-id: c1b00c30010c38923333a95874f8a75e542915d7

--- root/date/mydate.pl
+++ root/date/mydate.pl
@@ -5,13 +5,7 @@
 use strict;
 use DateTime;
 
-sub show($$)
-{
-	my ($hh, $ff) = @_;
-    printf("%s %s\n", $hh, $ff);
-}
-
-my $dt = DateTime->new(
+my $default_dt = DateTime->new(
 	year => 2012,
 	month => 2,
 	day => 10,
@@ -21,32 +15,50 @@ my $dt = DateTime->new(
 	time_zone => 'Asia/Taipei',
 );
 
-show("hms:", $dt->hms);
+sub show($$)
+{
+	my ($hh, $ff) = @_;
+    printf("%s %s\n", $hh, $ff);
+}
 
-show("ce_year:", $dt->ce_year());
-show("era_name:", $dt->era_name());
-show("era_abbr:", $dt->era_abbr());
-show("christian_era:", $dt->christian_era());
-show("secular_era:", $dt->secular_era());
-show("year_with_era:", $dt->year_with_era());
-show("year_with_christian_era:", $dt->year_with_christian_era());
+sub demo()
+{
+	#my $dt = $default_dt;
+    # add time zone setting
+    my $dt = DateTime->now(time_zone => 'Asia/Taipei');
 
-show("day_of_week:", $dt->day_of_week());
-show("day_name:", $dt->day_name());
-show("day_of_year:", $dt->day_of_year());
-show("quarter:", $dt->quarter());
-show("weekday_of_month:", $dt->weekday_of_month());
-show("ymd sep=/:", $dt->ymd("/"));
-show("week:", $dt->week());
-show("week_year:", $dt->week_year());
-show("week_of_month:", $dt->week_of_month());
+	show("hms:", $dt->hms);
+	show("ce_year:", $dt->ce_year());
+	show("era_name:", $dt->era_name());
+	show("era_abbr:", $dt->era_abbr());
+	show("christian_era:", $dt->christian_era());
+	show("secular_era:", $dt->secular_era());
+	show("year_with_era:", $dt->year_with_era());
+	show("year_with_christian_era:", $dt->year_with_christian_era());
 
-show("jd:", $dt->jd());
-show("mjd:", $dt->mjd());
+	show("day_of_week:", $dt->day_of_week());
+	show("day_name:", $dt->day_name());
+	show("day_of_year:", $dt->day_of_year());
+	show("quarter:", $dt->quarter());
+	show("weekday_of_month:", $dt->weekday_of_month());
+	show("ymd sep=/:", $dt->ymd("/"));
+	show("week:", $dt->week());
+	show("week_year:", $dt->week_year());
+	show("week_of_month:", $dt->week_of_month());
 
-show("time_zone:", $dt->time_zone());
-show("time_zone_long_name:", $dt->time_zone_long_name());
-show("time_zone_short_name:", $dt->time_zone_short_name());
-show("epoch:", $dt->epoch());
+	show("jd:", $dt->jd());
+	show("mjd:", $dt->mjd());
 
+	show("time_zone:", $dt->time_zone());
+	show("time_zone_long_name:", $dt->time_zone_long_name());
+	show("time_zone_short_name:", $dt->time_zone_short_name());
+	show("epoch:", $dt->epoch());
+}
+
+sub main()
+{
+	demo();
+}
+
+main;
 
