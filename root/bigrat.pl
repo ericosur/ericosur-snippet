f7220d700109d07996c244005ff5d58329b8962f
ericosur <ericosur@mba>
2014-08-05 09:19:45 +0800

    update bigrat.pl: add a demo function
    Add a demo function to say bigrat only works with contants.
    
    
    Former-commit-id: 944bdbb8340311270eaa12637c37ee65bb633c8a

--- root/bigrat.pl
+++ root/bigrat.pl
@@ -4,18 +4,42 @@
 #
 # 2007/01/10 by ericosur
 # 2008/02/19 reviewed
+# see git log for history
 
 use strict;
 use warnings;
 
 use bigrat;
 
-print "1/2 + 1/3 + 3/4 = ", 1/2 + 1/3 + 3/4, "\n";
-
-my @a = (1/7, 1/8, 1/3);
-my $sum = 0;
-foreach (@a)  {
-	$sum += $_;
+# you may see it does not work
+sub demo()
+{
+    my $sum = 0;
+    for my $ii (2..9) {
+        my $v = 1/$ii;
+        $sum += $v;
+    }
+    print "sum = ", $sum, "\n";
 }
-print join(' + ', @a), "= ";
-print $sum,"\n";
+
+sub easy()
+{
+	print "1/2 + 1/3 + 3/4 = ", 1/2 + 1/3 + 3/4, "\n";
+
+	my @a = (1/7, 1/8, 1/3);
+	my $sum = 0;
+	foreach (@a)  {
+		$sum += $_;
+	}
+	print join(' + ', @a), " = ";
+	print $sum,"\n";
+}
+
+sub main()
+{
+    easy();
+    demo();
+}
+
+main;
+
