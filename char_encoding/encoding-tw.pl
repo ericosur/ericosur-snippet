594cbbf91394b068cd3535a498575b893b068c21
ericosur <ericosur@mba>
2014-08-22 09:49:34 +0800

    update encoding-tw for more message
    Former-commit-id: 8be55c839f357d9a246069c1cb208d6f734d2471

--- char_encoding/encoding-tw.pl
+++ char_encoding/encoding-tw.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-use lib '../';
+use lib '../../root/';
 use Ericosur;
 
 use Encode qw/encode decode/;
@@ -9,11 +9,17 @@ sub main()
 	$utf8 = "中文abc";
 	#$utf8 = "\x{fffeb48c}";
 
+    print "utf8: $utf8\n";
+    hexdump($utf8);
+    sep();
+
+    print "output big5 char...\n";
 	$big5 = encode("big5", $utf8); # loads Encode::TW implicitly
 	show($big5);
 	hexdump($big5);
 	sep();
 
+    print "output big5 char again...\n";
 	$utf8 = decode("big5", $big5); # ditto
 	show($utf8);
 	hexdump($utf8);
