e066d2f70bb5cbd9034851fa0204ec705205f73a
ericosur <ericosur@mba>
2014-08-22 09:23:26 +0800

    update 5c5c.pl: add main() and message
    Former-commit-id: d95fca318a736a025bf41d0dd14a4edfe06eae09

--- char_encoding/chars/5c5c.pl
+++ char_encoding/chars/5c5c.pl
@@ -4,6 +4,8 @@
 # this text file is encoding as UTF-8
 #
 
+use strict;
+use warnings;
 use Encode qw(from_to);
 
 sub shex($)
@@ -13,25 +15,35 @@ sub shex($)
 		unpack('W*', shift));
 }
 
-# 許蓋功 (BIG5 encoding)
-print "big5 encoding: ";
-my $str = pack('H12', "b35cbb5ca55c");
-shex($str);
-print "\n";
+sub main()
+{
+    binmode STDOUT;
 
-#from_to($string, "Shift_JIS", "GB2312");
+    # 許蓋功 (BIG5 encoding)
+    print "big5 encoding: ";
+    my $str = pack('H12', "b35cbb5ca55c");
+    shex($str);
+    print "\n";
+    print $str,"\n";
 
-print "utf8 encoding: ";
-from_to($str, "BIG5", "UTF8");
-shex($str);
-print "\n";
+    #from_to($string, "Shift_JIS", "GB2312");
 
-print "gb2312 encoding: ";
-from_to($str, "UTF8", "GBK");
-shex($str);
-print "\n";
+    print "utf8 encoding: ";
+    from_to($str, "BIG5", "UTF8");
+    shex($str);
+    print "\n";
+    print $str,"\n";
 
-#
-# pack("CCC",0xef,0xbb,0xbf); ==> EF BB BF
-# pack("H6", "EFBBBF") ==> EF BB BF
-#
+    print "gb2312 encoding: ";
+    from_to($str, "UTF8", "GBK");
+    shex($str);
+    print "\n";
+    print $str,"\n";
+
+    #
+    # pack("CCC",0xef,0xbb,0xbf); ==> EF BB BF
+    # pack("H6", "EFBBBF") ==> EF BB BF
+    #
+}
+
+main;
