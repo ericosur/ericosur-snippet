a13cc9b525977902c9f1ba5b36cf40e2e09d645d
ericosur <ericosur@mba>
2014-09-19 09:49:58 +0800

    update bubblesort.c: fix a typo with 'for' loop
    Former-commit-id: 9abd07f33a1ee952ea99c0d3f73c88591f76c792

--- C/bubblesort.c
+++ C/bubblesort.c
@@ -5,7 +5,7 @@ void bubblesort(int val[], int n)
 	int i, j, tmp;
 
 	for (i=0; i<(n-1); i++)  {
-	    for (j=0; j<(n-c-1); j++)  {
+	    for (j=0; j<(n-i-1); j++)  {
 	        if ( val[j] > val[j+1] ) {
 	            tmp = val[j];
 	            val[j] = val[j+1];
