78b5e64cfdc064d7498d899ae9129c836698667c
ericosur <ericosur@mba>
2014-11-27 10:59:17 +0800

    add javascripting, learning js basics
    Former-commit-id: 67e6ae33e44165e8010a98c5932f02d912bd8aa4

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+function eat(food) {
+    return food + ' tasted really good.';
+}
+
+console.log(eat('bananas'));
+
+/*
+console.log( function (x) {
+    return x*x*3;
+}.call(5) );
+*/
---
+++ js/objects.js
@@ -0,0 +1,11 @@
+var pizza = {
+    toppings: ['cheese', 'sauce', 'pepperoni'],
+    crust: 'deep dish',
+    serves: 2
+}
+
+console.log(pizza);
+
+console.log(pizza.toppings);
+console.log(pizza.toppings[2]);
+console.log(--pizza.serves);
