2f6b10886e34afa369b665c27ddddf37f26b42f0
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2014-11-27 15:02:40 +0800

    update js, add more message into script
    Former-commit-id: 1f824372567dee869d64ed83c72f5f3054a0b316

--- js/for-loop.js
+++ js/for-loop.js
@@ -1,9 +1,10 @@
+// for loop and sum up
+
 var total = 0;
 var limit = 10;
 
 for (var i = 0; i < limit; i++) {
-    total = total + i;
+    total += i;
 }
 
-console.log(total);
-
+console.log('sum up from 1 to', limit, 'is', total);
--- js/number-to-string.js
+++ js/number-to-string.js
@@ -1,5 +1,7 @@
 var n = 128;
-n.toString();
-console.log(n);
-n=n+1;
-console.log(n);
+
+console.log('n is a number:', n);
+
+n = n.toString();
+n = n + 1;
+console.log('n.toString() + 1:', n);
