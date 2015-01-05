28bd38b6613fbbd49dca0339da18307ad18647b6
ericosur <ericosur@mba>
2015-01-05 12:45:55 +0800

    stupid_factorial.py:     add an example for 300!     add Stirling's approximation
    Former-commit-id: 59641250b4dafe272a4e84ae0256839fceabb9f8

--- python/fib/stupid_factorial.py
+++ python/fib/stupid_factorial.py
@@ -1,7 +1,28 @@
+import math
+
 def stupid_factorial(m):
 	if m <= 1:
 		return 1
 	else:
 		return m * stupid_factorial(m - 1)
 
-print stupid_factorial(5)
+def main():
+    n = 300 # try to get n!
+    ans = stupid_factorial(n)
+    print n, "! =", ans
+    print "log10(ans) = ", math.log10(ans)
+    print "string len: ", len(str(ans))
+    '''
+    try to use Stirling's approximation for value of n!
+    n! ~ sqrt(2*pi*n)*(n/e)^n
+    but (n/e)^n is too large
+
+    try to know how many digits for n!
+    log10(n!) ~ log10(sqrt(2*pi*n)) + n * log10(n/e)
+
+    '''
+    val = math.log10(2*math.pi*n) + n * math.log10(n/math.e)
+    print "log(val) =", val
+
+if __name__ == '__main__':
+    main()
