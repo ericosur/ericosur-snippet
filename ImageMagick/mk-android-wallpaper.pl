0d9f81dcff85655df6c802295a1c3fd7eb7670cb
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2015-01-06 11:36:52 +0800

    update mk-android-wallpaper.pl
    Former-commit-id: b8cbc1ef7b597833188e31d5ffcf3cf4cc643b72

--- ImageMagick/mk-android-wallpaper.pl
+++ ImageMagick/mk-android-wallpaper.pl
@@ -12,10 +12,69 @@ use strict;
 use warnings;
 
 use Image::Magick;
+use File::Basename;
+use Data::Dump qw(dump);
 use v5.10;
 
 my $debug = 0;
 
+
+# try to crop & resize to specified size
+# [in] input file name
+# [in] output file name
+sub make_size($$$$)
+{
+    dump(@_);
+	my ($ifile, $ofile, $target_w, $target_h) = @_;
+	my ($imgw, $imgh) = (0, 0);
+	my $ori = 0; 	# 0: landscape, 1: portrait
+	my $geo;
+	my $rc;
+
+	my $im1 = Image::Magick->new();
+	if ( -f $ifile )  {
+		$im1->Read($ifile);
+		($imgw, $imgh) = $im1->Ping($ifile);
+		if ($imgw > $imgh) {
+			$ori = 0;
+		} else {
+			$ori = 1;
+		}
+	} else {
+	    say "invalid input file";
+	    return;
+	}
+
+	printf("w / h = %.3f", $imgw / $imgh);
+
+    if ($target_w < $target_h) {
+        ($target_w, $target_h) = ($target_h, $target_w);
+    }
+
+	# must large enough to make android wallpaper corp algorithm happy
+	# galaxy nexus is 9:16 (720:1280)
+	# xperia XL is 9:16 (1080:1920)
+	#my ($target_w, $target_h);
+
+	if ($ori == 1) {
+		# for portrait
+		($target_w, $target_h) = ($target_h, $target_w);
+    }
+
+	my $im = Image::Magick->new();
+	my $size = sprintf("%dx%d", $target_w, $target_h);
+	say "size: $size";
+	$rc = $im->Set(size => $size);
+	$rc = $im->Read('xc:black');
+
+	my ($newx, $newy) = ();
+	$newx = ($target_w - $imgw) / 2;
+	$newy = ($target_h - $imgh) / 2;
+	say "new: $newx, $newy";
+
+	$rc = $im->Composite(image => $im1, x => $newx, y => $newy);
+	$im->Write($ofile);
+}
 
 # try to crop & resize to 8:5
 # [in] input file
@@ -66,19 +125,54 @@ sub make_8v5($$)
 	$im->Write($ofile);
 }
 
+# get output image file name
+sub get_ofn($$)
+{
+    my $in = shift;
+    my $append = shift // "-ext";
+    my $jpg = ".jpg";
+    my $bsn = basename($in, qw(.jpg .png));
+    my $res = $bsn . $append . $jpg;
+
+    # if file name exists, increase suffix
+    my $num = 0;
+    while (-e $res) {
+        $num ++;
+        $res = sprintf("%s%s-%d%s", $bsn, $append, $num, $jpg);
+    }
+
+    #say "ofn: in: $in";
+    #say "ofn: res: $res";
+    return $res;
+}
+
+sub doit($)
+{
+    my $im = shift;
+    my @comb = (
+        #{'n', q(85.jpg), 'w', 8000, 'h', 5000},
+        {'n', q(845.jpg), 'w', 8000, 'h', 4494},
+        #{'n', q(84.jpg), 'w', 8000, 'h', 4000},
+    );
+
+    #dump(%comb);
+    foreach my $e (@comb) {
+        #printf("n(%s), w(%d), h(%d)\n",
+        #    $e->{'n'}, $e->{'w'}, $e->{'h'});
+        make_size($im, $e->{'n'}, $e->{'w'}, $e->{'h'});
+    }
+
+}
 
 sub main()
 {
-	my @imgs = ($ARGV) // qw(all.jpg);
-	my $outimg = 'out.jpg';
-
-	say @imgs;
-	if (-f $imgs[0])  {
-		say "in: $imgs[0]";
-		say "out: $outimg";
-		make_8v5($imgs[0], $outimg);
+    my $im = $ARGV[0] // "tong.jpg";
+	if ($im && -f $im)  {
+		say "in: $im";
+        doit($im);
+	} else {
+	    say "err: please specify input image name";
 	}
-
 }
 
 main;
