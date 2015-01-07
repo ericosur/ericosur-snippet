670d60177aa3a0d94c3fe03b42ea6e25cb564c44
ericosur <ericosur@mba>
2015-01-07 19:55:46 +0800

    update guess_4.pl
    remove action of swap keys and values in hash
    sort hash by values
    add display of possible answers, percentage
    
    
    Former-commit-id: a0e4f9ad28e7fdfb9f5a2b4322fb576f6785216f

--- guess/guess_4.pl
+++ guess/guess_4.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl
 
+# ?A?B list
+# sort hash by value
+
 use strict;
 use warnings;
 
@@ -8,53 +11,53 @@ my @total = ();
 # from 0000 to 9999
 sub list_all
 {
-	my $str;
-	for (0..9999)  {
-		$str = sprintf "%04d", $_;
-		if (is_duplicate($str))  {	# no duplicated number
-			next;
-		}
-#		if (is_lead_zero($str))  {	# no start with 0
-#			next;
-#		}
-		#print $str,"\n";
-		push @total, $str;
-	}
+    my $str;
+    for (0..9999)  {
+        $str = sprintf "%04d", $_;
+        if (is_duplicate($str))  {  # no duplicated number
+            next;
+        }
+#       if (is_lead_zero($str))  {  # no start with 0
+#           next;
+#       }
+        #print $str,"\n";
+        push @total, $str;
+    }
 }
 
 sub is_duplicate($)
 {
-	my $value = shift;
+    my $value = shift;
 
-	if (length($value) ne 4)  {
-		#die "length is not 4";
+    if (length($value) ne 4)  {
+        #die "length is not 4";
         return 1;
-	}
+    }
 
-	my %dup = ();
-	for (split //, $value)  {
-		$dup{$_} ++;
-		#print "$_\t";
-	}
-	#print "\n";
-	for (keys %dup)  {
-		#print $_, ":", $dup{$_}, "\n";
-		if ($dup{$_} > 1)  {
-			return 1;
-		}
-	}
-	return 0;
+    my %dup = ();
+    for (split //, $value)  {
+        $dup{$_} ++;
+        #print "$_\t";
+    }
+    #print "\n";
+    for (keys %dup)  {
+        #print $_, ":", $dup{$_}, "\n";
+        if ($dup{$_} > 1)  {
+            return 1;
+        }
+    }
+    return 0;
 }
 
 sub is_lead_zero
 {
-	my $value = shift;
+    my $value = shift;
 
-	if ( $value =~ m/^0\d{3}/ )  {
-		return 1;
-	} else  {
-		return 0;
-	}
+    if ( $value =~ m/^0\d{3}/ )  {
+        return 1;
+    } else  {
+        return 0;
+    }
 }
 
 # [in] answer
@@ -62,61 +65,69 @@ sub is_lead_zero
 # [out] (aa, bb)
 sub get_ab
 {
-	my ($ans, $guess) = @_;
-	my ($a, $b) = (0, 0);
+    my ($ans, $guess) = @_;
+    my ($a, $b) = (0, 0);
 
-	for my $i (0..3)  {
-		for my $j (0..3)  {
-			my $mm = substr($guess, $i, 1);
-			my $nn = substr($ans, $j, 1);
-			if ($mm eq $nn)  {
-				if ($i eq $j)  {
-					$a ++;
-				}
-				else  {
-					$b ++;
-				}
-			}
-		}
-	}
-	my $result = sprintf "%dA%dB", $a, $b;
-	return $result;
+    for my $i (0..3)  {
+        for my $j (0..3)  {
+            my $mm = substr($guess, $i, 1);
+            my $nn = substr($ans, $j, 1);
+            if ($mm eq $nn)  {
+                if ($i eq $j)  {
+                    $a ++;
+                }
+                else  {
+                    $b ++;
+                }
+            }
+        }
+    }
+    my $result = sprintf "%dA%dB", $a, $b;
+    return $result;
 }
 
 sub main()
 {
-	my %all_guess = ();
-    my %bguess = ();
-	my $ans;
+    my %ags = ();
+    my %bgs = ();
+    my $ans;
 
+    # 0 <= $ans <= 9999, and check if valid
     do {
         $ans = int(rand(10000));
     } until ( !is_duplicate($ans) );
-	list_all();
-	for (@total)  {
-		my $res = get_ab($ans, $_);
-		$all_guess{$res} ++;
-        #$bguess{$res} = ('a');
-        #push($bguess{$res}, $res);
-	}
 
-	# print out the result
-	my $cnt = 0;
-	my %foo = ();
-	for (keys %all_guess)  {
-		#printf "%s: %d\n", $_, $all_guess{$_};
-		$cnt += $all_guess{$_};
-		$foo{ $all_guess{$_} } = $_;
-	}
-    if ($#total+1 != $cnt) {
-        print "cnt = $cnt\n";
+    list_all();
+    for my $tt (@total)  {
+        # $res = "0A3B"...
+        my $res = get_ab($ans, $tt);
+        $ags{$res} ++;
+        # if first used, put an empty array for it
+        if ( ref($bgs{$res}) ne 'ARRAY' ) {
+            $bgs{$res} = [];
+        }
+        # push( $bgs{'1A2B'}, '1234' )
+        push($bgs{$res}, $tt);
     }
-    printf("for answer: %s\n", $ans);
-	print "total = ", $#total+1,"\n";
 
-	for (sort {$a <=> $b} keys %foo)  {
-		printf "%s: %s (%.2g%%)\n", $foo{$_}, $_, ($_*100/$cnt);
-	}
+    # print out the result
+    my $cnt = $#total + 1;
+    my $sum = 0;
+    printf("for answer: %s\n", $ans);
+    print "total = ", $cnt,"\n";
+
+    for my $k (sort {$ags{$a} <=> $ags{$b}} keys %ags)  {
+        printf "%s: %s (%.2g%%)\n", $k, $ags{$k}, ($ags{$k}*100/$cnt);
+        $sum += $ags{$k};
+        if ($ags{$k} < 50) {
+            my $rr = $bgs{ $k };
+            foreach ( @$rr ) {
+                print $_,", ";
+            }
+            print "\n";
+        }
+    }
+    warn if $sum != $cnt;
 }
 
 main;
