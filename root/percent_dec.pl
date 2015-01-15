be2e1206c19d0c58c83ee57f9352d64d0109547d
ericosur <ericosur@mba>
2015-01-15 15:54:06 +0800

    update percent_dec.pl, chmod and tab->space
    Former-commit-id: 68431eceaac4c4d3bd0b82092d97f38107300b95

--- root/percent_dec.pl
+++ root/percent_dec.pl
@@ -12,60 +12,60 @@ use strict;
 
 sub decode_percent($)
 {
-	my $str = shift;
-	$str =~ s/%([a-fA-F0-9][a-fA-F0-9])/pack("C", hex($1))/eg;
-	return $str;
+    my $str = shift;
+    $str =~ s/%([a-fA-F0-9][a-fA-F0-9])/pack("C", hex($1))/eg;
+    return $str;
 }
 
 sub encode_percent($)
 {
-	#use Encode qw(from_to);
-	my $str = shift;
-	#from_to($string, "Shift_JIS", "GB2312");
-	$str =~ s/(\W)/sprintf("%%%02X",unpack("C",$1))/eg;
+    #use Encode qw(from_to);
+    my $str = shift;
+    #from_to($string, "Shift_JIS", "GB2312");
+    $str =~ s/(\W)/sprintf("%%%02X",unpack("C",$1))/eg;
 
-	# it is UTF8
-	return $str;
+    # it is UTF8
+    return $str;
 }
 
 sub process_file($$)
 {
-	my $ifile = shift;
-	my $ofile = shift;
-	my $ifh;
-	my $ofh;
+    my $ifile = shift;
+    my $ofile = shift;
+    my $ifh;
+    my $ofh;
 
-	open $ifh, $ifile or die "$ifile: $!";
-	if ($ofile eq 'stdout')  {
-		$ofh = \*STDOUT;
-	}
-	else  {
-		open $ofh, "> $ofile" or die;
-	}
+    open $ifh, $ifile or die "$ifile: $!";
+    if ($ofile eq 'stdout')  {
+        $ofh = \*STDOUT;
+    }
+    else  {
+        open $ofh, "> $ofile" or die;
+    }
 
-	while (<$ifh>)  {
-		my $dec = decode_percent($_);
-		print $ofh $dec;
-	}
-	close $ifh;
-	close $ofh;
+    while (<$ifh>)  {
+        my $dec = decode_percent($_);
+        print $ofh $dec;
+    }
+    close $ifh;
+    close $ofh;
 }
 
 sub main
 {
 =pod
-	# stdin version
+    # stdin version
 
-	while (<>)  {
-		print decode_percent($_);
-	}
+    while (<>)  {
+        print decode_percent($_);
+    }
 =cut
-	# file i/o version
-	my $ifile = $ARGV[0] // "-";
-	my $ofile = $ARGV[1] // "stdout";
+    # file i/o version
+    my $ifile = $ARGV[0] // "-";
+    my $ofile = $ARGV[1] // "stdout";
 
 
-	process_file($ifile, $ofile);
+    process_file($ifile, $ofile);
 }
 
 main;
