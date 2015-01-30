efab222eba347e932ac0670cdbfc59a6ac6847f0
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2015-01-30 10:29:14 +0800

    update argv.js, add help if no argv specified
    Former-commit-id: 7d42cf46efa6462a5c7d56ae13147cc574362791

--- nodejs/argv.js
+++ nodejs/argv.js
@@ -1,7 +1,16 @@
-//console.log(process.argv)
-var sum = 0;
-for (var i=2; i<process.argv.length; i++) {
-    //console.log(i,process.argv[i]);
-    sum = sum + Number(process.argv[i]);
-}
-console.log(sum);
+// simple demo to sum up argv
+
+(function() {
+    //console.log(process.argv)
+    if (process.argv.length <= 2) {
+        console.log("try: argv.js 1 2 3 4 ...");
+        return;
+    }
+
+    var sum = 0;
+    for (var i=2; i<process.argv.length; i++) {
+        //console.log(i,process.argv[i]);
+        sum = sum + Number(process.argv[i]);
+    }
+    console.log(sum);
+})();
