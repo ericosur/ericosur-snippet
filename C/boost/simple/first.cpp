f72ac841b9516b82c314943f5e6f8df0abf3701e
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2015-02-10 08:25:41 +0800

    add simple boost test C++ source
    Former-commit-id: 3a6c98fbf9820217c97ccf8bdbfb67fe00d5709d

---
+++ C/boost/simple/first.cpp
@@ -0,0 +1,19 @@
+// no library need to list
+// need specify boost include file path while compiling
+
+#include <boost/lambda/lambda.hpp>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+int main()
+{
+    using namespace boost::lambda;
+    typedef std::istream_iterator<int> in;
+
+    std::for_each(
+        in(std::cin), in(), std::cout << (_1 * 3) << " " );
+
+    return 0;
+}
+
