e34524bcab893ecfd0bfede4e7d5c826acd5346e
ericosur <ericosur@mba>
2015-03-09 12:51:36 +0800

    tab to space
    Former-commit-id: 9f80b4cfd7a7580d7b74fcbf4253de0c40deb41b

--- C/fib.c
+++ C/fib.c
@@ -15,47 +15,46 @@ int fib4(int,int,int);
 /* Hauptprogramm */
 
 int main() {
-        printf("Alg. 1 : %d\n",fib1(10));
-        printf("Alg. 4 : %d\n",fib4(10,1,1));
+    printf("Alg. 1 : %d\n",fib1(10));
+    printf("Alg. 4 : %d\n",fib4(10,1,1));
 }
 
 /* Algorithmus 1 */
 
 int fib1(int n) {
-        if (n<=2) return 1;
-        else return fib1(n-1)+fib1(n-2);
+    if (n<=2) return 1;
+    else return fib1(n-1)+fib1(n-2);
 }
 
 /* Algorithmus 1a */
 
 int fib1a(int n) {
-        if (n<=2) return 1;
-        return fib1a(n-1)+fib1a(n-2);
+    if (n<=2) return 1;
+    return fib1a(n-1)+fib1a(n-2);
 }
 
 /* Algorithmus 3 */
 
 int fib3(int n) {
-        int a,b,neu;
-        int zaehler;
+    int a,b,neu;
+    int zaehler;
 
-        if (n<=2) return 1;
-        a = 1;
-        b = 1;
-        for (zaehler=3; zaehler<=n; zaehler++)
-        {
-                neu = a+b;
-                a = b;
-                b = neu;
-        }
-        return neu;
+    if (n<=2) return 1;
+    a = 1;
+    b = 1;
+    for (zaehler=3; zaehler<=n; zaehler++)
+    {
+        neu = a+b;
+        a = b;
+        b = neu;
+    }
+    return neu;
 }
 
 /* Algorithmus 4 */
 
 int fib4(int n, int a, int b) {
-        if (n==1) return a;
-        if (n==2) return b;
-        return fib4(n-1,b,a+b);
+    if (n==1) return a;
+    if (n==2) return b;
+    return fib4(n-1,b,a+b);
 }
-
