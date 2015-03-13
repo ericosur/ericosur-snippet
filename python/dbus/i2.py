ab2d09bd5dc047d5b13a31d5e4a38f5003379a07
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2015-03-13 19:54:18 +0800

    add python/dbus/
    Former-commit-id: 8b4ac809514c5b42282e5e7837e6aa5d638df1af

---
+++ python/dbus/i2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+'''
+https://wiki.python.org/moin/DbusExamples
+'''
+
+# You must initialize the gobject/dbus support for threading
+# before doing anything.
+import gobject
+gobject.threads_init()
+
+from dbus import glib
+glib.init_threads()
+
+# Create a session bus.
+import dbus
+bus = dbus.SessionBus()
+
+# Create an object that will proxy for a particular remote object.
+remote_object = bus.get_object("org.freedesktop.DBus", # Connection name
+                               "/org/freedesktop/DBus" # Object's path
+                              )
+
+# Introspection returns an XML document containing information
+# about the methods supported by an interface.
+# print ("Introspection data:\n")
+# print remote_object.Introspect()
+
+# get a particular interface
+iface = dbus.Interface(remote_object, 'org.freedesktop.DBus')
+arr = iface.ListNames()
+for x in arr:
+    print x
+
