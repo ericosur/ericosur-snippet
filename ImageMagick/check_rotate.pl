12e94ba74bdcff565990b52c7c2c4b1a0b333561
ericosur <ericosur@mba>
2015-03-13 19:52:33 +0800

    tab2space and add main()
    Former-commit-id: 940fe8c572dc9d4cf21365c5088ca10dad598506

--- ImageMagick/check_rotate.pl
+++ ImageMagick/check_rotate.pl
@@ -16,90 +16,97 @@ sub process($);
 sub get_orientation($);
 sub get_image_size($);
 
+my $verbose = 0;
+
 #
 # start main procedure
 #
-my $file = 'list.txt';
-my $verbose = 0;
+sub main()
+{
 
-open my $fh, $file or die "file list needed\n";
+    my $file = 'list.txt';
 
-print '@echo off\n';
-print "rem brutely rotated image list:\n";
+    open my $fh, $file or die "file list needed\n";
 
-while (<$fh>)  {
-	s/\r//;
-	s/\n//;
+    print '@echo off\n';
+    print "rem brutely rotated image list:\n";
 
-	process($_);
+    while (<$fh>)  {
+        s/\r//;
+        s/\n//;
+
+        process($_);
+    }
+
+    close $fh;
 }
-
-close FH;
-
 
 sub process($)
 {
-	my $file = $_;
-	my $result1 = 0;
-	my $result2 = 0;
-	my $total_file = 0;
-	my $found_file = 0;
+    my $file = $_;
+    my $result1 = 0;
+    my $result2 = 0;
+    my $total_file = 0;
+    my $found_file = 0;
 
     printf "%s:\n", $_ if $verbose == 1;
-	$result1 = get_orientation($file);
-	$result2 = get_image_size($file);
+    $result1 = get_orientation($file);
+    $result2 = get_image_size($file);
 
-	if ($result1 == 1 && $result2 == 1)  {
-		my $cmd = sprintf "exiftool -exiftool -Orientation=Horizontal \"%s\"", $file;
+    if ($result1 == 1 && $result2 == 1)  {
+        my $cmd = sprintf "exiftool -exiftool -Orientation=Horizontal \"%s\"", $file;
 
-		print $file, "\n";
-		system $cmd;
-	}
+        print $file, "\n";
+        system $cmd;
+    }
 }
 
 
 sub get_orientation($)
 {
-	my $img_file = $_;
-	my $is_rotate = 0;
+    my $img_file = $_;
+    my $is_rotate = 0;
 
-	if ( -f $img_file )  {
-		my $info = ImageInfo($img_file);
-		my ($orient, $rotate) = (
-			$$info{'Orientation'},
-			$$info{'Auto Rotate'},
-		);
+    if ( -f $img_file )  {
+        my $info = ImageInfo($img_file);
+        my ($orient, $rotate) = (
+            $$info{'Orientation'},
+            $$info{'Auto Rotate'},
+        );
 
-		if ( $orient =~ /rotate/i )  {
-			$is_rotate = 1;
-		}
+        if ( $orient =~ /rotate/i )  {
+            $is_rotate = 1;
+        }
 
-		if ($verbose == 1)  {
-			print $orient;
-			print "\t", $rotate if $rotate;
-			print "\n";
-		}
-	}
-	return $is_rotate;
+        if ($verbose == 1)  {
+            print $orient;
+            print "\t", $rotate if $rotate;
+            print "\n";
+        }
+    }
+    return $is_rotate;
 }
 
 
 sub get_image_size($)
 {
-	my $img_file = $_;
-	my $result = 0;
+    my $img_file = $_;
+    my $result = 0;
 
-	if (-f $img_file)  {
-		my $im = Image::Magick->new();
+    if (-f $img_file)  {
+        my $im = Image::Magick->new();
 
-		if ( -f $img_file )  {
-			my ($width, $height) = $im->Ping($img_file);
-			printf "%d, %d\n", $width, $height if $verbose == 1;
-			if ($width < $height)  {
-				$result = 1;
-			}
-		}
-		undef $im;
-	}
-	return $result;
+        if ( -f $img_file )  {
+            my ($width, $height) = $im->Ping($img_file);
+            printf "%d, %d\n", $width, $height if $verbose == 1;
+            if ($width < $height)  {
+                $result = 1;
+            }
+        }
+        undef $im;
+    }
+    return $result;
 }
+
+main;
+
