b1c871257e27933d04c7dfda49ab9a6a3e91f8df
ericosur <ericosur@mba>
2015-05-12 11:04:41 +0800

    make cvcli for macx environment
    Former-commit-id: 308db361287e919038919a48635fbe7e1c20c541

--- opencv/blur/cvcli/cvcli.pro
+++ opencv/blur/cvcli/cvcli.pro
@@ -3,6 +3,11 @@ CONFIG += console
 CONFIG -= app_bundle
 CONFIG -= qt
 
+macx {
+    INCLUDEPATH += /opt/local/include
+    LIBS += -L/opt/local/lib
+}
+
 LIBS += -lopencv_core -lopencv_highgui -lopencv_imgproc
 
 SOURCES += main.cpp
--- opencv/blur/cvcli/main.cpp
+++ opencv/blur/cvcli/main.cpp
@@ -1,6 +1,8 @@
 // reference:
 // http://blogs.wcode.org/2014/10/howto-install-build-and-use-opencv-macosx-10-10/
 
+#include <unistd.h>
+#include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -10,7 +12,26 @@ using namespace cv;
 
 const char window_name1[] = "Unprocessed Image";
 const char window_name2[] = "Processed Image";
-const char default_img[] = "deer.jpg";
+#if defined(__APPLE__)
+const string DEFAULT_IAMGE = "/Users/ericosur/gcode/snippet/opencv/blur/deer.jpg";
+#endif
+#if defined(__linux__)
+const string DEFAULT_IAMGE = "/home/rasmus/gcode/snippet/opencv/blur/deer.jpg";
+#endif
+
+// load image into Mat, will check file exist or not
+bool load_image(Mat& img, const string& fn)
+{
+    cout << "load image: " << fn << endl;
+    if (access(fn.c_str(), F_OK) != -1) {
+        // file exists
+        img = imread(fn);
+        return true;
+    }
+
+    // file not exist
+    return false;
+}
 
 int main( int argc, char** argv )
 {
@@ -18,10 +39,16 @@ int main( int argc, char** argv )
     Mat dst;
 
     // Load the source image
+    bool ret = false;
     if (argc >= 2) {
-        src = imread(argv[1], 1);
+        ret = load_image(src, argv[1]);
     } else {
-        src = imread(default_img, 1);
+        ret = load_image(src, DEFAULT_IAMGE);
+    }
+
+    if (!ret) {
+        cout << "cannot load image, exit...\n";
+        return -1;
     }
 
     namedWindow( window_name1, WINDOW_AUTOSIZE );
