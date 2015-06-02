99161bb52d8a00ace88588154c6e174902301e4f
ericosur <ericosur@mba>
2015-06-02 11:26:43 +0800

    update comments
    Former-commit-id: 11b342dea755770cd180c4a6beb2052a3456b50f

--- js/nodejs/callmyfilter.js
+++ js/nodejs/callmyfilter.js
@@ -1,8 +1,10 @@
+// demo how to use myfilter.js
+
 var qq = require('./myfilter');
+
 qq(process.argv[2], process.argv[3], function(err,data) {
     if (err) throw err;
     data.forEach(function(elem,index,array) {
         console.log(elem);
     });
 });
-
