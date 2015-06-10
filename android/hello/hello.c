c60684c97adfede286994a35fdc753b93d80ff07
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2015-06-10 17:25:04 +0800

    update hello.c compiled against android 4.2
    Former-commit-id: 007ef0032593c59dc80bc5bbbefb696adc28ff8a

--- android/hello/hello.c
+++ android/hello/hello.c
@@ -3,20 +3,15 @@
 #include <utils/Log.h>
 
 #define LOG_TAG	"ericosur"
-#define REPEAT	100
-#define BUF_LEN	100
+#define REPEAT	10
 
 int main()
 {
 	unsigned int cnt = 0;
-	char msg[BUF_LEN];
 
 	printf("stupid hello world build at %s %s\n", __DATE__, __TIME__);
-	memset(msg, 0, BUF_LEN); 
 	while (cnt < REPEAT)  {
-		sprintf(msg, "(%d) hello, android", cnt);
-		printf("%s\n", msg);
-		LOGI(msg);
+		ALOGI("***** [%d] hello android from CLI *****\n", cnt);
 		sleep(1);
 		cnt ++;
 	}
