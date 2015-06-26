e217ac29c6d4ae4c53544a0322671fce45605e27
ericosur <ericosur@mba>
2015-06-26 10:55:24 +0800

    add two more sample for unicode characters
    Former-commit-id: 218e32cfd5a2cda3e5ad9d1db8730a5651e387f6

--- python/apple-logo.py
+++ python/apple-logo.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
 
+def show_utf8char(ch):
+    uch = ch.decode('utf8')
+    print uch
+    print 'len: ', len(uch)
+    # not work if codepoint > 0xffff
+    #print 'hex: ', hex(ord(uch))
+    print 'uen: ', uch.encode('unicode-escape')
+    print
+
+
 # utf8 octets for apple logo (private use area)
 # utf8: EF A3 BF
 # U+F8FF
 
 a = '\xef\xa3\xbf'
-ua = a.decode('utf8')
+show_utf8char(a)
 
-print ua
-print hex(ord(ua))
+b = '\xF0\x9F\x90\xB1'
+show_utf8char(b)
+
+# U+F92F (CJK Compatibility Ideographs)
+print u'\uF92F'
+
+# U+52DE (CJK Unified Ideographs)
+print u'\u52DE'
