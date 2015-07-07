7ef151ca21ed9bc96d7b7469c6318f4ca06a5ae9
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2015-07-07 17:11:06 +0800

    add Qt example for multilanguage
    Former-commit-id: c2970012fb331c17a4ac5414c2cfbdc5aa39733a

---
+++ qt/MultiLanguageApp/i18n/call_lupdate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+lupdate ../translation.cpp -ts lang_en_US.ts
+lupdate ../translation.cpp -ts lang_ru_RU.ts
+lupdate ../translation.cpp -ts lang_fr_FR.ts
+lupdate ../translation.cpp -ts lang_zh_TW.ts
+
