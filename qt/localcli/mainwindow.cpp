15c25c1f06dd391a32b407794120493b7cda83a2
ericosur <ericosur@mba>
2015-07-13 14:04:56 +0800

    test for QLocale
    Former-commit-id: 2b1dc700e1bd56f72de8103db212b46b3d449749

---
+++ qt/localcli/mainwindow.cpp
@@ -0,0 +1,54 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QLocale>
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    test();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::setLabel1(const QString& s)
+{
+    ui->label_1->setText(s);
+}
+
+void MainWindow::setLabel2(const QString& s)
+{
+    ui->label_2->setText(s);
+}
+
+void MainWindow::test()
+{
+    QLocale::setDefault(QLocale(QLocale::Hebrew, QLocale::Israel));
+    QLocale hebrew; // Constructs a default QLocale
+    QString s1 = hebrew.toString(15714.3, 'e');
+
+    setLabel1(s1);
+
+    bool ok;
+    double d;
+
+    QLocale::setDefault(QLocale::C);
+    d = QString("1234,56").toDouble(&ok);   // ok == false
+    d = QString("1234.56").toDouble(&ok);   // ok == true, d == 1234.56
+
+    QLocale::setDefault(QLocale::German);
+    d = QString("1234,56").toDouble(&ok);   // ok == true, d == 1234.56
+    d = QString("1234.56").toDouble(&ok);   // ok == true, d == 1234.56
+
+    QLocale::setDefault(QLocale(QLocale::English, QLocale::UnitedStates));
+    QString str = QString("%1 %L2 %L3").
+            arg(12345).arg(12345).arg(12345, 0, 16);
+    // str == "12345 12,345 3039"
+    setLabel2(str);
+}
---
+++ qt/localcli/mainwindow.h
@@ -0,0 +1,27 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+    void test();
+
+private:
+    Ui::MainWindow *ui;
+
+    void setLabel1(const QString& s);
+    void setLabel2(const QString& s);
+};
+
+#endif // MAINWINDOW_H
