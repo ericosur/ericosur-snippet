ba244b4ddaec0b1d9776429c3ced9e63d12a0e5f
ericosur <ericosur@mba>
2015-07-15 08:47:26 +0800

    remove MultiLanguageApp/
    Former-commit-id: 546a4c58fdb225d2ab675c7e9cb39fcef973889c

--- qt/MultiLanguageApp/i18n/call_lupdate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-lupdate ../translation.cpp -ts lang_en_US.ts
-lupdate ../translation.cpp -ts lang_ru_RU.ts
-lupdate ../translation.cpp -ts lang_fr_FR.ts
-lupdate ../translation.cpp -ts lang_zh_TW.ts
-
