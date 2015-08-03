9e60038b473ee3859ecb0fcc04758d920a21e30f
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2015-08-03 17:25:21 +0800

    add main() demo function for ucs2_to_utf8.c
    Former-commit-id: 3a37af5ddda029e3d7e62952d0ba2de21a9d7715

--- C/ucs2_to_utf8.c
+++ C/ucs2_to_utf8.c
@@ -1,4 +1,9 @@
-void ucs2_to_utf8(unsigned short /*unicode*/ c, char* utf) /* uses max 4 chars from utf (including the trailing 0) */
+#define MYMAINFUNCTION		1
+
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+
+void ucs2_to_utf8(ushort /*unicode*/ c, uchar* utf) /* uses max 4 chars from utf (including the trailing 0) */
 {
 	if (c < 0x80)
 	{
@@ -21,3 +26,43 @@ void ucs2_to_utf8(unsigned short /*unicode*/ c, char* utf) /* uses max 4 chars f
 	/* UTF-8 is defined for words of up to 31 bits,
 	but we need only 16 bits here */
 }
+
+#if MYMAINFUNCTION
+
+#include <stdio.h>
+
+void clear_utf8(uchar* utf)
+{
+	int i;
+	for (i=0; i<4; i++) {
+		utf[i] = 0;
+	}
+}
+
+void dump_utf8(uchar* utf)
+{
+	int i;
+	for (i=0; i<4; i++) {
+		printf("%02X ", utf[i]);
+	}
+}
+
+void test(ushort ucs)
+{
+	uchar utf[4];
+
+	clear_utf8(utf);
+	ucs2_to_utf8(ucs, utf);
+	dump_utf8(utf);
+	printf("\n");
+}
+
+int main()
+{
+	test(0x32);
+	test(0x074c);
+	test(0x3d5d);
+	test(0x7ac8);
+	return 0;
+}
+#endif
