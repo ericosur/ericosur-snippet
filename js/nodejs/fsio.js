ccd04637b04f0c9ad328cb274cf7e10ad8c40c65
ericosur <ericosur@mba>
2015-08-13 10:01:24 +0800

    update fsio.js
    Former-commit-id: ce7302b5137f4ef04c7c232f903b388f00a5148c

--- js/nodejs/fsio.js
+++ js/nodejs/fsio.js
@@ -1,14 +1,25 @@
 // http://stackoverflow.com/questions/6831918/node-js-read-a-text-file-into-an-array-each-line-an-item-in-the-array
+// use readFileSync()
+
 var fs = require('fs');
 var fname = process.argv[2];
-var lines = fs.readFileSync(fname).toString().split('\n');
 
-// no need toString() if using 'utf8' encoding specified
-// var utf8lines = fs.readFileSync(fname, 'utf8').split('\n');
+if (fname) {
+    var lines = fs.readFileSync(fname).toString().split('\n');
 
-/*
-for (i in lines)  {
-    console.log(lines[i]);
+    // no need toString() if using 'utf8' encoding specified
+    // var utf8lines = fs.readFileSync(fname, 'utf8').split('\n');
+
+    /*
+    // print out line by line
+    for (i in lines)  {
+        console.log(lines[i]);
+    }
+    */
+
+    // how many lines
+    var ln = lines.length - 1;
+    console.log('lines: ' + ln.toString());
+} else {
+    console.log('please specify file name');
 }
-*/
-console.log(lines.length-1);
