0c56de1682ef06341a886f1ff79a688398af5843
ericosur <ericosur@gmail.com>
2015-08-26 12:52:56 +0800

    add unicorn-hat python script to show string and report time
    Former-commit-id: aa9ffa7a0098be34f537e83cc5018223283ca192

---
+++ rpi/unicorn-hat/timestr.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+'''
+This basic example shows use of the Python Pillow library:
+
+sudo pip-3.2 install pillow # or sudo pip install pillow
+
+The tiny 8x8 chars in lofi.png are from Oddball:
+http://forums.tigsource.com/index.php?topic=8834.0
+
+Licensed under Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported License.
+'''
+
+import unicornhat as unicorn
+from PIL import Image
+import signal, numpy, time
+from random import randint
+
+img = Image.open('font.png')
+
+def fillme():
+    for x in range(8):
+        for y in range(8):
+            unicorn.set_pixel(8-x-1, 8-y-1, 
+				randint(0,255), randint(0,255), randint(0,255))
+    unicorn.show()
+
+def draw_char(ch):
+    o_x = ord(ch) % 16
+    o_y = int(ord(ch) / 16)
+    for x in range(8):
+        for y in range(8):
+            pixel = img.getpixel(((o_x*8)+x,(o_y*8)+y))
+            #print(pixel)
+            r, g, b = int(pixel[0]),int(pixel[1]),int(pixel[2])
+            unicorn.set_pixel(8-x-1, 8-y-1, r, g, b)
+    unicorn.show()
+
+def draw_string(str):
+    for ii in range(len(str)):
+        draw_char(str[ii])
+        time.sleep(0.5)
+
+unicorn.rotation(180)
+unicorn.brightness(0.03)
+
+from datetime import datetime
+str = datetime.strftime(datetime.now(), '%H:%M')
+print str
+fillme()
+time.sleep(0.5)
+draw_string(str)
+fillme()
+#fill(randint(0,255),randint(0,255),randint(0,255))
+time.sleep(2)
+
+
