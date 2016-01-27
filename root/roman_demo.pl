560911716bdf1ea3772b29c6a2e155255227f2fa
ericosur <ericosur@gmail.com>
2016-01-28 00:06:30 +0800

    update roman_demo.pl, add roman number of this year
    Former-commit-id: d581aed311324e1fbfc901cbb14e39758513443e

--- root/roman_demo.pl
+++ root/roman_demo.pl
@@ -3,14 +3,38 @@
 # example for Roman <-> arabic numbers
 #
 
+use strict;
 use Roman;
 
-$arabic = 2009;
+sub show($$)
+{
+	my ($lhs, $rhs) = @_;
+	printf("%s => %s\n", $lhs, $rhs);
+}
 
-$roman = Roman($arabic);
-#$roman = roman($arabic);
-printf "%s => %s\n", $arabic, $roman;
+sub getYear()
+{
+	my ($ss,$mm,$hh,$mdd,$mmm,$year,$wday,$yday,$isdst) = localtime();
 
-$roman = "mcmxciii";
-$arabic = arabic($roman) if isroman($roman);
-printf "%s => %s\n", $roman, $arabic;
+	#printf("%04d-%d-%02d    ",($year+1900),($mmm+1),$mdd);
+	#printf("%02d:%02d:%02d\n", $hh, $mm, $ss);
+	return $year+1900;
+}
+
+sub main()
+{
+	my $arabic = 2009;
+	my $roman = Roman($arabic);
+	#$roman = roman($arabic);
+	show($arabic, $roman);
+
+	$roman = "mcmxciii";
+	$arabic = arabic($roman) if isroman($roman);
+	show($roman, $arabic);
+
+	printf("This year: \n");
+	show(getYear(), roman($arabic));
+	show(getYear(), Roman($arabic));
+}
+
+main;
