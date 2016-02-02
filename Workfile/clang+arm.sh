81de0e363d195a21a32e025f52fddb045e3f9103
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2016-02-02 14:51:23 +0800

    add a demo script to use clang and armtool to build a arm binary
    Former-commit-id: 6d56288b2352891b129d4ac8255ca5257db6f27c

---
+++ Workfile/clang+arm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+# this script demos how to use clang as compiler
+# and arm2014.05 as cross compiler
+# to static link a c source
+
+CLANGDIR=$HOME/Downloads/llvm/clang+llvm-3.7.1-x86_64-linux-gnu-ubuntu-14.04
+ARMTOOL=$HOME/arm2014.05
+
+CLANG=$CLANGDIR/bin/clang-3.7
+SYSROOT=$ARMTOOL/arm-none-linux-gnueabi/libc
+BEGINDIR=$ARMTOOL/lib/gcc/arm-none-linux-gnueabi/4.8.3
+
+export PATH=$ARMTOOL/bin:$CLANGDIR/bin:$PATH
+ 
+cd $BEGINDIR
+$CLANG -target arm-none-linux-gnueabi  --sysroot=$SYSROOT \
+  -I${SYSROOT}/include  \
+  -L${BEGINDIR}         \
+  -static      \
+  -o $HOME/fib \
+  $HOME/gcode/snippet/C/fib.c
+
+#  ${ARMTOOL}/lib/gcc/arm-none-linux-gnueabi/4.8.3/crtbegin.o \
+
