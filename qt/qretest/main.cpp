a3c4641a6f09c19cf21f0c5c3099cf14d0b99eee
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2016-04-13 15:18:44 +0800

    add qretest, simple demo about QRegularExpression named capture group
    Former-commit-id: 4831fc6ca1ae3ae68e34cb329e41539ebc07ef63

--- qt/qretest/main.cpp
+++ qt/qretest/main.cpp
@@ -1,8 +1,21 @@
 #include <QCoreApplication>
+#include <QString>
+#include <QRegularExpression>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    return a.exec();
+    QString s = "v=0/t=1/title = hello world/Message=How are you?/Foo3=bar4";
+    QRegularExpression re("(?<key>[A-Za-z][A-Za-z0-9_]*)\\s*=\\s*(?<val>[^/]+)");
+    QRegularExpressionMatchIterator i = re.globalMatch(s);
+    while (i.hasNext()) {
+        QRegularExpressionMatch match = i.next();
+        QString key = match.captured("key");
+        QString val = match.captured("val");
+        qDebug() << "match: " << key << "=" << val;
+    }
+    //return a.exec();
+    return 0;
 }
