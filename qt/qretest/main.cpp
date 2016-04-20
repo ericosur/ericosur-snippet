550a674c203c2baa78e13e36f3ec7588450ee437
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2016-04-20 14:43:26 +0800

    update qretest, add another function to test regexp
    Former-commit-id: aaaa734fc6abada84e630c0a283031e47ac0861f

--- qt/qretest/main.cpp
+++ qt/qretest/main.cpp
@@ -16,12 +16,41 @@ void testRegexp()
     }
 }
 
+/*
+RF,Kpa,230,C,30,OK*4F
+RR,Kpa,230,C,30,OK*4F
+LR,Kpa,230,C,30,OK*4F
+LF,Kpa,230,C,30,OK*4F
+*/
+void test2()
+{
+    QRegularExpression re("(?<pos>[LR][RF]),"
+                          "(?<punit>(Kpa|Psi|Bar)),"
+                          "(?<press>\\d+\\.?\\d*),"
+                          "(?<tunit>(C|F)),"
+                          "(?<temp>\\d+),"
+                          "(?<batt>(OK|LOW))");
+    QString s = "RF,Kpa,230,C,30,OK*4F";
+    QRegularExpressionMatch match = re.match(s);
+    if (match.hasMatch()) {
+        QString pos = match.captured("pos");
+        QString punit = match.captured("punit");
+        QString press = match.captured("press");
+        QString tunit = match.captured("tunit");
+        QString temp = match.captured("temp");
+        QString batt = match.captured("batt");
+        qDebug() << pos << punit << press << tunit << temp << batt;
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+//    QCoreApplication a(argc, argv);
 
     testRegexp();
+    test2();
 
     //return a.exec();
     return 0;
 }
+
