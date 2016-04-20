5a59bf0f866f23767b65960cf6995f8bb8cc1a53
ericosur <ericosur@mba>
2016-04-20 14:14:13 +0800

    update qretest for macosx
    Former-commit-id: 228e0bf955e2c5b044682bbfc040942c474ff3a7

--- qt/qretest/main.cpp
+++ qt/qretest/main.cpp
@@ -3,10 +3,8 @@
 #include <QRegularExpression>
 #include <QDebug>
 
-int main(int argc, char *argv[])
+void testRegexp()
 {
-    QCoreApplication a(argc, argv);
-
     QString s = "v=0/t=1/title = hello world/Message=How are you?/Foo3=bar4";
     QRegularExpression re("(?<key>[A-Za-z][A-Za-z0-9_]*)\\s*=\\s*(?<val>[^/]+)");
     QRegularExpressionMatchIterator i = re.globalMatch(s);
@@ -16,6 +14,14 @@ int main(int argc, char *argv[])
         QString val = match.captured("val");
         qDebug() << "match: " << key << "=" << val;
     }
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    testRegexp();
+
     //return a.exec();
     return 0;
 }
--- qt/qretest/qretest.pro
+++ qt/qretest/qretest.pro
@@ -2,7 +2,7 @@ QT += core
 QT -= gui
 
 CONFIG += c++11
-
+QMAKE_MAC_SDK = macosx10.11
 TARGET = qretest
 CONFIG += console
 CONFIG -= app_bundle
