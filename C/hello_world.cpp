25f484e0537ac768c90745f8dd30d3e9c7aa6e9d
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2016-05-16 14:11:51 +0800

    update hello_world.cpp, add comments and info
    Former-commit-id: 61156ce3cf8737ce39fa14e4d0f9198108c0ec51

--- C/hello_world.cpp
+++ C/hello_world.cpp
@@ -6,13 +6,15 @@ class HelloWorld
 {
 public:
 	HelloWorld()  {
-		cout << "hello world\n";
+		cout << "hello world from class HelloWorld\n";
 	}
 };
 
+// the ctor will be run before main()
 HelloWorld hw;
 
 int main()
 {
+    cout << "hello world from main\n";
 	return 0;
 }
