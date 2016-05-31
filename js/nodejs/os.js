b1c6da057331d41e5b8e47f5d6c8d531f017485e
ericosur <ericosur@mba>
2016-05-31 19:45:34 +0800

    add more os functions
    Former-commit-id: b69691a4105bb4ef6703e5f1ae96466bcb3c55e0

--- js/nodejs/os.js
+++ js/nodejs/os.js
@@ -2,5 +2,22 @@
 // https://nodejs.org/dist/latest-v6.x/docs/api/os.html#os_os_arch
 
 const os = require('os');
-console.log(os.arch());
+const util = require('util');
+
+console.log('arch: ' + os.arch());
+console.log('free: ' + os.freemem());
+
+console.log('os.cpus()');
 console.log(os.cpus());
+// use util.inspect() to dump object
+//console.log('cpus: ' + util.inspect(os.cpus()));
+// use JSON.stringify() to dump object
+//console.log(JSON.stringify(os.cpus(), null, 2));
+
+console.log('os.networkInterfaces()');
+console.log( os.networkInterfaces() );
+
+console.log( os.platform() );
+console.log( os.release() );
+console.log( os.type() );
+console.log( os.uptime() );
