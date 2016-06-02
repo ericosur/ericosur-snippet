887a97cbaa291f619f1f97b79d028e6ea287492f
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2016-06-02 17:11:16 +0800

    update blackfriday.js, use const and dynamic range of year
    Former-commit-id: 5092df227189313160d1dfe9dc189afa0b260f81

--- js/blackfriday.js
+++ js/blackfriday.js
@@ -1,8 +1,11 @@
 // script to calculate black friday
-var tryThirteen = 13;   // black
-var tryFriday = 5;     // Friday
-var y_start = 2015;
-var y_end = y_start + 20;
+const THIRTEEN = 13;   // black
+const FRIDAY = 5;     // Friday
+const RANGE = 5;
+
+var thisYear = (new Date()).getFullYear();
+var y_start = thisYear - RANGE;
+var y_end = thisYear + RANGE;
 
 var testDay = new Date();
 var bfdates = [];
@@ -11,8 +14,8 @@ for (var i=y_start; i<=y_end; i++)  {
     for (var j=0; j<12; j++)  {
         testDay.setYear(i);
         testDay.setMonth(j);
-        testDay.setDate(tryThirteen);
-        if ( testDay.getDay() == tryFriday )  {
+        testDay.setDate(THIRTEEN);
+        if ( testDay.getDay() == FRIDAY )  {
             bfdates.push(j+1);
             //console.log(testDay, "is black Friday");
         }
