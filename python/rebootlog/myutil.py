9f987234a3f44de31a7bc125b9f94f6e8a2c8555
ericosur <ericosur@gmail.com>
2016-06-30 13:01:07 +0800

    change to full path to call external CLI
    Former-commit-id: b69988fdd92640f63dbdc4f0ff2e6b56796d16aa

--- python/rebootlog/myutil.py
+++ python/rebootlog/myutil.py
@@ -7,20 +7,20 @@ import subprocess
 # use python to run:
 # ip addr show eth0 |grep -w inet
 def get_iface_ip(iface):
-    p1 = subprocess.Popen(["ip", "addr", "show", iface], stdout=subprocess.PIPE)
-    p2 = subprocess.Popen(["grep", "-iw", "inet"], stdin=p1.stdout, stdout=subprocess.PIPE)
+    p1 = subprocess.Popen(["/sbin/ip", "addr", "show", iface], stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(["/bin/grep", "-iw", "inet"], stdin=p1.stdout, stdout=subprocess.PIPE)
     p1.stdout.close()
     ip = p2.communicate()[0].decode("utf-8").strip()
     return ip
 
 def get_ssid():
-    p0 = subprocess.Popen(["iwgetid", "-r"], stdout=subprocess.PIPE)
+    p0 = subprocess.Popen(["/sbin/iwgetid", "-r"], stdout=subprocess.PIPE)
 #    p0.stdout.close()
     ssid = p0.communicate()[0].decode("utf-8").strip()
     return ssid
 
 def get_extip():
-	p0 = subprocess.Popen(["wget", "http://ipinfo.io/ip", "-qO", "-"], stdout=subprocess.PIPE)
+	p0 = subprocess.Popen(["/usr/bin/wget", "http://ipinfo.io/ip", "-qO", "-"], stdout=subprocess.PIPE)
 	extip = p0.communicate()[0].decode("utf-8").strip()
 	return extip
 
