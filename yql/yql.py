5da4ee75da89b7b63c947b45d02cf33c4cee2f25
ericosur <ericosur@gmail.com>
2016-07-04 09:02:59 +0800

    update yql.py: python example for yql weather query
    Former-commit-id: 1f22d392e709c46e6649d41ea68f96f62f3ad29d

--- yql/yql.py
+++ yql/yql.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# from: http://57life.blogspot.tw/2012/12/msn-yahoo.html
+# 20070568   台北 (台北市內湖區)
 
 import urllib2, urllib, json
+
+woeid = '20070568'
+
 baseurl = "https://query.yahooapis.com/v1/public/yql?"
-yql_query = "select wind from weather.forecast where woeid=2460286"
+yql_query = "select wind from weather.forecast where woeid=" + woeid
 yql_url = baseurl + urllib.urlencode({'q':yql_query}) + "&format=json"
 result = urllib2.urlopen(yql_url).read()
 data = json.loads(result)
