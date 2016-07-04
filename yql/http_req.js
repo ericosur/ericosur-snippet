b64f2784e66874f23c882984fa9af0218125cfb2
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2016-07-04 09:13:59 +0800

    update http_req.js for add min/max range at request URL
    Former-commit-id: 1a3c1b2b9f7b2d4f1119c7f2fe741775e0c1a623

--- yql/http_req.js
+++ yql/http_req.js
@@ -1,28 +1,39 @@
+// file: http_req.js
+// example to use http(s) request for random numbers
+//
 // example from:
 // https://docs.nodejitsu.com/articles/HTTP/clients/how-to-create-a-HTTP-request/
 //
 
-var http = require('https');
-
 // www.random.org now use https
 //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-var options = {
-  host: 'www.random.org',
-  path: '/integers/?num=17&min=1&max=99&col=1&base=10&format=plain&rnd=new'
-};
 
-callback = function(response) {
-  var str = '';
+(function() {
 
-  //another chunk of data has been recieved, so append it to `str`
-  response.on('data', function (chunk) {
-    str += chunk;
-  });
+  var mymin = '1';
+  var mymax = '199';
 
-  //the whole response has been recieved, so we just print it out here
-  response.on('end', function () {
-    console.log(str);
-  });
-}
+  var options = {
+    host: 'www.random.org',
+    path: '/integers/?num=17&min=' + mymin + '&max=' + mymax
+      + '&col=1&base=10&format=plain&rnd=new'
+  };
 
-http.request(options, callback).end();
+  callback = function(response) {
+    var str = '';
+
+    //another chunk of data has been recieved, so append it to `str`
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
+
+    //the whole response has been recieved, so we just print it out here
+    response.on('end', function () {
+      console.log(str);
+    });
+  }
+
+  var http = require('https');
+  http.request(options, callback).end();
+
+})();
