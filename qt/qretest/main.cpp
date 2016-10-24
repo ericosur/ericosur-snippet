aa7c64b2281b86e11cb2c61fdc377dfa8826c327
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2016-10-24 13:35:15 +0800

    add test for QChar to QString
    Former-commit-id: 283d56c48b6b7a80b353841a40a4a15edeedd065

--- qt/qretest/main.cpp
+++ qt/qretest/main.cpp
@@ -60,14 +60,26 @@ void test3()
     }
 }
 
+void test_qchar()
+{
+	QChar c1 = QChar(0x4e2d);
+	QChar c2 = QChar(0x6587);
+	qDebug() << "digit val:" << c1.digitValue();
+	qDebug() << "unicode:" << QString::number(c1.unicode(), 16);
+	QString s = QString(c1) + c2;
+	qDebug() << "s:" << s;
+}
+
 int main(int argc, char *argv[])
 {
+	Q_UNUSED(argc);
+	Q_UNUSED(argv);
 //    QCoreApplication a(argc, argv);
 
     //testRegexp();
     //test2();
     test3();
-
+    test_qchar();
     //return a.exec();
     return 0;
 }
