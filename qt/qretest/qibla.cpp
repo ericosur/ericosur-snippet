0fccdf619aa1ae3583c0c172da00fe44b134cfea
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2016-11-04 09:18:51 +0800

    use constants while calculating qibla
    Former-commit-id: c0ef80ddfc1864e054cdcde3d6e25cd560611cac

--- qt/qretest/qibla.cpp
+++ qt/qretest/qibla.cpp
@@ -3,10 +3,13 @@
  */
 #include <math.h>
 
+static const double DEG2RAD = 0.017453292519943295769236907684886;
+static const double MECCA_LONGITUDE = 39.8230*DEG2RAD; // 39.8230E
+static const double MECCA_LATITUDE = 21.42330*DEG2RAD; // 21.42330N
 double get_qibla_angle(double longitude, double latitude)
 {
-    double m = sin(longitude - 39.8230);
-    double n = cos(latitude) * tan(21.42330) - sin(latitude) * cos(longitude - 39.8230);
-    double q = atan(m/n);
+    double m = sin(longitude*DEG2RAD - MECCA_LONGITUDE);
+    double n = cos(latitude*DEG2RAD) * tan(MECCA_LATITUDE) - sin(latitude*DEG2RAD) * cos(longitude*DEG2RAD - MECCA_LONGITUDE);
+    double q = atan(m/n) / DEG2RAD;
     return q;
 }
