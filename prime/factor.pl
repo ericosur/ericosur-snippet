a55f3355010219d39ca8420261383cd95e52fc3a
ericosur <ericosur@mba>
2016-11-29 09:24:04 +0800

    update factor.pl, group statements into subroutine
    Former-commit-id: 61cb7700b50419a3f75fc64b6722bb6a1402c373

--- prime/factor.pl
+++ prime/factor.pl
@@ -4,54 +4,62 @@
 use strict;
 
 my @primes;
-
-my $ifile = q(basic_prime.txt);
-die "need $ifile\n" if not -e $ifile;
-@primes = ();
-open my $ifh, $ifile or die;
-while (<$ifh>)  {
-	s/[\r\n]//;
-	push @primes, $_;
-}
-close $ifh;
-
 my @factor = ();
 my %ff = ();
 my $cnt = 0;
 
+sub main()
+{
+    my $ifile = q(basic_prime.txt);
+    die "need $ifile\n" if not -e $ifile;
+    @primes = ();
+    open my $ifh, $ifile or die;
+    while (<$ifh>)  {
+        s/[\r\n]//;
+        push @primes, $_;
+    }
+    close $ifh;
+
+    if (@ARGV) {
+        factorize($ARGV[0]);
+    } else {
+        factorize(100);
+    }
+
+    print '-' x 40,"\n";
+    foreach my $kk (keys %ff)  {
+        printf "%d ^ %d\n", $kk, $ff{$kk};
+    }
+
+}
+
 sub factorize($)
 {
-	my $val = shift;
-#	my $sq = sqrt($val);
+    my $val = shift;
+#   my $sq = sqrt($val);
 
-	if ($val <= 1)  {
-		return;
-	}
-	++ $cnt;
-	print ">>>>>>> $cnt, arg=$val\n";
+    if ($val <= 1)  {
+        return;
+    }
+    ++ $cnt;
+    #print ">>>>>>> $cnt, arg=$val\n";
 
-	foreach my $nn (@primes)  {
-		if ($val == $nn)  {
-			return;
-		}
-		if (($val % $nn) == 0)  {
-			push @factor, $nn;
-			++ $ff{$nn};
-			#print $val," / ", $nn,"\n";
-			$val = $val / $nn;
-			#print $val,"\n";
-			if ($val != 1)  {
-				print "factorize($val) >>>\n";
-				factorize($val);
-			}
-		}
-	}
+    foreach my $nn (@primes)  {
+        if ($val == $nn)  {
+            return;
+        }
+        if (($val % $nn) == 0)  {
+            push @factor, $nn;
+            ++ $ff{$nn};
+            #print $val," / ", $nn,"\n";
+            $val = $val / $nn;
+            #print $val,"\n";
+            if ($val != 1)  {
+                print "factorize($val) >>>\n";
+                factorize($val);
+            }
+        }
+    }
 }
 
-
-factorize(100);
-
-print '-' x 40,"\n";
-foreach my $kk (keys %ff)  {
-	printf "%d ^ %d\n", $kk, $ff{$kk};
-}
+main;
