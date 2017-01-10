54a5b1aa61d8a421631819e29d83916d0008f228
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2017-01-10 09:32:32 +0800

    lorem: add sample to get screen geometry
    Former-commit-id: 99a79c441c9cce4cb26f0a8b2fe86ba2e08337f9

--- qt/lorem/main.cpp
+++ qt/lorem/main.cpp
@@ -1,11 +1,22 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QtQml>
+#include <QDebug>
+#include <QScreen>
 #include "loadtext.h"
+
+void show_geometry()
+{
+    QString msg = QString("width: %1, height: %2").
+        arg(QGuiApplication::primaryScreen()->geometry().width()).
+        arg(QGuiApplication::primaryScreen()->geometry().height());
+    qDebug() << Q_FUNC_INFO << msg;
+}
+
 
 int main(int argc, char *argv[])
 {
-    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+    //QTextCodec *codec = QTextCodec::codecForName("UTF-8");
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
     LoadText loadtext;
@@ -13,6 +24,8 @@ int main(int argc, char *argv[])
     engine.rootContext()->setContextProperty("mytext", &loadtext);
     qmlRegisterType<LoadText>("com.rasmus", 1, 0, "LoadText");
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    show_geometry();
 
     return app.exec();
 }
