cd3ce23ed585cb38dfe83113098976bbcf33a11b
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2017-02-13 16:48:56 +0800

    add mediainfo, note to cross copmile mediainfo from x86 for ARM
    Former-commit-id: e0705521603850af2bb8352f23bc040349d6c3a1

---
+++ rpi/mediainfo/README.md
@@ -0,0 +1,13 @@
+HOWTO cross compile mediainfo for raspberry pi
+==============================================
+
+source for mediainfo:
+https://mediaarea.net/download/binary/mediainfo/0.7.92.1/MediaInfo_CLI_0.7.92.1_GNU_FromSource.tar.bz2
+
+toolchain for raspberry pi:
+https://github.com/raspberrypi/tools.git
+
+first build libz for arm
+
+myenv.sh
+a.sh
---
+++ rpi/mediainfo/a.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TOP=/home/rasmus/src/github/rpi-git/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian
+CC=$TOP/bin/arm-linux-gnueabihf-gcc
+CXX=$TOP/bin/arm-linux-gnueabihf-g++
+LD=$TOP/bin/arm-linux-gnueabihf-ld
+CFLAGS=-I$TOP/arm-linux-gnueabihf/include
+LDFLAGS=-L$TOP/arm-linux-gnueabihf/lib
+PATH=$TOP/bin:$PATH
+./CLI_Compile.sh --host=arm-linux-gnueabihf \
+  --prefix=$TOP
+
