6f82103bd7e2003e583c2c83904bd31b9fec8526
ericosur <ericosur@gmail.com>
2017-02-25 21:57:59 +0800

    add sha3.pl, demo for Digest::SHA3
    Former-commit-id: c5d2f891a54aafed0c43fb738dc15b7544feb93c

---
+++ root/sha3.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use Digest::SHA3 qw(sha3_224_hex sha3_256_hex sha3_384_base64 sha3_512_hex);
+
+
+sub show($)
+{
+    my $d = shift;
+    print $d,"\n";
+}
+
+$date = "hello world!";
+
+$digest = sha3_224_hex($data);
+show($digest);
+$digest = sha3_256_hex($data);
+show($digest);
+$digest = sha3_384_base64($data);
+show($digest);
+$digest = sha3_512_hex($data);
+show($digest);
+# $digest = sha3_0_hex($data);
+# show($digest);
