43ef4d9c4ddbce00c7d598fe85598032f7956a17
ericosur <ericosur@gmail.com>
2017-04-21 13:50:28 +0800

    update deck for output and readme, remove output data file from git
    Former-commit-id: 186f914e450d486d87d927d387cb071a4451f1aa

--- Topics/deck/17poker.py
+++ Topics/deck/17poker.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-
+# -*- coding: utf-8 -*-
+#
 # use shuffle_array() in fisher_yates_shuffle.py
 from fisher_yates_shuffle import shuffle_array
 
@@ -13,53 +14,62 @@ Simply init a pile of cards by order and deal 5 cards to 4 players.
 
 
 def init_deck(foo):
-	card = []
-	for suit in xrange(1,5):
-		for point in xrange(1,14):
-			card = [suit, point]
-			foo.append(card)
+    card = []
+    for suit in xrange(1,5):
+        for point in xrange(1,14):
+            card = [suit, point]
+            foo.append(card)
 
 def show_deck(foo):
-	for cc in foo:
-		dir(cc)
-		print show_one_card(cc),
+    for cc in foo:
+        dir(cc)
+        print show_one_card(cc),
 
 def show_one_card(card):
-	'''
-	'x': joker, 'S': spade, 'H': heart, 'D': diamond, 'C': club
-	'''
-	#print 'card', card[0]
-	suit = ['x','S','H','D','C']
-	return '(' + suit[card[0]] + str(card[1]) + ')'
+    '''
+    'x': joker, 'S': spade, 'H': heart, 'D': diamond, 'C': club
+    '''
+    #print 'card', card[0]
+    suit0 = ['x','S','H','D','C']
+    suit = ['x', u"\u2660", u"\u2665", u"\u2666", u"\u2663"]
+    result = suit0[card[0]] + suit[card[0]] + ' ' + str(card[1])
+    return result
+    #return '(' + suit[card[0]] + str(card[1]) + ')'
 
 '''
 def deal_cards(pile, num):
-	deal_cards = []
-	for i in xrange(num):
-		deal_cards.append(pile.pop())
-	return deal_cards
+    deal_cards = []
+    for i in xrange(num):
+        deal_cards.append(pile.pop())
+    return deal_cards
 '''
 
-mycards = []
-init_deck(mycards)
-#show_deck(mycards)
+if __name__ == '__main__':
+    perform_shuffle = False
+    mycards = []
+    init_deck(mycards)
+    #show_deck(mycards)
 
-# show total cards
-'''
-print '-------------------'
-#shuffle_array(mycards)
-show_deck(mycards)
-print '-------------------'
-'''
+    # show total cards
+    '''
+    print '-------------------'
+    #shuffle_array(mycards)
+    show_deck(mycards)
+    print '-------------------'
+    '''
+    if perform_shuffle:
+        shuffle_array(mycards)
+    else:
+        print('WARNING: no shuffled')
 
-players = [[],[],[],[]]
-for pp in xrange(5):	# deal 5 cards
-	one = []
-	for cc in xrange(4):	# here are 4 players
-		#print 'cc',cc
-		players[cc].append( mycards.pop() )
+    players = [[],[],[],[]]
+    for pp in xrange(5):    # deal 5 cards
+        one = []
+        for cc in xrange(4):    # here are 4 players
+            #print 'cc',cc
+            players[cc].append( mycards.pop() )
 
-for pp in players:
-	show_deck(pp)
-	print
+    for pp in players:
+        show_deck(pp)
+        print
 
--- Topics/deck/cmb.py
+++ Topics/deck/cmb.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python
 
 '''
-In the game '17 pokers', combinations could be described as 
-C(17, 5). This script uses ''itertools.combinations'' to list 
+In the game '17 pokers', combinations could be described as
+C(17, 5). This script uses ''itertools.combinations'' to list
 all possible combination.
 '''
 from itertools import combinations
 
-# a = [0, 1, 2, ..., 16]
-a = range(17)
+print_to_stdout = 0
 
-# ii = C(17, 5)
-ii = combinations(a, 5)
+def output_list_to_file(output_file, cclist):
+    with  open(output_file, "w") as text_file:
+        cnt = 0
+        for cc in cclist:
+            if print_to_stdout:
+               print cc
+            text_file.write(str(cc)+'\n')
+            cnt = cnt + 1
 
-cnt = 0
-for cc in ii:
-	print cc
-	cnt = cnt + 1
+    print("output to file {0}, total {1} items".format(output_file, cnt))
 
-#print "cnt = ", cnt
 
+if __name__ == '__main__':
+    output_file = 'all-cmb.txt'
+
+    # a = [0, 1, 2, ..., 16]
+    a = range(17)
+
+    # ii = C(17, 5)
+    ii = combinations(a, 5)
+
+    output_list_to_file(output_file, ii)
