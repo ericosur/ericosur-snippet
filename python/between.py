50f3bade3b14935ce0fcd2f3ae4812337b99c818
ericosur <ericosur@gmail.com>
2017-04-26 14:57:56 +0800

    update between.py for more details
    Former-commit-id: fc20fdd234e0eeb678ad66b10782438a1a7a5a62

--- python/between.py
+++ python/between.py
@@ -3,7 +3,7 @@
 import datetime
 
 #ww = [8, 30]
-ww = [9, 39]
+ww = [8, 21]
 # here is time I enter workspace
 from_time = datetime.datetime.now().replace(hour=ww[0],minute=ww[1],second=0,microsecond=0)
 print from_time
@@ -12,6 +12,9 @@ print from_time
 now_time = datetime.datetime.now().replace(microsecond=0)
 print now_time
 
+most_early_time = datetime.datetime.now().replace(hour=17,minute=30,second=0,microsecond=0)
+#print most_early_time
+
 # need more than work_hour
 work_hour = datetime.timedelta(hours=9)
 
@@ -19,4 +22,8 @@ work_hour = datetime.timedelta(hours=9)
 if now_time - from_time > work_hour:
     print "ok, long enough"
 else:
-    print "nok, should be:", from_time + work_hour
+    real_work_hour = from_time + work_hour
+    if real_work_hour < most_early_time:
+        real_work_hour = most_early_time
+    print "nok, should be:", real_work_hour
+    print "need hour:", most_early_time - now_time
