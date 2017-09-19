20dce8569494212b85378b487f66e2bcf7c932f4
ericosur <ericosur@gmail.com>
2017-09-19 15:07:30 +0800

    add sort-dict-by-value sample
    Former-commit-id: 4ca788a0f4f23c64a62fd0343facac61a404b81b

---
+++ python/sort-dict-by-value.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+#
+# from:
+# http://stackoverflow.com/questions/613183/sort-a-python-dictionary-by-value
+#
+
+import operator
+x = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+
+# sort by values of dict
+sorted_x = sorted(x.items(), key=operator.itemgetter(1))
+print("x:{0}".format(x))
+print("sorted by values: {0}".format(sorted_x))
+
+# sort by keys of dict
+#x = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+sorted_x = sorted(x.items(), key=operator.itemgetter(0))
+print("sorted by keys: {0}".format(sorted_x))
