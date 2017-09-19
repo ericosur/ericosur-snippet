d01aa9f0867f3163e6c2e9cc7d5133d56a385326
ericosur <ericosur@gmail.com>
2017-09-19 11:43:53 +0800

    update dict_test.py add format print and lambda sample
    Former-commit-id: 30a286792b213aedf9bdce38fe227396e99a20e8

--- python/dict_test.py
+++ python/dict_test.py
@@ -3,29 +3,37 @@
 # python dictionary
 
 def print_dict(**foo):
-	for x in foo:
-		print x,"=>",foo[x]
-
-rec = {	'name': {'first': 'Brown', 'last': 'Smith'},
-		'job': ['dev', 'mgr'],
-		'age': 40.5}
-print("print rec: ")
-print_dict(**rec)
-		
-print("rec['name']: ", rec['name'])
-print("rec['name']['first']:", rec['name']['first'])
-print("rec['name']['last']", rec['name']['last'])
-
-rec['job'].append('foo')
-
-# cannot pass at python 3.0
-print(('age' in rec))
-
+    for x in foo:
+        print x,"=>",foo[x]
 
 ''' map() test '''
 def cube(x):
-	return x*x*x
+    return x*x*x
 
-mm = range(5, 9)
-mm = map(cube, mm)
-print "map(): ", mm
+
+if __name__ == '__main__':
+    rec = { 'name': {'first': 'Brown', 'last': 'Smith'},
+            'job': ['dev', 'mgr'],
+            'age': 40.5}
+    print("print rec ==> ")
+    print_dict(**rec)
+
+    print('''access rec[] ==>
+rec['name']: {0}
+rec['name']['first']: {1}
+rec['name']['last']: {2}
+'''.format(rec['name'], rec['name']['first'], rec['name']['last']))
+
+    rec['job'].append('foo')
+
+    # cannot pass at python 3.0
+    print("if age in rec? {0}".format('age' in rec))
+
+    # demo of map()
+    items = [11, 13, 17, 19, 23, 29]
+    cubed = map(cube, items)
+    print "map(): ", cubed, type(cubed)
+
+    items = (31, 37, 41, 43, 47, 53)
+    squared = map(lambda x: x**2, items)
+    print "map(): ", squared, type(squared)
