fe7b97b3ac686d6512437ce8c08bd9616e7dac05
ericosur <ericosur@gmail.com>
2017-11-29 17:18:22 +0800

    add three python script using plot module to draw box plots
    Former-commit-id: 3653425b19e16d7471417efa2b87bf79c32463ea

---
+++ python/plot/box.py
@@ -0,0 +1,19 @@
+#!/Users/ericosur/anaconda/bin/python
+
+from plotly.offline import plot
+import plotly.graph_objs as go
+
+#import numpy as np
+
+#y0 = np.random.randn(40)-1.5
+y0 = [0.22,-0.87,-2.39,-1.79,0.37,-1.54,1.28,-0.31,-0.74,1.72,0.38,-0.17,-0.62,-1.10,0.30,0.15,2.30,0.19,-0.50,-0.09]
+#y1 = np.random.randn(40)+3.1415926535897932
+y1= [-5.13,-2.19,-2.43,-3.83,0.50,-3.25,4.32,1.63,5.18,-0.43,7.11,4.87,-3.10,-5.81,3.76,6.31,2.58,0.07,5.76,3.50]
+trace0 = go.Box(
+    y=y0
+)
+trace1 = go.Box(
+    y=y1
+)
+data = [trace0, trace1]
+plot(data, filename='boxplot.html')
---
+++ python/plot/dots.py
@@ -0,0 +1,57 @@
+#!/Users/ericosur/anaconda/bin/python
+
+# Get this figure: fig = py.get_figure("https://plot.ly/~foo12345/6/")
+# Get this figure's data: data = py.get_figure("https://plot.ly/~foo12345/6/").get_data()
+# Add data to this figure: py.plot(Data([Scatter(x=[1, 2], y=[2, 3])]), filename ="plot from API (6)", fileopt="extend")
+# Get y data of first trace: y1 = py.get_figure("https://plot.ly/~foo12345/6/").get_data()[0]["y"]
+
+# Get figure documentation: https://plot.ly/python/get-requests/
+# Add data documentation: https://plot.ly/python/file-options/
+
+# If you're using unicode in your file, you may need to specify the encoding.
+# You can reproduce this figure in Python with the following code!
+
+# Learn about API authentication here: https://plot.ly/python/getting-started
+# Find your api_key here: https://plot.ly/settings/api
+
+import plotly.offline as offplot
+#import plotly.plotly as py
+from plotly.graph_objs import *
+import numpy as np
+
+#py.sign_in('username', 'api_key')
+#
+y1 = np.random.randn(100)*10+22
+y2 = np.random.randn(100)*33
+
+trace1 = {
+  "y": y1,
+  "boxpoints": "all",
+  "jitter": 0.3,
+  "name": 'blue',
+  "type": "box"
+}
+trace2 = {
+  "y": y2,
+  "boxpoints": "all",
+  "jitter": 0.3,
+  "name": 'orange',
+  "type": "box"
+}
+data = Data([trace1, trace2])
+layout = {
+  "plot_bgcolor": "rgb(233,233,233)",
+  "showlegend": False,
+  "xaxis": {
+    "showgrid": False,
+    "title": "two series",
+    "zeroline": False
+  },
+  "yaxis": {
+    "gridcolor": "white",
+    "title": "Age",
+    "zeroline": False
+  }
+}
+fig = Figure(data=data, layout=layout)
+plot_url = offplot.plot(fig, filename='class.html')
