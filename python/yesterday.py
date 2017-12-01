9aef41fcd0f681ca1ad0c2e5a48ea83fb3bfb12a
ericosur <ericosur@gmail.com>
2017-12-01 18:25:55 +0800

    update yesterday.py for detailed output
    Former-commit-id: 2b28fac15a38e1d2c6dccd7fd4da26648bc787e2

--- python/yesterday.py
+++ python/yesterday.py
@@ -6,12 +6,12 @@ import time
 # get current time
 foo = datetime.fromtimestamp(time.time())
 foo = foo.replace(microsecond=0)  # remove microsecond part
-print foo;
+print("current time from time stamp: {0}".format(foo))
 
 # define offset 1 day
 offset = timedelta(days=1)
-print offset
+print("timedelta: {0}".format(offset))
 
 # get yesterday
 foo -= offset
-print foo
+print('current - timedelta = "{0}"'.format(foo))
