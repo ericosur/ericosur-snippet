93022bbce6c7611bcc726b666b60fddf5830cbfe
ericosur <ericosur@gmail.com>
2018-01-23 17:09:58 +0800

    update split.py and its own settings
    Former-commit-id: f36924c8aade17c8c220ae30f628d1025d9be57c

--- opencv/python/split.py
+++ opencv/python/split.py
@@ -5,18 +5,23 @@ import cv2
 import numpy as np
 import myutil
 
+thumbnail_height = 300
+thumbnail_width = 300
+
 def output_img(file, img):
-    print("output to: {}".format(file))
-    cv2.imwrite(file, img)
+    #print("output to: {}".format(file))
+    #cv2.imwrite(file, img)
+    cv2.imshow(file, img)
 
 def preprocess(src):
-    img = cv2.resize(src, (300,300))
+    img = cv2.resize(src, (thumbnail_width, thumbnail_height))
     img = img - 127.5
     #img = img * 0.007843
     return img
 
 def apply_mean(src):
-    b,g,r = cv2.split(src)
+    img = cv2.resize(src, (thumbnail_width, thumbnail_height))
+    b,g,r = cv2.split(img)
     #104,117,123
     b = np.subtract(b, 104)
     g = np.subtract(g, 117)
@@ -25,6 +30,26 @@ def apply_mean(src):
     img = cv2.merge([b, g, r])
     return img
 
+def setup_windows():
+    cv2.namedWindow('red')
+    cv2.moveWindow('red', 0, 0)
+    cv2.namedWindow('green')
+    cv2.moveWindow('green', thumbnail_width, 0)
+    cv2.namedWindow('blue')
+    cv2.moveWindow('blue', thumbnail_width*2, 0)
+    cv2.namedWindow('merged.png')
+    cv2.moveWindow('merged.png', 0, thumbnail_height+50)
+    cv2.namedWindow('preproc.png')
+    cv2.moveWindow('preproc.png', thumbnail_width, thumbnail_height+50)
+
+
+def split_and_show(img):
+    img = cv2.resize(img, (thumbnail_width, thumbnail_height))
+    b,g,r = cv2.split(img)
+    zeros = np.zeros(img.shape[:2], dtype=img.dtype)
+    cv2.imshow('red', cv2.merge([zeros, zeros, r]))
+    cv2.imshow('blue', cv2.merge([b, zeros, zeros]))
+    cv2.imshow('green', cv2.merge([zeros, g, zeros]))
 
 if __name__ == "__main__":
     app_name = 'split.py'
@@ -37,7 +62,10 @@ if __name__ == "__main__":
         print("image not found {}!".format(image))
         exit(1)
 
+    setup_windows()
+
     img = cv2.imread(image)
+    split_and_show(img)
 
     mrg = apply_mean(img)
     output_img('merged.png', mrg)
@@ -45,5 +73,6 @@ if __name__ == "__main__":
     newimg = preprocess(img)
     output_img('preproc.png', newimg)
 
+    cv2.waitKey()
     # When everything done, release the capture
     cv2.destroyAllWindows()
