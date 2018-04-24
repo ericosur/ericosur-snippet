ec2826ac987fbea61ce11b73859ff4cef8304929
ericosur <ericosur@gmail.com>
2018-04-24 11:06:56 +0800

    add pp.py to list sys.path
    Former-commit-id: f2bfa0711193427d76980cf7ef5155980c9172ca

---
+++ python/pp.py
@@ -0,0 +1,13 @@
+#
+# pp.py will list all sys.path
+# this script will search for modules from these path
+#
+#
+# you may run it via python2 or python3 (or anaconda python)
+# to see the difference
+#
+
+import sys
+
+for idx, pp in enumerate(sys.path):
+    print('[{0:02d}]: {1}'.format(idx, pp))
