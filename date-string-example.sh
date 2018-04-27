328916acac70b07b9c15f33942ed076a9643b1cf
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2018-04-27 14:58:57 +0800

    add showing date string with different options
    Former-commit-id: 2a602ebe741cd5b30d24172ca2a018c8778ae909

--- date-string-example.sh
+++ date-string-example.sh
@@ -2,7 +2,11 @@
 
 # get epoch from date
 testdate=$(date +%s)
-echo $testdate
+echo "epoch: ${testdate}"
 
 # convert epoch to local readable date/time string
-date -d @$testdate
+# it may show date string to follow system locale settings
+date -d @${testdate}
+
+# -R, --rfc-2822 output date and time in RFC 2822 format
+date -R -d @${testdate}
