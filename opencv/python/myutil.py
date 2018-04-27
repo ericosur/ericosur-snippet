65eac68c62c5c4175f79de10c3390d21ae652289
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2018-04-27 14:33:58 +0800

    update myutil.py, move cv function to cvutil.py
    * keep json reading functions
    * add function with default value if not found
    
    
    Former-commit-id: 01b5837b7e73c0a439737d23984d71ea8a27bcc6

--- opencv/python/myutil.py
+++ opencv/python/myutil.py
@@ -1,8 +1,50 @@
+import os
 import json
 
-def read_setting(filename):
-    #home = os.environ.get('HOME')
+def read_jsonfile(fn):
+    '''
+    specify json filename and return whole json object
+    '''
+    print('load json from {}'.format(fn))
+    if not os.path.exists(fn):
+        print('file not found')
+        return None
     # read from json file
-    with open(filename) as sec_file:
-        data = json.load(sec_file)
+
+    # method #1
+#    with open(filename) as sec_file:
+#        data = json.load(sec_file)
+
+    # kiss method #2
+    data = json.load(open(fn))
+
     return data
+
+# in order not to break client scripts
+def read_setting(fn):
+    return read_jsonfile(fn)
+
+def request_value(data, key, default_value = None):
+    '''
+    given json object and request key, if key does not exist, return None
+    if default_value is specified, will return default value if value not
+    found
+    '''
+    ret = default_value
+    try:
+        ret = data[key]
+    except:
+        print('key "{}" not found, will use default_value'.format(key))
+    return ret
+
+
+def main():
+    file = 'nosuchfile'
+    data = read_jsonfile(file)
+    if data == None:
+        return
+
+
+
+if __name__ == '__main__':
+    main()
