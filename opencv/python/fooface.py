c19eb532c4b14a7282647058c7a7eca382beaadc
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2018-06-15 16:06:33 +0800

    add fooface.py, capture from camera and perform face detection/landmarks

---
+++ opencv/python/fooface.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python
+# The contents of this file are in the public domain. See LICENSE_FOR_EXAMPLE_PROGRAMS.txt
+#
+#   This example program shows how to find frontal human faces in a webcam stream using OpenCV.
+#   It is also meant to demonstrate that rgb images from Dlib can be used with opencv by just
+#   swapping the Red and Blue channels.
+#
+#   You can run this program and see the detections from your webcam by executing the
+#   following command:
+#       ./opencv_face_detection.py
+#
+#   This face detector is made using the now classic Histogram of Oriented
+#   Gradients (HOG) feature combined with a linear classifier, an image
+#   pyramid, and sliding window detection scheme.  This type of object detector
+#   is fairly general and capable of detecting many types of semi-rigid objects
+#   in addition to human faces.  Therefore, if you are interested in making
+#   your own object detectors then read the train_object_detector.py example
+#   program.
+#
+#
+# COMPILING/INSTALLING THE DLIB PYTHON INTERFACE
+#   You can install dlib using the command:
+#       pip install dlib
+#
+#   Alternatively, if you want to compile dlib yourself then go into the dlib
+#   root folder and run:
+#       python setup.py install
+#
+#   Compiling dlib should work on any operating system so long as you have
+#   CMake installed.  On Ubuntu, this can be done easily by running the
+#   command:
+#       sudo apt-get install cmake
+#
+#   Also note that this example requires Numpy which can be installed
+#   via the command:
+#       pip install numpy
+
+# ref: http://dlib.net/face_landmark_detection.py.html
+# ref: opencv_webcam_face_detection.py
+
+import sys
+import dlib
+import cv2
+import numpy as np
+
+class Foo:
+    def __init__(self):
+        self.font = cv2.FONT_HERSHEY_SIMPLEX
+        #self.tracker = dlib.correlation_tracker()
+        #self.win = dlib.image_window()
+        self.detector = dlib.get_frontal_face_detector()
+
+        # dat download from: http://dlib.net/face_landmark_detection.py.html
+        self.predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+
+
+    '''
+    def tracking(self):
+        # Create the correlation tracker - the object needs to be initialized
+        # before it can be used
+
+        # We will track the frames as we load them off of disk
+        for k, f in enumerate(sorted(glob.glob(os.path.join(video_folder, "*.jpg")))):
+            print("Processing Frame {}".format(k))
+            img = dlib.load_rgb_image(f)
+
+            # We need to initialize the tracker on the first frame
+            if k == 0:
+                # Start a track on the juice box. If you look at the first frame you
+                # will see that the juice box is contained within the bounding
+                # box (74, 67, 112, 153).
+                tracker.start_track(img, dlib.rectangle(74, 67, 112, 153))
+            else:
+                # Else we just attempt to track from the previous frame
+                tracker.update(img)
+
+            win.clear_overlay()
+            win.set_image(img)
+            win.add_overlay(tracker.get_position())
+    '''
+
+    def action(self):
+        cam = cv2.VideoCapture(0)
+        color_green = (0,255,0)
+        line_width = 1
+        while True:
+            ret_val, img = cam.read()
+            rgb_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            #gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            dets = self.detector(rgb_image)
+            if len(dets) == 0:
+
+                cv2.putText(img, 'no dets',(10, 40), self.font, 1, (127,0,255), 2, cv2.LINE_AA)
+            else:
+                for det in dets:
+                    cv2.rectangle(img,(det.left(), det.top()), (det.right(), det.bottom()), color_green, line_width)
+                    landmarks = np.matrix([[p.x, p.y] for p in self.predictor(img, det).parts()])
+                    for idx, point in enumerate(landmarks):
+                        pos = (point[0, 0], point[0, 1])
+                        cv2.circle(img, pos, 2, color=(0, 255, 0))
+                        cv2.putText(img, str(idx + 1), pos, self.font, 0.2, (0, 0, 255), 1, cv2.LINE_AA)
+
+            cv2.imshow('webcam', img)
+            if cv2.waitKey(1) == 27:
+                break  # esc to quit
+        cv2.destroyAllWindows()
+
+
+def main():
+    foo = Foo()
+    foo.action()
+
+if __name__ == '__main__':
+    main()
