d2b9754cb85ed4ea132b8d7ebe95ddef36d20707
ericosur <ericosur@gmail.com>
2018-07-17 14:08:15 +0800

    update black_friday.py, more structure

--- python/black_friday.py
+++ python/black_friday.py
@@ -5,14 +5,29 @@
 
 import datetime
 
-FROM_YEAR = 2010
-TO_YEAR = 2020
-MIN_MONTH = 1
-MAX_MONTH = 12
-WEEKDAY_FRIDAY = 4	# 0:MON, 1:TUE, 2:WED, 3:THU, 4:FRI, 5:SAT, 6:SUN
 
-for yr in xrange(FROM_YEAR, TO_YEAR+1):
-	for mnth in xrange(MIN_MONTH, MAX_MONTH+1):
-		dd = datetime.datetime(yr, mnth, 13)
-		if dd.weekday() == WEEKDAY_FRIDAY:
-			print dd.isoformat(), "is Black Friday"
+def get_blackfriday(from_year, to_year):
+    MIN_MONTH = 1
+    MAX_MONTH = 12
+    # 0:MON, 1:TUE, 2:WED, 3:THU, 4:FRI, 5:SAT, 6:SUN
+    WEEKDAY_FRIDAY = 4
+
+    for yr in xrange(from_year, to_year+1):
+        for mnth in xrange(MIN_MONTH, MAX_MONTH+1):
+            dd = datetime.datetime(yr, mnth, 13)
+            if dd.weekday() == WEEKDAY_FRIDAY:
+                print get_date_str(dd), "is Black Friday"
+
+def get_date_str(dd):
+    str = dd.strftime("%d %b %Y")
+    return str
+
+def main():
+    today = datetime.date.today()
+    from_y = today.year
+    get_blackfriday(from_y, from_y+15)
+
+
+if __name__ == '__main__':
+    #print(get_date_str(datetime.date.today()))
+    main()
