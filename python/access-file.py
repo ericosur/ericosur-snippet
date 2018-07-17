770e936f71336208da7964b0cd18440327b6b9a2
ericosur <ericosur@gmail.com>
2018-07-17 14:07:11 +0800

    update access-file.py, add more output for description

--- python/access-file.py
+++ python/access-file.py
@@ -9,9 +9,11 @@ def test_file(fn):
         fp = open(fn)
     except IOError as e:
         if e.errno == errno.EACCES:
+            print('errno: %s' % 'EACCES')
             return False
         # other than permission error
         if e.errno == errno.ENOENT:
+            print('errno: %s' % 'ENOENT')
             return False
         pass
     else:
@@ -21,6 +23,9 @@ def main(argv):
     fn = '/dev/shm/reid.jpg'
     if len(argv) > 1:
         fn = argv[1]
+    else:
+        print('specify an image file path, using default: %s' % fn)
+
     if test_file(fn):
         img = cv2.imread(fn)
         cv2.imshow('test', img)
