cfae110253166c10d8f892de100008bc448bcb1d
ericosur <ericosur@gmail.com>
2018-09-10 16:15:53 +0800

    update mycsv.py, more detailed messages

--- python/mycsv.py
+++ python/mycsv.py
@@ -7,20 +7,23 @@ from __future__ import print_function
 import csv
 import random
 
+COUNT = 10
+
 def get_filled_array():
     '''get ten random numbers'''
-    ten = 10
     arr = []
-    for _ in xrange(ten):
+    for _ in range(COUNT):
         arr.append(random.randint(0, 99))
     return arr
 
 def main():
     '''main function'''
-    with open('eggs.csv', 'wb') as csvfile:
+    FILEN = 'eggs.csv'
+    with open(FILEN, 'wb') as csvfile:
         sw = csv.writer(csvfile, delimiter=',',
                         quotechar='"', quoting=csv.QUOTE_ALL)
         sw.writerow(get_filled_array())
+    print('output {} random numbers to {}'.format(COUNT, FILEN))
 
 if __name__ == '__main__':
     main()
