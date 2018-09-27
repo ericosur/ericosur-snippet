329aa4aa95e7dc0cf50b950d63db2aa348d3b01b
ericosur <ericosur@gmail.com>
2018-09-27 11:27:10 +0800

    add driving_data.py, and update readme for using this
    will show mean and median value from table.

--- python/pandas/Readme.md
+++ python/pandas/Readme.md
@@ -12,3 +12,16 @@ install **pandas** module if needed
 ```
 $ sudo -H pip install pandas
 ```
+
+### driving_data.py ###
+
+This script uses *__*driving_data.csv*__* which is exported from [google drive](https://docs.google.com/spreadsheets/d/1blQrG8jU20Uy5Vwm1C6FkNjaCKoNW0UWjRCzrwpSSSM/edit#gid=0).
+Table looks like:
+
+| date | duration |
+|:----:|:--------:|
+|2018-09-05|54:15.96|
+|2018-09-07|52:09.26|
+
+The script will read the table and output mean and 50% value from table.
+
---
+++ python/pandas/driving_data.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+''' read driving data and make stastics '''
+
+from __future__ import print_function
+import pandas as pd
+from datetime import time
+
+
+def str2sec(timestr):
+    arr = timestr.split(':')
+    sec = 0.0
+    if len(arr) == 2:
+        sec = float(arr[0]) * 60.0 + float(arr[1])
+    return sec
+
+def strify(nn):
+    return '{:02}'.format(nn)
+
+def sec2str(sec):
+    ss = []
+    nn = int(sec)
+    while nn >= 60:
+        qq = nn / 60
+        rr = nn % 60
+        ss.append(strify(rr))
+        nn = qq
+    ss.append(strify(nn))
+    ss.reverse()
+    res = ':'.join(ss)
+    return res
+
+def print_sep():
+    print('-' * 40)
+
+def peek_target(desc_table, target):
+    #print('desc_table:', desc_table)
+    desc_list = desc_table.index.tolist()
+    try:
+        midx = desc_list.index(target)
+        mean_value = desc_table.iloc[midx, 0]
+        result = sec2str(mean_value)
+        return result
+    except ValueError:
+        return ''
+
+def main():
+    ''' main function '''
+    #print("pd.__version__: {}".format(pd.__version__))
+    FILEN = 'driving_data.csv'
+    # read csv file as dataframe
+    data = pd.read_csv(FILEN)
+
+    s1 = data['duration']
+    dates = []
+    secs = []
+    for ii, ds in enumerate(data['date']):
+        dates.append(ds)
+        secs.append(str2sec(s1[ii]))
+
+    ndict = {"date": dates, "seconds": secs}
+    res = pd.DataFrame(ndict)
+    #print(res.info())
+
+    print_sep()
+    des = res.describe()
+    #print('68', des)
+
+    queries = ['mean', '50%']
+    for qq in queries:
+        ans = peek_target(des, qq)
+        print('{}: {}'.format(qq, ans))
+    print_sep()
+
+
+if __name__ == '__main__':
+    main()
