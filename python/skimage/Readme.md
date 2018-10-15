9cc4705197db93d7ec12d9973e17e87ff073c73f
ericosur <ericosur@gmail.com>
2018-10-15 10:01:55 +0800

    simple scikit-image sample

---
+++ python/skimage/Readme.md
@@ -0,0 +1,12 @@
+# readme
+
+## module
+
+```
+$ pip install scikit-image
+```
+
+## data file
+
+This script takes one image from
+![moon.png](/usr/local/lib/python2.7/dist-packages/skimage/data/moon.png)
---
+++ python/skimage/sk.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# my first script for skimage (scikit-image)
+
+import os
+from skimage import io, data_dir
+
+def main():
+    fn = os.path.join(data_dir, 'moon.png')
+    print('skimage.data_dir: {}'.format(fn))
+    if not os.path.isfile(fn):
+        print('file not found, exit')
+        return
+    moon = io.imread(fn)
+    io.imshow(moon)
+    io.show()
+
+
+if __name__ == '__main__':
+    main()
