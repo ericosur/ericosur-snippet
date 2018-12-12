395154f5ad2b52311531c693c0d79accf078df94
ericosur <ericosur@gmail.com>
2018-12-12 10:06:29 +0800

    UPDATE hello_world.cpp, add STL regex sample

--- C/simple_test/hello_world.cpp
+++ C/simple_test/hello_world.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
-#include "json.hpp"
+#include <regex>
+#include <nlohmann/json.hpp>
 
 using namespace std;
 
@@ -24,10 +25,38 @@ void test()
     cout << "json.hpp version: " << query["version"]["string"] << endl;
 }
 
+void testre()
+{
+    cout << __func__ << endl;
+    string s = "color1544508130.png";
+
+    std::regex word_regex("(\\d+)");
+    auto words_begin =
+        std::sregex_iterator(s.begin(), s.end(), word_regex);
+    auto words_end = std::sregex_iterator();
+
+    for (std::sregex_iterator i = words_begin; i != words_end; ++i) {
+        std::smatch match = *i;
+        std::string match_str = match.str();
+        if (match_str.size()) {
+            std::cout << match_str << '\n';
+        }
+    }
+}
+
+void what()
+{
+    string s;
+    s = string("color") + string("12345") + ".png";
+    cout << s << endl;
+}
+
 int main()
 {
     cout << "hello world from " << __func__ << endl;
-	test();
+	//test();
+    //testre();
+    what();
 
 	return 0;
 }
