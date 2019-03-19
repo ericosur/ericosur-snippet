9e865de65a925aaa40257c017bac48f707f5930e
ericosur <ericosur@gmail.com>
2019-03-19 17:37:38 +0800

    UPDATE misc, add stdc.cpp for STL timing example

--- C/misc/stdc.cpp
+++ C/misc/stdc.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <chrono>
+#include <ratio>
+#include <thread>
 
 using namespace std;
+
+#define PRINT_HEADER()  \
+    std::cout << __func__ << " =====>>>" << std::endl;
 
 class Foo {
 public:
@@ -9,16 +15,53 @@ public:
     }
 
     void output() {
-        cout << __func__ << endl;
+        PRINT_HEADER();
+    }
+
+    static void f() {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 };
 
-int main()
+
+// reference: https://en.cppreference.com/w/cpp/chrono/duration/duration_cast
+int test1()
 {
-    cout << __func__ << endl;
+    PRINT_HEADER();
+    auto t1 = std::chrono::high_resolution_clock::now();
+    Foo::f();
+    auto t2 = std::chrono::high_resolution_clock::now();
+
+    // floating-point duration: no duration_cast needed
+    std::chrono::duration<double, std::milli> fp_ms = t2 - t1;
+
+    // integral duration: requires duration_cast
+    auto int_ms = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+
+    // converting integral duration to integral duration of shorter divisible time unit:
+    // no duration_cast needed
+    std::chrono::duration<long, std::micro> int_usec = int_ms;
+
+    std::cout << "f() took " << fp_ms.count() << " ms, "
+              << "or " << int_ms.count() << " whole milliseconds "
+              << "(which is " << int_usec.count() << " whole microseconds)" << std::endl;
+
+    return 0;
+}
+
+int test0()
+{
+    PRINT_HEADER();
 //    cout << __STDC_VERSION__ << endl;
     cout << __cplusplus << endl;
     Foo foo;
     foo.output();
+    return 0;
+}
+
+int main()
+{
+    test0();
+    test1();
     return 0;
 }
