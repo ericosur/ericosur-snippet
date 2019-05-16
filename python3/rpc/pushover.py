00fc956bc0493e6b27c58de668f4d082a8f91111
ericosur <ericosur@gmail.com>
2019-05-16 18:21:12 +0800

    add pushover.py, script to send notification via pushover.net

---
+++ python3/rpc/pushover.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+'''
+using pushover to send notification to my iphone
+'''
+
+import os
+import sys
+from datetime import datetime
+import time
+import http.client, urllib
+import myutil
+
+class foobar(object):
+    def __init__(self, msg):
+        (self.userkey, self.apitoken) = self.get_apikey()
+        self.title = 'pushover.py'
+        self.message = '{} at {}'.format(msg, datetime.now())
+
+    def show(self):
+        print('userkey: {}\napitoken: {}'.format(self.userkey, self.apitoken))
+
+    def shoot(self):
+        conn = http.client.HTTPSConnection("api.pushover.net:443")
+        conn.request("POST", "/1/messages.json",
+            urllib.parse.urlencode({
+                "token": self.apitoken,
+                "user": self.userkey,
+                "title": self.title,
+                "message": self.message,
+            }), { "Content-type": "application/x-www-form-urlencoded" })
+        resp = conn.getresponse()
+        print('status: {} reason: {}'.format(resp.status, resp.reason))
+
+    def set_title(self, title):
+        self.title = title
+
+    def set_message(self, message):
+        self.message = message
+
+    def get_timestamp(self):
+        return int(time.time())
+
+    @staticmethod
+    def get_apikey():
+        keyfile = 'pushover-net.json'
+        userkey = None
+        apitoken = None
+        home = os.environ.get('HOME')
+        keypath = home + '/Private/' + keyfile
+        if not myutil.isfile(keypath):
+            print('[FAIL] key file not exist: {}'.format(keypath))
+            return None, None
+        data = myutil.read_jsonfile(keypath)
+        if data is None:
+            return None, None
+
+        userkey = data.get('userkey')
+        apitoken = data.get('apitoken')
+        return userkey, apitoken
+
+def main():
+    gg = foobar('test pushover notification!')
+    gg.shoot()
+
+if __name__ == '__main__':
+    main()
