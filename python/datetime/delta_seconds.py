cfa8889f2ece05a82a7628ee2fc3daca42cc33a2
ericosur <ericosur@gmail.com>
2019-05-16 11:54:00 +0800

    update datatime, minor output and python3

--- python/datetime/delta_seconds.py
+++ python/datetime/delta_seconds.py
@@ -12,6 +12,7 @@ def main():
     '''main function'''
     birthday = datetime.datetime(2012, 2, 10, hour=16, minute=44)
     #birthday = datetime.datetime(1975, 6, 17, hour=12, minute=30)
+    print('birthday: {}'.format(birthday))
     base = 2
 
     for i in range(27, 32):
