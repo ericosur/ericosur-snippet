c23487c5c13b719bca9b6cde98889d15f3f0cfea
ericosur <ericosur@gmail.com>
2019-05-16 15:57:53 +0800

    update ep.py, use int for epoch number

--- python3/ep.py
+++ python3/ep.py
@@ -13,15 +13,14 @@ def epoch2timestr(epoch=-1):
     ''' Replace time.localtime with time.gmtime for GMT time '''
     if epoch == -1:
         epoch = int(time.time())
-        msg = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(time.time()))
-    else:
-        msg = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(epoch))
+
+    msg = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(epoch))
     return [epoch, msg]
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         print('argv[1]:({})'.format(sys.argv[1]))
-        print(epoch2timestr(float(sys.argv[1])))
+        print(epoch2timestr(int(sys.argv[1])))
     else:
         print(epoch2timestr())
         print(epoch2timestr(1468123201))
