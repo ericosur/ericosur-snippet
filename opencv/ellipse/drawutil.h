1c0420e8015223998ab5ba8908df7311959136d5
ericosur <ericosur@gmail.com>
2019-06-21 10:58:31 +0800

    UPDATE ellipse, group drawing function into standalone module files

---
+++ opencv/ellipse/drawutil.h
@@ -0,0 +1,12 @@
+#ifndef __UTIL_DRAWUTIL_H__
+#define __UTIL_DRAWUTIL_H__
+
+#include <math.h>
+#include <opencv2/opencv.hpp>
+
+void draw_ellipse();
+void draw_superellipse();
+
+
+
+#endif  // __UTIL_DRAWUTIL_H__
