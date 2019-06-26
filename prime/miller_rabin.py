8260d0ce019fea84dd81c4d6858795d94d0c4850
ericosur <ericosur@gmail.com>
2019-06-26 17:28:04 +0800

    update miller_rabin.py, CLI interface and pylint

--- prime/miller_rabin.py
+++ prime/miller_rabin.py
@@ -1,48 +1,74 @@
-#!/usr/bin/env python
-# reference:
-# https://comeoncodeon.wordpress.com/2010/09/18/miller-rabin-primality-test/
-#
+#!/usr/bin/env python3
+# coding: utf-8
+
+'''
+reference:
+https://comeoncodeon.wordpress.com/2010/09/18/miller-rabin-primality-test/
+'''
 
 import random
 
-def modulo(a,b,c):
+def modulo(a, b, c):
+    ''' modulo '''
     x = 1
     y = a
-    while b>0:
-        if b%2==1:
-            x = (x*y)%c
-        y = (y*y)%c
-        b = b/2
-    return x%c
+    while b > 0:
+        if b % 2 == 1:
+            x = (x * y) % c
+        y = (y * y) % c
+        b = b // 2
+    return x % c
 
-def millerRabin(n, iteration):
-    if n<2:
+def miller_rabin(n, iteration):
+    ''' miller rabin '''
+    if n < 2:
         return False
-    if n!=2 and n%2==0:
+    if n != 2 and n % 2 == 0:
         return False
 
     d = n - 1
-    while d%2 == 0:
-        d = d / 2
-    for i in range(iteration):
-        a = random.randint(1, n-1)
+    while d % 2 == 0:
+        d = d // 2
+    for _ in range(iteration):
+        a = random.randint(1, n - 1)
         temp = d
         #x = modulo(a, temp, n)
         x = pow(a, temp, n)     # use built-in function
-        while (temp != n-1 and x!=1 and x!=n-1):
-            x = (x*x)%n
+        while temp != n - 1 and x != 1 and x != n - 1:
+            x = (x * x) % n
             temp = temp * 2
-        if (x!=n-1 and temp%2==0):
+        if x != n - 1 and temp % 2 == 0:
             return False
     return True
 
-if __name__ == '__main__':
-    '''
-    for i in xrange(10):
-        (a, b, n) = (random.randint(70,100), random.randint(40,80), random.randint(41, 97))
-        print "pow(%d, %d, %d) = %d" % (a, b, n, pow(a, b, n))
-        print "pow(%d, %d, %d) = %d" % (a, b, n, modulo(a, b, n))
-    '''
-    n = 795028841
-    print n, 'is', (millerRabin(n, 20) and "PRIME" or "COMPOSITE")
 
+def test():
+    ''' test '''
+    for _ in range(10):
+        (a, b, n) = (random.randint(201, 299), random.randint(101, 199), random.randint(11, 97))
+        p = pow(a, b, n)
+        q = modulo(a, b, n)
+        assert p == q
+        print("pow(%d, %d, %d) = %d" % (a, b, n, p))
+        #print("pow(%d, %d, %d) = %d" % (a, b, n, modulo(a, b, n)))
+
+
+def main(n):
+    ''' main '''
+    print(n, 'is', (miller_rabin(n, 20) and "PRIME" or "COMPOSITE"))
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) == 1:
+        print('may specify numbers from cli...')
+        main(795028841)
+    else:
+        if sys.argv[1] == 'test':
+            test()
+        else:
+            for i in sys.argv[1:]:
+                try:
+                    main(int(i))
+                except ValueError:
+                    print('cannot convert "{}" into integer'.format(i))
