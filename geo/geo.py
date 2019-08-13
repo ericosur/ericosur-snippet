bc19873b9213f50314d7a5e6db20fc1d1c9c532f
ericosur <ericosur@gmail.com>
2019-08-13 15:58:22 +0800

    update geo.py, need more test, calling limit?

--- geo/geo.py
+++ geo/geo.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# use google geocoding API to query location, address, and etc.
-# refer to:
-# https://github.com/google/open-location-code/wiki/Plus-codes-API
-#
+'''
+use google geocoding API to query location, address, and etc.
+refer to:
+https://github.com/google/open-location-code/wiki/Plus-codes-API
+'''
 
 import sys
 import os
@@ -12,15 +13,17 @@ import time
 import json
 import requests
 
-debug = False
+DEBUG = False
 
 def help_message():
-    helpmsg='''
+    ''' help message '''
+    helpmsg = '''
     specify POI names to query plus code
     '''
     print(helpmsg.strip())
 
 def read_secret():
+    ''' read secret keys '''
     home = os.environ.get('HOME')
     file = home + '/Private/geocoding-key.json'
     # read from json file
@@ -28,7 +31,7 @@ def read_secret():
         data = json.load(sec_file)
     key = data.get('key')
     email = data.get('email')
-    if debug:
+    if DEBUG:
         print("apikey:{0}\nemail:{1}".format(key, email))
     return (key, email)
 
@@ -37,13 +40,14 @@ def read_secret():
 URL="https://plus.codes/api?address=${ADDR}&ekey=${KEY}&${EMAIL}"
 '''
 def query_geocoding(addr, key, email):
+    ''' compose api call and request '''
     payload = {'address': addr, 'ekey': key, 'email': email}
     url = "https://plus.codes/api"
 
     r = requests.get(url, params=payload)
     print('url:', r.url)
-    #print("r.json():", r.json())
-    #print("r.text:", r.text)
+    print("r.json():", r.json())
+    print("r.text:", r.text)
     j = r.json()
     print('plus code:', j['plus_code']['global_code'])
     print('location:', j['plus_code']['geometry']['location'])
@@ -54,11 +58,19 @@ def query_geocoding(addr, key, email):
     print('output to {}'.format(fn))
 
 
-if __name__ == '__main__':
+def main(argv):
+    ''' main '''
     key, email = read_secret()
-    if len(sys.argv) == 1:
+    if argv == []:
+        argv.append('自由女神像')
+
+    for pp in argv:
+        print(pp)
+        query_geocoding(pp, key, email)
+
+if __name__ == '__main__':
+
+    if len(sys.argv) <= 1:
         help_message()
-        query_geocoding("自由女神像", key, email)
-    elif len(sys.argv) > 1:
-        for pp in sys.argv:
-            query_geocoding(pp, key, email)
+
+    main(sys.argv[1:])
