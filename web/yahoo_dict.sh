ec4041528ffd49f0ff936a89ab599329b52daaf8
ericosur <ericosur@gmail.com>
2019-08-13 15:34:36 +0800

    remove all not working scripts in folder web

--- web/yahoo_dict.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-curl -sd "p=$1" http://tw.dictionary.yahoo.com/search  | \
-         awk "/pexplain/{ print } \
-         /peng/{ print } \
-         /pchi/{ print ; print \"\n\" }" | \
-         sed -e :a -e 's/<[^>]*>//g;/</N;//ba'
