b8c086d2177e2ad2a9a7268d47ce12c6845bf464
ericosur <ericosur@gmail.com>
2019-09-12 17:35:53 +0800

    UPDATE b6485.py, multiple encoding including: base58, 64, 85

--- python3/b6485.py
+++ python3/b6485.py
@@ -2,12 +2,14 @@
 # utf-8
 
 '''
+base58
 base64
 base85
 '''
 
 import base64
 from random import randint
+import base58
 import numpy as np
 
 def int_to_bytes(x: int) -> bytes:
@@ -26,17 +28,46 @@ def fill_bytearray(size: int = 24) -> bytes:
 
 def test(v: bytes):
     ''' test '''
-    a = base64.b64encode(v)
-    print('base64:', a)
-    b = base64.a85encode(v)
-    print('base85:', b)
+
+    def show(m, n):
+        ''' show '''
+        print('{:<14s}: {}'.format(m, n))
+
+    #hx = binascii.hexlify(v)    # bytes: b'([0-9a-f][0-9a-f])+'
+    #show('input', hx)
+    hxx = v.hex()               # str: ([0-9a-f][0-9a-f])+
+    show('input hex', hxx)
+    #b16 = base64.b16encode(v)   # bytes: b'([0-9A-F][0-9A-F])+'
+    #show('b16encode', b16)
+    print('-' * 60)
+
+    r0 = base64.standard_b64encode(v)
+    r1 = base64.b64encode(v)
+    r2 = base64.urlsafe_b64encode(v)
+
+    if r0 != r1:
+        show('std base64', r0)
+    show('base64', r1)
+
+    if r1 != r2:
+        show('urlsafe base64', r2)
+
+    print('-' * 60)
+    r = base64.a85encode(v)
+    show('base85a', r)
+    r = base64.b85encode(v)
+    show('base85b', r)
+
+    print('-' * 60)
+    r = base58.b58encode(v)
+    show('base58', r)
 
 
 def main(argv):
     ''' main '''
     if argv == []:
-        for _ in range(5):
-            x = fill_bytearray(randint(16, 48))
+        for _ in range(1):
+            x = fill_bytearray(randint(10, 40))
             argv.append(x)
 
     for e in argv:
@@ -47,10 +78,11 @@ def main(argv):
                 v = int_to_bytes(e)
             elif isinstance(e, bytes):
                 v = e
-                print('len', len(v))
             else:
                 v = bytes(e)
+
             test(v)
+            print()
         except ValueError:
             print('invalid input: {}'.format(e))
 
