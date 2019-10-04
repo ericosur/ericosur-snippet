c97500ba44ac46a9eb60ecad1bc54ef1b9fdd26a
ericosur <ericosur@gmail.com>
2019-10-04 18:17:14 +0800

    add extfactor.py, use '/usr/bin/factor' to factorize a number
    it supports:
    - demo mode without any input
    - read from STDIN, and
    - CLI input

---
+++ python3/extfactor.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+'''
+    call /usr/bin/factor and pick results
+    also refer to **factorint.py** that uses sympy to do factorize
+'''
+
+import os
+from random import randint
+import re
+import sys
+
+def call_factor(n: int):
+    ''' call external factor '''
+    if n <= 0:
+        print('{} cannot be zero or negative'.format(n))
+        return
+    cmd = '/usr/bin/factor {}'.format(n)
+    a = os.popen(cmd).read()
+    #print(res)
+    b = re.sub(r'^(\d+: )(.+)$', '\\2', a.strip())
+    vals = b.split(' ')
+    print('{}: '.format(n), end='')
+    if len(vals) == 1:
+        print('a prime number')
+    else:
+        print(vals)
+
+def read_from_stdin():
+    ''' read from stdin '''
+    args = []
+    for line in sys.stdin:
+        args.append(line.strip())
+    main(args)
+
+
+def main(argv):
+    ''' main '''
+    if argv == []:
+        argv.append(randint(1001, 9999))
+
+    for v in argv:
+        try:
+            call_factor(int(v))
+        except ValueError:
+            print('not a value:', v)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print('use "{} -" read input from STDIN'.format(sys.argv[0]))
+        # demo mode
+        print('demo mode...')
+        main([])
+        sys.exit()
+
+    if sys.argv[1] == '-':
+        read_from_stdin()
+    else:
+        main(sys.argv[1:])
