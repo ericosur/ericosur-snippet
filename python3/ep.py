e9efbc021e04a7ffec49bb877b2b13be84a5bed8
ericosur <ericosur@gmail.com>
2019-10-16 15:26:20 +0800

    update ep.py, add description

--- python3/ep.py
+++ python3/ep.py
@@ -3,13 +3,42 @@
 
 '''
 convert epoch value to date/time string
+
+reference:
+https://www.epochconverter.com/
+
+python:
+import calendar, time
+calendar.timegm(time.strptime('2000-01-01 12:34:00', '%Y-%m-%d %H:%M:%S'))
+# as the following
+
+import time
+time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime(epoch))
+# Replace time.localtime with time.gmtime for GMT time.
+# Or using datetime:
+import datetime
+datetime.datetime.utcfromtimestamp(epoch).replace(tzinfo=datetime.timezone.utc)
+
+
+## tips
+linux shell:
+# Replace '-d' with '-ud' to input in GMT/UTC time
+# human date string to epoch
+date +%s -d"Jan 1, 1980 00:00:01"
+# epoch to date string
+date -d @1520000000
+
+## in my own git repository, also see:
+https://github.com/ericosur/ericosur-snippet/blob/master/shell/date-string-example.sh
+https://github.com/ericosur/ericosur-snippet/tree/master/root/epoch
+
 '''
 
-from __future__ import print_function
 import time
+from random import randint
 import sys
 
-def epoch2timestr(epoch=-1):
+def epoch2timestr(epoch: int):
     ''' Replace time.localtime with time.gmtime for GMT time '''
     if epoch == -1:
         epoch = int(time.time())
@@ -17,10 +46,30 @@ def epoch2timestr(epoch=-1):
     msg = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(epoch))
     return [epoch, msg]
 
+def read_from_stdin():
+    ''' read from stdin '''
+    args = []
+    for line in sys.stdin:
+        args.append(line.strip())
+    main(args)
+
+def main(args: list):
+    ''' main '''
+    if args == []:
+        args.append(time.time())
+        args.append(172162800)
+        for _ in range(5):
+            args.append(randint(946656000, 1609344000))
+
+    for v in args:
+        try:
+            num = int(v)
+            print(epoch2timestr(num))
+        except ValueError:
+            print('{} is not a valid integer'.format(v))
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        print('argv[1]:({})'.format(sys.argv[1]))
-        print(epoch2timestr(int(sys.argv[1])))
+        main(sys.argv[1:])
     else:
-        print(epoch2timestr())
-        print(epoch2timestr(1468123201))
+        read_from_stdin()
