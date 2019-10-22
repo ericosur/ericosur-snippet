de66501ea22cdec74d1dc41a8f53f64073749c40
ericosur <ericosur@gmail.com>
2019-10-22 18:29:48 +0800

    update sdir.py, rewrite and pylint'ed

--- python/sdir.py
+++ python/sdir.py
@@ -1,44 +1,35 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+# coding: utf-8
 
 """
-    try to locate specified name in PATH
+try to locate specified name in PATH
 """
 
-from __future__ import print_function
 import os
 import sys
 
 
-def search_file(filename, search_path, pathsep=os.pathsep):
-    """
-    Given a search path, find file with requested name
-    """
+def main(args: list):
+    ''' input list of items to search '''
+    if args == []:
+        args.append('git')
 
-    result = None
-    print("searching", filename, "...")
-    pathext = os.getenv('pathext')
-    for path in search_path.split(pathsep):
-        #print 'search in', path
-        for ext in pathext.split(pathsep):
-            ff = filename + ext
-            candidate = os.path.join(path, ff)
-            #print candidate;
+    res = os.environ['PATH']
 
-            if os.path.isfile(candidate):
-                result = os.path.abspath(candidate)
-                print('found in', result)
-                break   # just break one inner loop for pathext, will search in next path
-
-    return result
+    for a in args:
+        found = False
+        for p in res.split(os.pathsep):
+            fp = p + '/' + a
+            if os.path.isfile(fp):
+                print('found: {}'.format(fp))
+                found = True
+        if not found:
+            print('not found: {}'.format(a))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        print('please specify file to search in path')
-        quit()
-    for idx in range(1, len(sys.argv)):
-        x = sys.argv[idx]
-        #print 'search:', x
-        res = search_file(x, os.getenv('path'))
-    if res is None:
-        print("not found in PATH")
+    if len(sys.argv) > 1:
+        main(sys.argv[1:])
+    else:
+        print('specify arg to search in PATH')
+        main([])
