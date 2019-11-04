7773500f908d8d086f4ac8a62cab51b6d656cb8d
ericosur <ericosur@gmail.com>
2019-11-04 18:00:51 +0800

    UPDATE regexp python scripts, pylint'ed

--- regexp/re_1to99.py
+++ regexp/re_1to99.py
@@ -1,16 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+
+''' trivia regexp demo of python '''
 
 import re
 
-test = ['1', '01', '2', '03', '5', '8', '13', '21', '34', '101']
-pattern = r"^([1-9][0-9]?|)$"
-p = re.compile(pattern)
+def main():
+    ''' main '''
+    test = ['1', '01', '2', '03', '5', '8', '13', '21', '34', '101']
+    pattern = r"^([1-9][0-9]?|)$"
+    p = re.compile(pattern)
 
-for i in test:
-	m = p.match(i)
-	print i,"\t",
-	if m is None:
-		print "is not matched"
-	else:
-		print "is matched: ",
-		print m.group(1)
+    for i in test:
+        m = p.match(i)
+        print(i, "\t", end='')
+        if m is None:
+            print("is not matched")
+        else:
+            print("is matched: ", m.group(1))
+
+if __name__ == '__main__':
+    main()
--- regexp/re_show.py
+++ regexp/re_show.py
@@ -1,11 +1,25 @@
+#!/usr/bin/env python3
+# coding: utf-8
+#
+
+'''
+simple show regexp string
+'''
 
 import re
-def re_show(pat, s):
-    print re.compile(pat, re.M).sub("{\g<0>}", s.rstrip()),'\n'
 
-s = '''Mary had a little lamb
+def re_show(pat, s):
+    ''' show regexp '''
+    print(re.compile(pat, re.M).sub(r"{\g<0>}", s.rstrip()), '\n')
+
+def main():
+    ''' main '''
+    s = '''Mary had a little lamb
 And everywhere that Mary
 went, the lamb was sure
 to go'''
 
-re_show("\w+", s)
+    re_show(r"\w+", s)
+
+if __name__ == '__main__':
+    main()
