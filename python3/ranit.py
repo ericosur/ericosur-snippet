5cf643b5c91b68c67cef85cd1c124afceef0e30b
ericosur <ericosur@gmail.com>
2020-01-02 10:16:27 +0800

    UPDATE ranit.py

--- python3/ranit.py
+++ python3/ranit.py
@@ -9,11 +9,18 @@ import random
 
 def main():
     ''' main '''
-    for _ in range(10):
-        r = random.randint(1000, 9999)
-        print(r, divmod(r, 7))
-        if r % 7 == 0:
-            print('{} is seven multiple...'.format(r))
+    DIVISOR = 7
+    REPEAT = 10
+    MIN = 1000
+    MAX = 99999
+    for _ in range(REPEAT):
+        n = random.randint(MIN, MAX)
+        (q, r) = divmod(n, DIVISOR)
+        print('{} / {} = {}'.format(n, DIVISOR, q), end='')
+        if r:
+            print(' ... {}'.format(r))
+        else:
+            print()
 
 
 if __name__ == '__main__':
