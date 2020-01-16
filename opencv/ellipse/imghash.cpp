489f7d0ff3cffbd6fe651bb64db5b243206cdeb8
ericosur <ericosur@gmail.com>
2020-01-16 17:29:48 +0800

    UPDATE imghash.cpp, method to include opencv headers

--- opencv/ellipse/imghash.cpp
+++ opencv/ellipse/imghash.cpp
@@ -1,8 +1,7 @@
-#include "opencv2/core.hpp"
-#include "opencv2/core/ocl.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/img_hash.hpp"
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/img_hash.hpp>
+#include <opencv2/core/ocl.hpp>
 
 using namespace cv;
 using namespace cv::img_hash;
