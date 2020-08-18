a6a50c52226c9f6f4b001feefb4f71141dbcb74b
ericosur <ericosur@gmail.com>
2020-08-18 09:26:45 +0800

    ADD ten_digits.py
    To solve:
    Pick five digits from a ten-digit number in original order, get the
    max one and the left one.
    
    For example, a number 3,686,167,107, pick the biggest 87,107, and
    left part is 36616 (not 36661)

---
+++ python3/ten_digits.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+''' 10-digit number, find the max one '''
+
+import random
+import itertools as it
+
+class Solution():
+    ''' solution '''
+    def __init__(self, start: int = 0):
+        if start != 0:
+            self.start = str(start)
+        else:
+            self.get_start()
+
+    def __str__(self):
+        return str(self.start)
+
+    def get_start(self):
+        ''' get start '''
+        s = '9876543210'
+        l = list(s)
+        while True:
+            a = list()
+            for _ in range(10):
+                a.append(l[random.randint(0, 9)])
+                #a.append(random.choice(s))
+            #print('picked:', a)
+            try:
+                m = ''.join(a)
+                #print('m:{} len:{}'.format(m, len(m)))
+                if int(m) >= 1e9:
+                    break
+            except ValueError:
+                print('ValueError')
+                break
+        self.start = m
+
+    def test000(self):
+        ''' start '''
+        l = list(str(self.start))
+        m = sorted(l, reverse=True)
+        print('l:', l)
+        print('m:', m)
+
+    def test_01(self):
+        ''' test '''
+        for _ in range(100):
+            self.get_start()
+            print(str(self.start))
+
+    def get_left(self, remove_part: str) -> str:
+        ''' get left digits
+            input: 3686167107, 87107
+            output: 36616, notice: not 36661
+        '''
+        begin = self.start
+        arr = list(begin)
+        print('begin: {}, remove: {}'.format(begin, remove_part))
+        res = list()
+        for cc in list(remove_part):
+            i = arr.index(cc)
+            lhs = arr[0:i]
+            rhs = arr[i+1:]
+            #print(cc, ''.join(lhs), ' --- ', ''.join(rhs))
+            arr = rhs
+            res.extend(lhs)
+        return ''.join(res)
+
+    def test(self):
+        ''' test '''
+        #print('self.start', self.start)
+        cnt = 0
+        arr = list()
+        for ii in it.combinations(self.start, 5):
+            cnt += 1
+            s = ''.join(ii)
+            arr.append(int(s))
+
+        #print('max:{}, cnt:{}'.format(max(arr), cnt))
+        # find the max number in all combination
+        mpart = str(max(arr))
+        # get the left part, remove the max number digits
+        misc = self.get_left(mpart)
+        print("max: {}, misc: {}".format(mpart, misc))
+
+    def test_02(self):
+        ''' test '''
+        self.start = '3686167107'
+        print(self.get_left('87107'))
+
+
+def main():
+    ''' main '''
+    # 1
+    s = Solution(3686167107)
+    s.test()
+    # 2
+    for _ in range(10):
+        m = Solution()
+        m.test()
+        del m
+
+
+if __name__ == '__main__':
+    main()
