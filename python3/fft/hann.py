aad9fe7b173fc2b27d10b0e55e7992b13134a5e9
ericosur <ericosur@gmail.com>
2020-08-20 15:34:33 +0800

    pylint'ed, and update

--- python3/fft/hann.py
+++ python3/fft/hann.py
@@ -11,27 +11,32 @@ import numpy as np
 from numpy.fft import fft, fftshift
 from myutil import get_python_version
 
-if float(get_python_version()) < 3.6:
-    # explicitly assign TkAgg, default maybe Qt
-    matplotlib.use('TkAgg')
+def main():
+    ''' main '''
+    if float(get_python_version()) < 3.6:
+        # explicitly assign TkAgg, default maybe Qt
+        matplotlib.use('TkAgg')
 
-window = np.hanning(51)
-plt.plot(window)
-plt.title('Hann Window')
-plt.ylabel('Amplitude')
-plt.xlabel('Sample')
+    window = np.hanning(51)
+    plt.plot(window)
+    plt.title('Hann Window')
+    plt.ylabel('Amplitude')
+    plt.xlabel('Sample')
 
-plt.figure()
-A = fft(window, 2048) / 25.5
-mag = np.abs(fftshift(A))
-freq = np.linspace(-0.5, 0.5, len(A))
-with np.errstate(divide='ignore', invalid='ignore'):
-    response = 20 * np.log10(mag)
+    plt.figure()
+    A = fft(window, 2048) / 25.5
+    mag = np.abs(fftshift(A))
+    freq = np.linspace(-0.5, 0.5, len(A))
+    with np.errstate(divide='ignore', invalid='ignore'):
+        response = 20 * np.log10(mag)
 
-response = np.clip(response, -100, 100)
-plt.plot(freq, response)
-plt.title('Frequency response of the Hann Window')
-plt.ylabel('Magnitude [dB]')
-plt.xlabel('Normalized frequency [cycles per sample')
-plt.axis('tight')
-plt.show()
+    response = np.clip(response, -100, 100)
+    plt.plot(freq, response)
+    plt.title('Frequency response of the Hann Window')
+    plt.ylabel('Magnitude [dB]')
+    plt.xlabel('Normalized frequency [cycles per sample')
+    plt.axis('tight')
+    plt.show()
+
+if __name__ == '__main__':
+    main()
