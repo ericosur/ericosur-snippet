1e0f0f09e786837116e171c4de7d410ae8a1f43f
ericosur <ericosur@gmail.com>
2020-09-15 10:23:52 +0800

    UPDATE godir.py, will not chdir into subdirectory that has no py files

--- python3/godir.py
+++ python3/godir.py
@@ -5,27 +5,30 @@
 list all dir
 '''
 
+from glob import glob
 from os import listdir, system, chdir, getcwd
 from os.path import isdir, join
 
 def check_dir(d):
     ''' check dir with pylint '''
     chdir(d)
-    print('chdir: ', d)
-    system('pylint *.py')
+    arr = glob('*.py')
+    # if no py files, will not issue pylint command
+    if arr:
+        print('pylint check dir:', d)
+        system('pylint *.py')
 
 def main():
     ''' main '''
-    # 指定要列出所有檔案的目錄
+    # record the current working directory
     top = getcwd()
     print('cwd:', top)
 
-    # 取得所有檔案與子目錄名稱
+    # get sub-directories & files under CWD (one level only)
     files = listdir(top)
-
-
+    ignores = ['__pycache__']
     for f in files:
-        if f == '__pycache__':
+        if f in ignores:
             continue
         fullpath = join(top, f)
         if isdir(fullpath):
