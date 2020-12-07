128edf131022d9190903ac14e83a83f2c2807bbd
ericosur <ericosur@gmail.com>
2020-12-07 17:36:55 +0800

    🚧 Add opencv perspective transform example

---
+++ opencv/python/persp-transform/sim.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3.6
+# coding: utf-8
+
+'''
+cv2 imshow
+'''
+
+import sys
+import numpy as np
+import cv2
+
+def show_img(fn):
+    ''' show img '''
+    img = cv2.imread(fn)
+    cv2.imshow('img', img)
+    print('{}: {}x{}'.format(fn, img.shape[1], img.shape[0]))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def main(argv):
+    ''' main '''
+    for f in argv:
+        show_img(f)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        main(sys.argv[1:])
+    else:
+        print('please specify file name')
