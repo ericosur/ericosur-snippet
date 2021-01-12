275a2e70ca10d4801c4d258d1ef4b8ebce296e80
ericosur <ericosur@gmail.com>
2021-01-12 10:00:13 +0800

    ➕ Add an example for sum up all nodes in binary tree

---
+++ Topics/coin_change/sumofnodes.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3.6
+# coding: utf-8
+
+'''
+https://www.geeksforgeeks.org/sum-nodes-binary-tree/
+'''
+
+# Python3 Program to print sum of all
+# the elements of a binary tree
+
+# Binary Tree Node
+# This code is contributed by
+# Shubham Singh(SHUBHAMSINGH10)
+
+""" utility that allocates a new Node
+with the given key """
+
+# pylint: disable=invalid-name
+class newNode:
+    ''' Construct to create a new node '''
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+# Function to find sum of all the element
+def addBT(root):
+    ''' to traverse all nodes and sum up '''
+    if root is None:
+        return 0
+    return root.key + addBT(root.left) + addBT(root.right)
+
+# Driver Code
+def main():
+    ''' main '''
+    root = newNode(1)
+    root.left = newNode(2)
+    root.right = newNode(3)
+    root.left.left = newNode(4)
+    root.left.right = newNode(5)
+    root.right.left = newNode(6)
+    root.right.right = newNode(7)
+    root.right.left.right = newNode(8)
+
+    node_sum = addBT(root)
+    print("Sum of all the nodes is:", node_sum)
+
+if __name__ == '__main__':
+    main()
