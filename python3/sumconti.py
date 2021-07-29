c142a29e757bd7441435236171b5a980ffbc0812
ericosur <ericosur@gmail.com>
2021-07-29 14:46:51 +0800

    ADD sumconti.py, sum of continuous integers

---
+++ python3/sumconti.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+'''
+continuous sum
+
+eg:
+15 = 1 + 2 + 3 + 4 + 5
+15 = 4 + 5 + 6
+15 = 7 + 8
+
+ask 45?
+'''
+
+import argparse
+
+
+def solve(n):
+    ''' solve '''
+    print("=======> solve this: {}".format(n))
+    upper = n // 2
+    s = 0
+    ans = list()
+    for i in range(1, upper+1):
+        s = 0
+        r = i
+        while s < n:
+            s += r
+            ans.append(r)
+            if s == n:
+                print('exactly: ', ans)
+                break
+            if s > n:
+                break
+            r += 1
+        ans.clear()
+
+def main():
+    ''' main '''
+    parser = argparse.ArgumentParser(description='brief description for this script')
+    parser.add_argument("integers", metavar='ints', type=int, nargs='*',
+        help="trial with these integers")
+
+    args = parser.parse_args()
+    if args.integers == []:
+        print('apply demo question: 45')
+        args.integers.append(45)
+
+    for ii in args.integers:
+        solve(ii)
+
+if __name__ == '__main__':
+    main()
