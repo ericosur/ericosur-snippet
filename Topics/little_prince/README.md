19abc4cdd934d1da78840323fcf46b40c90fb6d7
ericosur <ericosur@gmail.com>
2021-10-29 16:12:03 +0800

    ADD little prince and rad.txt

---
+++ Topics/little_prince/README.md
@@ -0,0 +1,22 @@
+# README
+
+also refer to: ```src/charencoding/ime/little_prince```
+
+* __littleprince.txt__ is a specilized version for practicing
+typing in boshiamy. More than 80% of characters could be typing in quick codes,
+that is, one or two radicals could compose one Han character.
+
+* [rad.txt](./rad.txt)
+
+## reference
+
+* __littleprince.txt__ is downloaded and transformed from [boshiamy practice](https://boshiamy.com/LiuCAI/article_prince.html)
+
+* __a64.txt__ I download it from [myreader](http://myreader.tinpok.com/list.php?bid=66). It is a traditional chinese version of Little Prince.
+
+* __liu.json__ is translated by [UCL_LIU](https://github.com/shadowjohn/UCL_LIU)
+
+## ucl-liu
+
+https://github.com/shadowjohn/UCL_LIU/blob/master/README.md
+
---
+++ Topics/little_prince/myown.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+'''
+read in __liu.json__
+input boshiamy radicals, output characters
+'''
+
+import os
+import json
+
+def read_jsonfile(fn, debug=False):
+    '''
+    specify json filename and return whole json object
+    '''
+    if debug:
+        print(f'load json from {fn}')
+    if not os.path.exists(fn):
+        print('file not found')
+        return None
+
+    # read from json file
+    with open(fn, 'r', encoding='utf8') as fstream:
+        data = json.load(fstream)
+    return data
+
+class Bosha:
+    ''' read liu.json and rad.txt output han characters '''
+    json_fn = 'liu.json'
+    def __init__(self, fn: str):
+        self.fn = fn
+        self.data = None
+
+    def action(self):
+        ''' main flow '''
+        self.data = read_jsonfile(self.json_fn)
+        cdefs = self.data["chardefs"]
+        #size = len(self.data)
+        #print(f'read size: {size}')
+        with open(self.fn, 'rt', encoding='utf-8') as fobj:
+            for ln in fobj.readlines():
+                ln = ln.strip()
+                chars = ln.split(' ')
+                for c in chars:
+                    if c == '':
+                        continue
+                    try:
+                        r = cdefs[c]
+                        ans = r[0]
+                        print(f'{ans}', end='\u2003')
+                    except KeyError as e:
+                        print(f'Spelling not found: [{e}]')
+                        print()
+                print()
+
+def main():
+    ''' main '''
+    bs = Bosha('rad.txt')
+    bs.action()
+
+if __name__ == '__main__':
+    main()
