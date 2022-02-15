1b0cf8eb8e9ec203ed05f286233fe4f844fa3e1a
ericosur <ericosur@gmail.com>
2022-02-15 15:31:02 +0800

    ADD os_path_join.py
    To demo os.path.join() which takes care of different OS dir seperators.

---
+++ python3/os_path_join.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# coding: utf-8
+#
+
+'''
+demo how to use os.path.join()
+
+It will take care the dir seperator from different OS.
+'''
+
+import os.path
+
+def main():
+    ''' main '''
+    folder_output_name = 'output'
+    for frame_number in range(10):
+        fn = f'frame_{frame_number:05d}.png'
+        f = os.path.join(folder_output_name, fn)
+        print(f)
+
+if __name__ == '__main__':
+    main()
