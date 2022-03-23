53ad0d874673e6b30f9a0dcaf0563a1cf981f2eb
ericosur <ericosur@gmail.com>
2022-03-23 10:10:17 +0800

    ADD nineteen.py: a large number mod 99

---
+++ python3/nineteen.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# 191919....19 mod 99
+
+import math
+
+def main():
+    ''' main '''
+    tok = '19'
+    res = ''
+    for i in range(20):
+        res = res + tok
+    print('len:', len(res))
+    big_value = int(res)
+    r1 = big_value % 99
+    print('log10:', math.log10(big_value))
+    r2 = (19 * 20) % 99
+    print(f'r1:{r1}, r2:{r2}')
+
+
+if __name__ == '__main__':
+    main()
