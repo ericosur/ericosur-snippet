39f1e5edee83bbbe0d087481ec7704176301bf63
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2022-08-31 17:53:42 +0800

    UPDATE: hello_cargo for println

--- rust/hello_cargo/src/main.rs
+++ rust/hello_cargo/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, Cargo!");
+
+    let mut x = 5;
+    println!("x = {}", x);
+
+    const ONE_MINUTE: u32 = 60;
+    println!("om = {}", ONE_MINUTE);
+
+    x = 6 * ONE_MINUTE;
+    println!("x = {}", x);
+
 }
