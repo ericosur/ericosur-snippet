f18beab30d547b6643b4b9baca679911677997ed
ericosur <ericosur@gmail.com>
2023-01-06 23:30:50 +0800

    ADD 🔨 abcd.py

--- python3/abcd.py
+++ python3/abcd.py
@@ -1,39 +1,75 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # coding: utf-8
 
 '''
-problem:
-1. 4 digits
-2. new number is 4 times bigger than old one
-3. it is reversed, eg 1234 -> 4321
+a, b, c, d are four different digits,
+and abc + ab = b = ddd
 '''
 
-import itertools as it
-import numpy as np
-
-def valueOf(p):
-    ''' get scalar value of list p '''
-    b = [1000, 100, 10, 1]
-    r = sum(np.multiply(b, p))
-    return r
-
 def test():
-    ''' try the value to meet the problem '''
-    d = list(range(1, 10))  # 1 .. 9
-    for dd in it.product(d, d, d, d):
-        pp = list(dd)
-        qq = list(dd)
-        qq.reverse()
+	''' run '''
+	assert Solution.has_duplicate(0) is False
+	assert Solution.has_duplicate(10) is False
+	assert Solution.has_duplicate(22) is True
+	assert Solution.has_duplicate(98) is False
+	assert Solution.has_duplicate(100) is True
+	assert Solution.has_duplicate(111) is True
+	assert Solution.has_duplicate(123) is False
 
-        x = valueOf(pp)
-        y = valueOf(qq)
-        if y / 4 == x:
-            print(x, y)
+
+class Solution():
+	def __init__(self, v):
+		self.input = v
+		dig = list(str(v))
+		self.a = int(dig[0])
+		self.b = int(dig[1])
+		self.c = int(dig[2])
+		self.d = 0
+
+	@staticmethod
+	def has_duplicate(v):
+		digits = set(str(v))
+		return len(digits) != len(str(v))
+
+	def check(self):
+		m1 = self.a * 100 + self.b * 10 + self.c * 1
+		m2 = self.a * 10 + self.b * 1
+		m3 = self.b
+		tmp = m1 + m2 + m3
+		if tmp % 111 == 0:
+			#print(f'{m1} + {m2} + {m3}')
+			#print(f'input: {self.input} tmp: {tmp}')
+			return True
+		return False
+
+	@staticmethod
+	def report(v):
+		m1 = v
+		m2 = v // 10
+		m3 = m2 % 10
+		m4 = m1 + m2 + m3
+		print(f'{m1} + {m2} + {m3} = {m4}')
+		print()
+
+
+def run():
+	cnt = 0
+	for i in range(100,1000):
+		if Solution.has_duplicate(i):
+			#print(f'reject {i}')
+			continue
+		if i < 100:
+			continue
+		sol = Solution(i)
+		if sol.check():
+			sol.report(i)
+
+		cnt += 1
+	#print(f'cnt: {cnt}')
 
 def main():
-    ''' main '''
-    print(__doc__)
-    test()
+	''' main '''
+	run()
 
 if __name__ == '__main__':
-    main()
+	main()
