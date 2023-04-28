c46e182295b13564e8ec73de01c611b0b3d3d2eb
ericosur <ericosur@gmail.com>
2023-04-28 16:29:07 +0800

    ADD toml example scripts
    Signed-off-by: ericosur <ericosur@gmail.com>

---
+++ python3/basic/readtoml.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+'''
+example to read config.toml
+https://docs.python.org/zh-tw/dev/library/tomllib.html
+'''
+
+import toml
+import numpy as np
+
+def test_np():
+    ''' numpy vs toml '''
+    n = np.arange(0, 10, dtype=np.double)
+    output = {'n': n}
+
+    t = toml.dumps(output)
+    #n = [ "0.0", "1.0", "2.0", "3.0", "4.0", "5.0", "6.0", "7.0", "8.0", "9.0",]\n
+    print(t)
+
+    t = toml.dumps(output, encoder=toml.TomlNumpyEncoder())
+    #n = [ 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0,]\n
+    print(t)
+
+
+def test0():
+    ''' test0 '''
+    # Load data from a TOML file
+    with open('config.toml', 'r', encoding='utf-8') as f:
+        data = toml.load(f)
+
+    # Access data from the loaded TOML file
+    print(data['title'])
+
+    o = data['owner']
+    dob = o['dob']
+    print(dob, type(dob))
+
+    d = data['clients']['wtf']
+    print(f"{d['odd']=}")
+    print(f'{d["even"]=}')
+
+def main():
+    ''' main '''
+    test0()
+
+if __name__ == '__main__':
+    main()
