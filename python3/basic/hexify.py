47292aff7c618784cc9d97040bc4b1551a4e0f51
ericosur <ericosur@gmail.com>
2023-05-11 15:42:12 +0800

    UPDATE scripts
    Signed-off-by: ericosur <ericosur@gmail.com>

---
+++ python3/basic/hexify.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+''' binascii '''
+
+import binascii
+
+def main():
+    ''' main '''
+    hexstr = "e98099e698afe4b880e6aeb5e4b8ade69687e5ad97e794a87574662d38e7b7a8e7a2bc"
+    print(binascii.unhexlify(hexstr))
+
+    data = b"A quick fox jumps over a lazy dog"
+    print(binascii.hexlify(data))
+
+if __name__ == '__main__':
+    main()
---
+++ python3/basic/strcrc32.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+'''
+module binascii example
+'''
+
+import binascii
+
+def main():
+    ''' main '''
+    one = binascii.crc32(b"hello world")
+    print(f'crc1 = 0x{one:08x}')
+    # Or, in two pieces:
+    two = binascii.crc32(b"hello")
+    two = binascii.crc32(b" world", two) & 0xffffffff
+    print(f'crc2 = 0x{two:08x}')
+
+if __name__ == '__main__':
+    main()
