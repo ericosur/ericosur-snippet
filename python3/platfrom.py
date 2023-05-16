3a6a4dd0196cfe82ad98becf994134749c3959ef
ericosur <ericosur@gmail.com>
2023-05-16 09:53:03 +0800

    UPDATE platformdirs module samples
    Signed-off-by: ericosur <ericosur@gmail.com>

--- python3/platfrom.py
+++ python3/platfrom.py
@@ -5,13 +5,43 @@
 
 from __future__ import print_function
 import sys
+import re
+from platformdirs import PlatformDirs
 
-print(f"OS platform is {sys.platform}")
-print("python version:", sys.version_info)
-print('version:', sys.version)
-print("lib path:", sys.path)
-print('executable:', sys.executable)
-print('implementation:', sys.implementation)
+def show_sysplatform():
+	''' show '''
+	print(f"OS platform is {sys.platform}")
+	print("python version:", sys.version_info)
+	#print('version:', sys.version)
+	#print("lib path:", sys.path)
+	#print('executable:', sys.executable)
+	#print('implementation:', sys.implementation)
 
-# too long to print out
-#print('modules:', sys.modules)
+	# too long to print out
+	#print('modules:', sys.modules)
+
+def show_platformdirs():
+	''' module platformdirs '''
+	p = PlatformDirs('myapp', 'acme')
+	print(f'{p.site_cache_dir=}')
+	print(f'{p.site_config_dir=}')
+	print(f'{p.site_data_dir=}')
+	print(f'{p.user_cache_dir=}')
+	print(f'{p.user_config_dir=}')
+	print(f'{p.user_data_dir=}')
+	print(f'{p.user_documents_dir=}')
+	print(f'{p.user_log_dir=}')
+	print(f'{p.user_music_dir=}')
+	print(f'{p.user_pictures_dir=}')
+	print(f'{p.user_runtime_dir=}')
+	print(f'{p.user_state_dir=}')
+	print(f'{p.user_videos_dir=}')
+
+def main():
+	''' main '''
+	show_sysplatform()
+	print()
+	show_platformdirs()
+
+if __name__ == '__main__':
+	main()
