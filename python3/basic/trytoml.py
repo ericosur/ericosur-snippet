d7fd499e9ae84f03139d5ea89c8a36d90d1dfa44
ericosur <ericosur@gmail.com>
2023-10-17 16:52:11 +0800

    UPDATE trytoml.py
    Signed-off-by: ericosur <ericosur@gmail.com>

--- python3/basic/trytoml.py
+++ python3/basic/trytoml.py
@@ -8,18 +8,25 @@ try tomllib or tomli
 try:
     import tomllib
 except ModuleNotFoundError:
+    print('[WARN] no tomllib, try tomli')
     import tomli as tomllib
 
 class Solution():
     ''' class solution '''
+    toml = '''
+# a long string in format TOML
+[main]
+filename = "trytoml.py"
+md5sum = "e5b1f6b7a8aaa5ec34b6b6573fd66f38"
+    '''
     def __init__(self):
         self.value = 0
 
     @staticmethod
     def test():
         ''' test '''
-        s = tomllib.loads("['This parses fine with Python 3.6+']")
-        print(f'test: {s=}')
+        s = tomllib.loads(Solution.toml)
+        print(s)
 
     def action(self):
         ''' action '''
