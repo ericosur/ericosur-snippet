8a49048eb8835270e530b4dfe3a3a4aaea043c74
ericosur <ericosur@gmail.com>
2023-10-25 16:07:04 +0800

    ADD script to calculate ratio between dates
    Signed-off-by: ericosur <ericosur@gmail.com>

---
+++ python3/datetime/ratio_month.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+'''
+ratio between a month
+data plan count from 10/15 to 11/15 (mm/dd)
+get the ratio as a reference for today usage
+
+'''
+
+import time
+from datetime import date, timedelta
+
+def test_dates():
+    ''' test '''
+    r = NextMonth.get_nextmonthdate(date(2023,2,28))
+    assert r == date(2023,3,28)
+    r = NextMonth.get_nextmonthdate(date(2023,1,29))
+    assert r is None
+    r = NextMonth.get_nextmonthdate(date(2024,1,29))
+    assert r == date(2024,2,29)
+    r = NextMonth.get_nextmonthdate(date(2023,11,29))
+    assert r == date(2023,12,29)
+    r = NextMonth.get_nextmonthdate(date(2023,12,29))
+    assert r == date(2024,1,29)
+    r = NextMonth.get_nextmonth15(date(2023,12,29))
+    assert r == date(2024,1,15)
+    r = NextMonth.get_nextmonth15(date(2023,10,25))
+    assert r == date(2023,11,15)
+
+    r = NextMonth.get_next15(date(2023,10,5))
+    assert r == date(2023,10,15)
+    r = NextMonth.get_next15(date(2023,10,14))
+    assert r == date(2023,10,15)
+    r = NextMonth.get_next15(date(2023,10,15))
+    assert r == date(2023,11,15)
+    r = NextMonth.get_next15(date(2023,11,15))
+    assert r == date(2023,12,15)
+    r = NextMonth.get_next15(date(2023,12,15))
+    assert r == date(2024,1,15)
+    r = NextMonth.get_next15(date(2023,12,21))
+    assert r == date(2024,1,15)
+
+class NextMonth():
+    ''' class solution '''
+    def __init__(self):
+        self.today = date.today()
+        self.nextm = NextMonth.get_nextmonthdate(self.today)
+        self.next15 = NextMonth.get_next15(self.today)
+        self.this15 = NextMonth.get_this15(self.today)
+
+    def __str__(self):
+        msg = f'today: {self.today}, next month: {self.nextm}'
+        msg += f', next 15: {self.next15}'
+        return msg
+
+    @staticmethod
+    def get_nextmonthdate(x: date):
+        '''
+        https://stackoverflow.com/questions/2249956/how-to-get-the-same-day-of-next-month-of-a-given-day-in-python-using-datetime
+        '''
+        try:
+            nextmonthdate = x.replace(month=x.month+1)
+        except ValueError:
+            if x.month == 12:
+                nextmonthdate = x.replace(year=x.year+1, month=1)
+            else:
+                # next month is too short to have "same date"
+                # pick your own heuristic, or re-raise the exception:
+                return None
+        return nextmonthdate
+
+    @staticmethod
+    def get_nextmonth15(d: date):
+        ''' get next month 15 '''
+        next15 = None
+        try:
+            next15 = d.replace(month=d.month+1, day=15)
+        except ValueError:
+            if d.month == 12:
+                next15 = d.replace(year=d.year+1, month=1, day=15)
+            else:
+                return None
+        return next15
+
+    @staticmethod
+    def get_next15(d: date):
+        ''' get next 15 '''
+        next15 = None
+        try:
+            if d.day < 15:
+                next15 = d.replace(day=15)
+            else:
+                next15 = d.replace(month=d.month+1, day=15)
+        except ValueError:
+            if d.month == 12:
+                next15 = d.replace(year=d.year+1, month=1, day=15)
+            else:
+                return None
+        return next15
+
+    @staticmethod
+    def get_this15(d: date):
+        ''' get 15 this month '''
+        this15 = None
+        try:
+            this15 = d.replace(day=15)
+        except ValueError:
+            return None
+        return this15
+
+    @staticmethod
+    def getd(start, end):
+        ''' get diff days '''
+        d = start - end
+        print(f'{start} - {end} = {d}')
+        return d
+
+    def action(self):
+        ''' action '''
+        print('action!')
+        td = date(2023, 10, 25)
+        t15 = NextMonth.get_this15(td)
+        n15 = NextMonth.get_nextmonth15(td)
+        curr = self.getd(td, n15)
+        full = self.getd(t15, n15)
+        ratio = curr / full
+        mass = 24 * ratio
+        print(f'at least > {ratio*100:.0f}%, least: {mass:.2f} GB')
+
+    @classmethod
+    def run(cls):
+        ''' run '''
+        obj = cls()
+        print(obj)
+        #test_dates()
+        obj.action()
+
+def main():
+    ''' main '''
+    NextMonth.run()
+
+if __name__ == '__main__':
+    main()
