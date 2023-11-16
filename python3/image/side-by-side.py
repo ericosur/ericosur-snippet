c5c31d07859f3c729e583ea6c9a0903a98e6411a
ericosur <ericosur@gmail.com>
2023-11-16 15:20:46 +0800

    ADD side-by-side.py
    Signed-off-by: ericosur <ericosur@gmail.com>

---
+++ python3/image/side-by-side.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+#coding:utf-8
+
+'''
+use ImageMagick combine 2 image side-by-side
+
+cli:
+magick montage \
+    lhs.png rhs.png \
+    -mode Concatenate -tile x1 \
+    output.png
+
+'''
+
+DEMO = False
+
+import os
+import sys
+from wand.image import Image
+from wand.display import display
+
+class Solution():
+    ''' solution '''
+
+    def __init__(self):
+        home = os.getenv("HOME")
+        self.images = ["padlock1.png", "padlock2.png"]
+        self.src_dir = os.path.join(home, 'Pictures/data')
+        self.ofn = "output.png"
+        self._check_images()
+
+    def __str__(self):
+        return self.ofn
+
+    def _check_images(self):
+        ''' check images exists '''
+        self.fullpaths = [os.path.join(self.src_dir, f) for f in self.images]
+        for f in self.fullpaths:
+            if not os.path.exists(f):
+                print('[FAIL] fail at once, file not found:', f)
+                sys.exit(-1)
+
+    def concatenate_images(self):
+        ''' concatenate images
+        https://docs.wand-py.org/en/0.6.12/guide/montage.html#concatenation-mode
+        '''
+        with Image() as img:
+            for src in self.fullpaths:
+                with Image(filename=src) as item:
+                    img.image_add(item)
+                    img.montage(mode='concatenate')
+                    #img.border('magenta', 2, 2)
+                    img.save(filename=self.ofn)
+        print('concatenate_images: output to:', self.ofn)
+
+    def rotate_and_show(self):
+        ''' rotate and show '''
+        with Image(filename=self.fullpaths[0]) as img:
+            print(img.size)
+            # resize and rotate
+            for r in 1, 2, 3:
+                with img.clone() as i:
+                    i.resize(int(i.width * r * 0.25), int(i.height * r * 0.25))
+                    i.rotate(90 * r)
+                    #i.save(filename=f'mona-lisa-{r}.png')
+                    display(i)
+
+    @classmethod
+    def run(cls):
+        ''' run me '''
+        obj = cls()
+        if DEMO:
+            print('[INFO] press ESC or close the display window to next one')
+            obj.rotate_and_show()
+        else:
+            obj.concatenate_images()
+
+def main():
+    ''' main '''
+    Solution.run()
+
+if __name__ == '__main__':
+    main()
