408aa759522cd5861dd3eab922150b5b04c506f9
ericosur <ericosur@gmail.com>
2023-11-16 10:34:11 +0800

    ADD imamp script, read / write images
    - read image with imageio
    - convert color by opencv
    - write image with opencv

--- python3/image/imamp.py
+++ python3/image/imamp.py
@@ -2,32 +2,49 @@
 #coding: utf-8
 
 '''
-imageio examples
-iterate files in specified dir
+imageio x opencv
+1. iterate files in specified dir
+2. imageio read image file
+3. pass image obj to opencv to write image file
 '''
 
 import os
-import imageio.v3 as iio
 from pathlib import Path
+import imageio.v3 as iio
+import cv2
+
+# do not complain no-member for opencv
+# pylint: disable=no-member
 
 class Solution():
     ''' class solution '''
     def __init__(self):
-        self.home = os.getenv('HOME')
+        home = os.getenv('HOME')
+        self.src_dir = os.path.join(home, 'Pictures/data')
+        self.cnt = 0
 
-    @staticmethod
-    def test():
-        ''' test '''
-        print('test staticmethod')
+    def write_imgfile(self, img):
+        ''' use cv2 to write image '''
+        ofn = f'imamp{self.cnt:03d}.jpg'
+        cv2.imwrite(ofn, img)
+        print('[INFO] cv2.imwrite:', ofn)
+        self.cnt += 1
 
     def action(self):
         ''' action '''
-        #images = list()
-        for file in Path(self.home, "dropbox").iterdir():
+        # iterate files under src_dir
+        for file in Path(self.src_dir).iterdir():
             if not file.is_file():
                 continue
-            print(file)
-        #images.append(iio.imread(file))
+            try:
+                # use imageio to read the file as object img
+                img = iio.imread(file)
+                # need convert RGB to BGR (for cv2)
+                new_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+                self.write_imgfile(new_img)
+            except OSError:
+                # handle non-image files
+                print('[FAIL] maybe not an valid image: ', file)
 
     @classmethod
     def run(cls):
