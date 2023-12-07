04e1d2ed3d3763550a72272925384a95d06edfbb
ericosur <ericosur@gmail.com>
2023-12-07 15:39:20 +0800

    UPDATE aaa.py, add more characters
    Signed-off-by: ericosur <ericosur@gmail.com>

--- python3/aaa.py
+++ python3/aaa.py
@@ -5,23 +5,26 @@
 unicode zjw test
 '''
 
-def main():
-    ''' main test '''
-    # MUST NOT edit the following string if your editor/system
-    # could not handle this correctly
-    s = '❤️🇧🇴🙋‍♀️🏈😃'
-    #line = '"'
+def show_code(s):
+    ''' show code '''
     for cc in s:
         hx = hex(ord(cc))  # str, 0x1f1e7
 
         hx = hx.upper()
         hx = hx.replace('0X', '\\u')
-        print(hx)
+        print(f'{cc:8s}{hx:12s}{cc.encode()}')
 
-        print(cc.encode())
+def main():
+    ''' main test '''
+    # MUST NOT edit the following string if your editor/system
+    # could not handle this correctly
+    s = '❤️🇧🇴🙋‍♀️🏈😃'
+    print(s)
+    show_code(s)
 
-    #line += '"'
-    #print(line)
+    s = '龍龖龘' + '\U0002A6A5'
+    print(s)
+    show_code(s)
 
 if __name__ == '__main__':
     main()
