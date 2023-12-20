8f8429a3611fc0cee47cae1ceac30c1fa2ce6da0
ericosur <ericosur@gmail.com>
2023-12-20 17:11:40 +0800

    UPDATE ngorgo.py
    Signed-off-by: ericosur <ericosur@gmail.com>

--- python3/datetime/ngorgo.py
+++ python3/datetime/ngorgo.py
@@ -12,10 +12,16 @@ except ValueError:
 
 '''
 
+import argparse
 import re
 import os
 import sys
 from datetime import date, timedelta
+
+SUCCESS = 0
+FILENOTFOUND = 1
+NOTOLDENOUGH = 2
+CANNOTPARSE = 3
 
 class Solution():
     ''' class to parse and action '''
@@ -36,14 +42,23 @@ class Solution():
         self.inputfn = ''
         self.lastdate = None
         self.offset = timedelta(days=Solution.MINDIFF)
+        self.quiet = False
+        self.verbose = False
+
+    def set_quiet(self, yesno):
+        ''' set quiet '''
+        self.quiet = yesno
 
     def set_stampfile(self, p):
         ''' set path of stamp file '''
+        if not self.quiet:
+            print('set_stampfile:', p)
         self.inputfn = p
 
     def get_thelastine(self):
         ''' get the last line with content of a file '''
-        print(self.inputfn)
+        if self.verbose:
+            print(f'parsing: {self.inputfn}')
         lastline = None
         with open(self.inputfn, 'rt', encoding='UTF-8') as fobj:
             for l in fobj.readlines():
@@ -58,7 +73,7 @@ class Solution():
         if m:
             # m[1]: year, m[2]: month, m[3]: day
             self.lastdate = date(year=int(m[1]),month=int(m[2]),day=int(m[3]))
-            print(self.lastdate)
+            #print(self.lastdate)
             return self.lastdate
 
         #print('[warn] cannot parse with format1...')
@@ -87,19 +102,21 @@ class Solution():
             # m[1]: month name, m[2]: dd, m[3]: yyyy
             self.compose_date_obj(m[3], m[1], m[2])
 
-        print(self.lastdate)
+        #print(self.lastdate)
         return self.lastdate
 
 
     def parse_stampfile(self):
         ''' parse stamp file '''
+        if not os.path.exists(self.inputfn):
+            print('[FAIL] file not found:', self.inputfn)
+            sys.exit(FILENOTFOUND)
         ln = self.get_thelastine()
-        print(ln)
+        #print(ln)
         still_try = True
         try_count = 0
-        while still_try:
-            if try_count > 3:
-                break
+        self.lastdate = None
+        while still_try and try_count < 2:
             d = self.parse_date_format1(ln)
             try_count += 1
             if d:
@@ -109,43 +126,72 @@ class Solution():
             if d:
                 break
 
+    def show_msg(self, msg):
+        ''' show message '''
+        if not self.quiet:
+            print(msg)
 
     def action(self):
         ''' action '''
+        #self.show_msg('action')
         self.parse_stampfile()
         if self.lastdate:
             if self.lastdate >= date.today() - self.offset:
-                print('[INFO] rather new, no need to bother')
-                sys.exit(-2)
+                self.show_msg(f'[INFO] {self.lastdate} rather new, no need to bother')
+                sys.exit(NOTOLDENOUGH)
             else:
-                print('[INFO] old enough, suggest you refresh folder')
-                sys.exit(0)
-        print('fail to compare')
-        sys.exit(-1)
+                self.show_msg(f'[INFO] {self.lastdate} old enough, ok to refresh')
+                return
+        self.show_msg('[fail] cannot parse the last date')
+        sys.exit(CANNOTPARSE)
 
 
     @classmethod
     def run(cls, argv=None):
         ''' run me '''
-        if not argv:
-            # give a default value
-            argv = os.path.join('.', cls.TS)
-
-        if not os.path.exists(argv):
-            print('[fail] specified path not found:', argv)
-            sys.exit(-1)
-
         obj = cls()
         obj.set_stampfile(argv)
         obj.action()
 
+def start_solution(files, isquiet):
+    ''' control flow '''
+    # if not isquiet:
+    #     print('files:', files)
+    obj = Solution()
+    obj.set_quiet(isquiet)
+
+    if len(files) < 1:
+        # if not isquiet:
+        #     print('give a default value...')
+        files.append(os.path.join('.', Solution.TS))
+
+    for f in files:
+        obj.set_stampfile(f)
+        obj.action()
+
 def main():
     ''' main '''
-    # pass full path of latest.txt
-    if len(sys.argv) > 1:
-        Solution.run(sys.argv[1])
-    else:
-        Solution.run()
+    parser = argparse.ArgumentParser(description='from given latest.txt, tell if refresh or not')
+    # nargs like regexp, '*' means 0+, '+' means 1+
+    parser.add_argument("files", metavar='fn', type=str, nargs='*',
+        help="file1 file2 file3 ...")
+    #parser.add_argument('-o', '--output', help='Output file name', default='stdout')
+    parser.add_argument("-q", "--quiet", action='store_true', default=False,
+        help='quiet, use return code to judge')
+    parser.add_argument("-v", "--verbose", action='store_true', default=False,
+        help='verbose mode')
+    # define the required args
+    #requiredNamed = parser.add_argument_group('required named arguments')
+    #requiredNamed.add_argument('-i', '--input', help='Input file name', required=True)
+
+    #parser.parse_args(['-i input.txt -o out.txt str1 str2'])
+
+    args = parser.parse_args()
+
+    start_solution(args.files, args.quiet)
+
+    # to show help message directly
+    #parser.print_help()
 
 if __name__ == '__main__':
     main()
