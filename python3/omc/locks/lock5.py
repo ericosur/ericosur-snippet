a0ba64e282e6ac958a69dbd419f8801539d3c4dc
ericosur <ericosur@gmail.com>
2024-02-01 18:04:46 +0800

    ADD lock5.py and unlock.pdf
    Signed-off-by: ericosur <ericosur@gmail.com>

---
+++ python3/omc/locks/lock5.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# coding: utf-8
+#
+
+'''
+5 pirates, 10 locks, only 3 guys could unlock all locks
+each guy takes 6 keys
+'''
+
+from itertools import combinations
+
+class Solution():
+    ''' to solve '''
+    def __init__(self):
+        self.locks = [0 for _ in range(10)]
+
+
+    def action(self):
+        ''' action '''
+        print('action!')
+        print(self.locks)
+
+    @classmethod
+    def run(cls):
+        ''' run '''
+        obj = cls()
+        obj.action()
+
+def main() -> None:
+    ''' main '''
+    print(__doc__)
+    Solution.run()
+
+if __name__ == '__main__':
+    main()
