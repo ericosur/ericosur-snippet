cd1f7c567a20fecd6c12a10f2c204492b4fc0082
ericosur <ericosur@gmail.com>
2024-03-07 15:21:10 +0800

    ADD script clockemoji.py
    Signed-off-by: ericosur <ericosur@gmail.com>

---
+++ python3/datetime/clockemoji.py
@@ -0,0 +1,163 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+'''
+show an emoji that matches the current time
+'''
+
+from datetime import datetime
+
+def clamp(value, minimum, maximum):
+    """Clamps a value between a minimum and maximum value.
+
+    Args:
+      value: The value to be clamped.
+      minimum: The minimum allowed value.
+      maximum: The maximum allowed value.
+
+    Returns:
+      The clamped value, which is within the range [minimum, maximum].
+    """
+    return max(minimum, min(value, maximum))
+
+
+class ShowClock():
+    ''' class to show clock '''
+    clocks = {
+        "0100": "🕐",
+        "0200": "🕑",
+        "0300": "🕒",
+        "0400": "🕓",
+        "0500": "🕔",
+        "0600": "🕕",
+        "0700": "🕖",
+        "0800": "🕗",
+        "0900": "🕘",
+        "1000": "🕙",
+        "1100": "🕚",
+        "1200": "🕛",
+        "0130": "🕜",
+        "0230": "🕝",
+        "0330": "🕞",
+        "0430": "🕟",
+        "0530": "🕠",
+        "0630": "🕡",
+        "0730": "🕢",
+        "0830": "🕣",
+        "0930": "🕤",
+        "1030": "🕥",
+        "1130": "🕦",
+        "1230": "🕧",
+    }
+
+    def __init__(self):
+        self.hh = None
+        self.mm = None
+        self._whatnow()
+
+    def setmm(self, m):
+        ''' set m '''
+        if 0 <= m <= 59:
+            self.mm = m
+        else:
+            raise ValueError
+
+    def sethh(self, h):
+        ''' set h '''
+        if h == 0:
+            h = 12
+        if 12 < h <= 23:
+            h = h - 12
+        if 1 <= h <= 12:
+            self.hh = h
+            #print(f'sethh: {self.hh=}')
+        else:
+            raise ValueError
+
+    def _whatnow(self):
+        ''' get current hour and minute (type: int)
+            hh (0 to 23)
+            mm (0 to 59)
+        '''
+        now = datetime.now()
+        # Extract hour and minute
+        self.sethh(now.hour)
+        self.setmm(now.minute)
+        assert 0 <= self.mm <= 59
+        assert 1 <= self.hh <= 12
+
+
+    def _step_hour(self):
+        ''' 1 to 2, 2 to 3, 11 to 12, 12 to 1
+            will not change the current time
+        '''
+        hh = self.hh + 1
+        if hh > 12:
+            hh = 1
+        assert 0 < hh <= 12
+        return hh
+
+    def choose_icon(self):
+        ''' choose an icon '''
+        m = self.mm
+        h = self.hh
+        if 0 <= m <= 15:
+            # if 1415 use 1400 icon
+            m = 0
+        elif 16 <= m < 45:
+            # if 1623 use 1630 icon
+            # if 1342 use 1330 icon
+            m = 30
+        else:
+            m = 0
+            h = self._step_hour()
+        return f'{h:02d}{m:02d}'
+
+    def get_icon(self):
+        ''' return icon '''
+        return self.clocks.get(self.choose_icon())
+
+    def test(self, hh, mm):
+        ''' test '''
+        self.sethh(hh)
+        self.setmm(mm)
+        r = self.choose_icon()
+        i = self.get_icon()
+        print(f'{hh:02d}{mm:02d} ==> {r}\t{i}')
+
+
+    def do_tests(self):
+        ''' run tests '''
+        #print(f"Current hour: {self.hh}, minute: {self.mm}")
+        #self.choose_icon()
+        self.test(0, 0)
+        self.test(0, 14)
+        self.test(0, 15)
+        self.test(0, 16)
+        self.test(0, 29)
+        self.test(0, 30)
+        self.test(0, 31)
+        self.test(0, 44)
+        self.test(0, 45)
+        self.test(0, 46)
+        self.test(12, 15)
+        self.test(12, 16)
+        self.test(12, 45)
+        self.test(12, 46)
+
+    def action(self):
+        ''' action '''
+        print(self.get_icon())
+
+    @classmethod
+    def run(cls):
+        ''' run me '''
+        obj = cls()
+        obj.action()
+
+def main():
+    ''' main '''
+    ShowClock.run()
+
+if __name__ == '__main__':
+    main()
