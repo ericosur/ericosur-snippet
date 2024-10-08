7560964ec8d6ab8a71194f2dbbe0a0a19f24b2de
ericosur <ericosur@gmail.com>
2024-10-08 17:09:58 +0800

    UPDATE ly.py for output
    Signed-off-by: ericosur <ericosur@gmail.com>

--- python3/yaml/ly.py
+++ python3/yaml/ly.py
@@ -4,7 +4,7 @@
 ''' loading yaml '''
 
 import json
-
+# pip install pyyaml
 import yaml
 
 
@@ -13,8 +13,12 @@ def main():
     with open('invoice.yaml', 'rt', encoding='utf8') as f:
         #data = yaml.load(f)
         data = yaml.load(f, Loader=yaml.FullLoader)
-    print(data)
-    print('type:', type(data))
+
+    print('type:', type(data), '-' * 20)
+    for k,v in data.items():
+        print(f'{k}: {v}')
+
+    print('try to use json.dumps(data)', '-' * 20)
     try:
         # The data type "date" could not be converted from yaml to json directly
         print(json.dumps(data, indent=2, sort_keys=False))
