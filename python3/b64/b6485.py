178b3518579b6dd58743adfd30e1d26fc04218ac
ericosur <ericosur@gmail.com>
2024-10-24 17:13:14 +0800

    UPDATE b6485.py
    Signed-off-by: ericosur <ericosur@gmail.com>

--- python3/b64/b6485.py
+++ python3/b64/b6485.py
@@ -6,6 +6,8 @@ demo for the following base-N functions:
     base58
     base64
     base85
+
+reference: https://docs.python.org/zh-tw/3/library/base64.html
 '''
 
 import base64
@@ -20,6 +22,11 @@ try:
 except ImportError:
     print('WARN: cannot import module: base58', file=sys.stderr)
 
+
+def is_py310plus() -> bool:
+    ''' return python version in (major, minor) integers '''
+    return sys.version_info[0]==3 and sys.version_info[1]>=10
+
 def int_to_bytes(x: int) -> bytes:
     ''' int to bytes '''
     return x.to_bytes((x.bit_length() + 7) // 8, 'big')
@@ -31,13 +38,21 @@ def int_from_bytes(xbytes: bytes) -> int:
 
 def fill_bytearray(size: int = 24) -> bytes:
     ''' fill byte array '''
+    show("size", size)
     return np.random.bytes(size)
 
 def show(m, n):
     ''' show '''
-    print(f'{m:<14s}: {n}')
+    s = None
+    if isinstance(n, bytes):
+        s = n.decode('UTF-8')
+    elif isinstance(n, str):
+        s = n
+    else:
+        s = str(n)
+    print(f'{m:<16s}: {s}')
 
-def testb85(v: bytes):
+def demo85(v: bytes):
     ''' test base85 '''
     print('-' * 60)
     r = base64.a85encode(v) # r is bytes
@@ -49,6 +64,25 @@ def testb85(v: bytes):
         r = base58.b58encode(v)
         show('base58', r)
 
+def demo64(v: bytes):
+    ''' demo base64 '''
+    b0 = base64.standard_b64encode(v)
+    b1 = base64.b64encode(v)
+    b2 = base64.urlsafe_b64encode(v)
+
+    if b0 != b1:
+        show('std base64', b0)
+    show('base64', b1)
+
+    if b1 != b2:
+        show('urlsafe base64', b2)
+
+def demo32(v: bytes):
+    ''' demo base32 '''
+    show("base32", base64.b32encode(v))
+    if is_py310plus():
+        show("base32hex", base64.b32hexencode(v))
+
 def test(v: bytes):
     ''' test '''
     #hx = binascii.hexlify(v)    # bytes: b'([0-9a-f][0-9a-f])+'
@@ -58,26 +92,19 @@ def test(v: bytes):
     #b16 = base64.b16encode(v)   # bytes: b'([0-9A-F][0-9A-F])+'
     #show('b16encode', b16)
     print('-' * 60)
-
-    r0 = base64.standard_b64encode(v)
-    r1 = base64.b64encode(v)
-    r2 = base64.urlsafe_b64encode(v)
-
-    if r0 != r1:
-        show('std base64', r0)
-    show('base64', r1)
-
-    if r1 != r2:
-        show('urlsafe base64', r2)
+    # base32
+    demo32(v)
+    # base64
+    demo64(v)
     # base85
-    testb85(v)
+    demo85(v)
 
 
 def main(argv):
     ''' main '''
     if argv == []:
         for _ in range(1):
-            x = fill_bytearray(randint(10, 40))
+            x = fill_bytearray(19)
             argv.append(x)
 
     for e in argv:
