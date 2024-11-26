48587c449dc5ebaca8f110e16858c5c3f86161ac
ericosur <ericosur@gmail.com>
2024-11-26 16:34:56 +0800

    UPDATE print style
    Signed-off-by: ericosur <ericosur@gmail.com>

--- python3/euc_dist.py
+++ python3/euc_dist.py
@@ -4,14 +4,26 @@
 ''' euclidean distance '''
 
 import numpy as np
+USE_RICH = False
+try:
+    from rich.console import Console
+    USE_RICH = True
+except ImportError:
+    pass
 
+def show(m, d):
+    ''' show '''
+    if USE_RICH:
+        console = Console()
+        console.log(f'[red]{m}[/]: {d}')
+    else:
+        print(f'{m}: {d}')
 
 def main():
     ''' main '''
     m = np.array([3, 4])
-
     d = np.linalg.norm(m)
-    print(d)
+    show(m, d)
 
 if __name__ == '__main__':
     main()
