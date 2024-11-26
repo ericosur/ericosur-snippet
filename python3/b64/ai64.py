04c643ee73ff2f956ea0befc4db25013247ef632
ericosur <ericosur@gmail.com>
2024-11-26 15:41:01 +0800

    UPDATE ai64.py
    Signed-off-by: ericosur <ericosur@gmail.com>

--- python3/b64/ai64.py
+++ python3/b64/ai64.py
@@ -13,21 +13,25 @@ md5sum ?.txt
 
 import base64
 import os
+import stat
+from typing_extensions import Annotated
+import typer
 
 # if 57, one line 76 characters
 # use 3n to avoid padding issues (4 char from 3 bytes)
 CHUNK_SIZE = 57
 
-import os
-import stat
 from loguru import logger
 
-logd = logger.debug
-logi = logger.info
+def do_nothing(*args):
+    ''' do nothing '''
+    return args
 
-def is_chardev(filepath) -> bool:
+def is_chardev(filepath, log=do_nothing) -> bool:
     ''' return if a char dev '''
+    logd = log
     mode = os.stat(filepath).st_mode  # Get the file's mode
+    logd(f'{mode=}')
     if stat.S_ISCHR(mode):
         return True
     # if stat.S_ISREG(mode): return "Regular file"
@@ -41,18 +45,22 @@ def is_chardev(filepath) -> bool:
     # except PermissionError: return "Permission denied"
     return False
 
-def encode_file_to_base64_chunked(file_path, chunk_size=CHUNK_SIZE):
+def encode_file_to_base64_chunked(file_path, chunk_size=CHUNK_SIZE, log=do_nothing):
     ''' read file into chunks, and base64 encoding chunk by chunk
         it is iterable
     '''
+    logd = log
     max_repeat, cnt = -1, 0
+    logd(f'{file_path=}')
+    logd(f'{chunk_size=}')
     # if char dev, repeat in limited count
     if is_chardev(file_path):
         max_repeat = 2
 
     with open(file_path, "rb") as file:
         while chunk := file.read(chunk_size):
-            if max_repeat>0 and cnt > max_repeat:
+            if 0 < max_repeat < cnt:
+                logd("reach the max repeat, break...")
                 break
             # Encode the current chunk to Base64
             base64_chunk = base64.b64encode(chunk)
@@ -60,12 +68,35 @@ def encode_file_to_base64_chunked(file_path, chunk_size=CHUNK_SIZE):
             yield base64_chunk.decode('utf-8')
             cnt += 1
 
-def main():
-    ''' main '''
-    # Usage example: print each chunk of Base64 encoded data
+def main(
+        input_fn: Annotated[str, typer.Option("--input", "-i", help="specify input file")]=None,
+        debug: Annotated[bool, typer.Option("--debug", "-d", help="show debug info")]=False,
+        chunk: Annotated[int, typer.Option("--chunk", "-c",
+                                    help="specify the size of chunk")] = 57,
+):
+    '''
+    print each chunk of Base64 encoded data
+    Usage example:
+
+    dd if=/dev/urandom of=abc.bin count=57 bs=1
+
+    ai64.py --input abc.bin
+
+    RZv+frUlOjplVto6Bwdjn2ZXr/2ARvV9ZX+LaFgkgLfDDtyplU+IogelNQXu77dsC5Z61pXFLWy5
+    '''
+
+    logd = do_nothing
+    if debug:
+        logd = logger.debug
+
     fn = "/dev/urandom"
-    for base64_chunk in encode_file_to_base64_chunked(fn):
+    #logd(f'{fn=}')
+    if input_fn and os.path.isfile(input_fn):
+        fn = input_fn
+        logd(fn)
+
+    for base64_chunk in encode_file_to_base64_chunked(fn, chunk_size=chunk, log=logd):
         print(base64_chunk)
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
