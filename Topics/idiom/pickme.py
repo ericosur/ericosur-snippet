4ed7f71ff4781059ae1d363b9c8d854defbce7cd
ericosur <ericosur@gmail.com>
2024-11-27 16:28:22 +0800

    UPDATE idiom scripts
    Signed-off-by: ericosur <ericosur@gmail.com>

--- Topics/idiom/pickme.py
+++ Topics/idiom/pickme.py
@@ -2,11 +2,12 @@
 #coding: utf-8
 
 '''
-choice
+grep 4-character idioms from IDIOM_BOPOMOFO
 '''
 
 from random import randint
 from idiom_list import IDIOM_BOPOMOFO as idioms
+from numer4_idioms import FourCharIdioms as id4iom
 from rich.console import Console
 console = Console()
 logd = console.log
@@ -14,9 +15,9 @@ logd = console.log
 class ChoiceIdiom():
     ''' pick some idioms from list '''
     def __init__(self):
-        self.text0 = []
-        self.text4 = []
-        logd(f'{len(idioms)=}')
+        self.text0 = [] # not-4-char idioms here
+        self.text4 = [] # 4-character idioms here
+        logd(f'total idioms: {len(idioms)=}')
         self.__seperate4()
 
     @classmethod
@@ -36,25 +37,35 @@ class ChoiceIdiom():
         self.text4.sort()
         len4 = len(self.text4)
         len0 = len(self.text0)
-        logd(f"{len4=}")
-        logd(f"{len0=}")
+        logd(f"4-character idioms: {len4}")
+        logd(f"other idioms: {len0}")
         assert len(idioms)==len4+len0
 
-    def get_some_4c(self):
-        ''' get some 4 character-idiom '''
-        repeat = 10
+    @staticmethod
+    def get_some_items_from_list(the_list: list, repeat: int=10) -> list:
+        ''' from the_list get some items '''
         ans = []
-        tmp = self.text4.copy()
+        tmp = the_list.copy()
+        logd(f'will return {repeat} idioms...')
         while len(ans) < repeat and len(tmp) > 1:
             rint = randint(0, len(tmp)-1)
-            ret = tmp.pop(rint)
+            i = tmp.pop(rint)
             # only pick 4-han-character idioms
-            if len(ret[0]) == 4:
-                ans.append(ret)
-            else:
-                logd(f'not match: {ret}')
+            ans.append(i)
         ans.sort()
-        logd(ans)
+        return ans
+
+    def get_some_idioms(self) -> None:
+        ''' get some idioms '''
+        def inner(msg:str, the_list:list, n:int) -> None:
+            ''' inner '''
+            logd(msg)
+            r = self.get_some_items_from_list(the_list, n)
+            logd(r)
+
+        inner('get some item from self.text4...', self.text4, 4)
+        inner('get some item from self.text0...', self.text0, 3)
+        inner('get some item from id4iom...', id4iom, 5)
 
     def show0(self):
         ''' show text0 '''
@@ -62,9 +73,8 @@ class ChoiceIdiom():
 
     def action(self):
         ''' action '''
-        logd("action...")
-        self.show0()
-        self.get_some_4c()
+        #self.show0()
+        self.get_some_idioms()
 
 def main():
     ''' main '''
