fe53c32e5798c3d3a56aae6a50daf8b560b5e130
ericosur <ericosur@gmail.com>
2024-12-20 12:50:28 +0800

    UPDTE ly.py
    Signed-off-by: ericosur <ericosur@gmail.com>

--- python3/yaml/ly.py
+++ python3/yaml/ly.py
@@ -4,26 +4,56 @@
 ''' loading yaml '''
 
 import json
-# pip install pyyaml
-import yaml
+import os
+import sys
+from typing import Union
+sys.path.insert(0, "..")
+sys.path.insert(0, "python3/")
+from myutil import prt
 
+try:
+    import yaml
+except ImportError as e:
+    prt("cannot import module:", e)
+    prt('pip install PyYAML')
+    sys.exit(1)
+
+def try_location(fn: str) -> Union[str, None]:
+    ''' try to find file from current location and then
+        same location of current script
+
+        fn: only filename, without path
+    '''
+    dirn = os.path.dirname(os.path.abspath(__file__))
+    newf = os.path.join(dirn, fn)
+    if os.path.isfile(newf):
+        return newf
+    return None
 
 def main():
     ''' main '''
-    with open('invoice.yaml', 'rt', encoding='utf8') as f:
+    fn = 'invoice.yaml'
+    if not os.path.isfile(fn):
+        fn = try_location(fn)
+        if fn is None:
+            prt(f'cannot load data file ({fn}), exit...')
+            sys.exit(1)
+    prt(f'load: {fn}')
+    with open(fn, 'rt', encoding='utf8') as f:
         #data = yaml.load(f)
         data = yaml.load(f, Loader=yaml.FullLoader)
 
-    print('type:', type(data), '-' * 20)
+    prt(f'type of date: {type(data)}')
+    prt('-'*20)
     for k,v in data.items():
-        print(f'{k}: {v}')
+        prt(f'{k}: {v}')
 
-    print('try to use json.dumps(data)', '-' * 20)
+    prt('try to use json.dumps(data) =====>')
     try:
         # The data type "date" could not be converted from yaml to json directly
-        print(json.dumps(data, indent=2, sort_keys=False))
-    except TypeError as te:
-        print('TypeError:', te)
+        prt(json.dumps(data, indent=2, sort_keys=False))
+    except TypeError as e:
+        prt('TypeError:', e)
 
 if __name__ == '__main__':
     main()
