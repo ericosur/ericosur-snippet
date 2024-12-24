a3a766fdf3e8d8626b382d1950b0a1d8848eb0ae
ericosur <ericosur@gmail.com>
2024-12-24 09:17:31 +0800

    UPDATE finetune output messages
    Signed-off-by: ericosur <ericosur@gmail.com>

--- python3/basic/sum.py
+++ python3/basic/sum.py
@@ -9,9 +9,8 @@ np.arange and np.sum
 
 from time import perf_counter as pc
 from time import sleep
-
+from functools import reduce
 import numpy as np
-
 
 class Solution():
     ''' basic flow '''
@@ -44,14 +43,14 @@ class Solution():
         self.duration = pc() - start
         self.print_out()
 
-
 class SumUp(Solution):
     ''' different method for sum '''
-    def __init__(self, max_num=9999999):
+    def __init__(self, max_num=9_999_999):
         super().__init__(max_num)
 
     def sum1(self):
         ''' using for-loop to sum up '''
+        print("sum1: for-loop to sum up")
         s = 0
         for i in range(self.max_num + 1):
             s += i
@@ -59,14 +58,31 @@ class SumUp(Solution):
 
     def sum2(self):
         ''' using list() and sum() '''
+        print("sum2: built-in sum(List[int])")
         arr = list(range(self.max_num + 1))
         return sum(arr)
 
     def sum3(self):
         ''' using np.sum '''
+        print("sum3: numpy.sum")
         # np.arange fill an array with inc/dec numbers
         arr = np.arange(1, self.max_num + 1)
         return np.sum(arr)
+
+    def sum4(self):
+        ''' using list comprehension '''
+        print("sum4: list comprehension")
+        return sum([i for i in range(self.max_num + 1)])
+
+    def sum5(self):
+        ''' using generator expression '''
+        print("sum5: generator expression")
+        return sum(i for i in range(self.max_num + 1))
+
+    def sum6(self):
+        ''' using functools.reduce '''
+        print("sum6: functools.reduce")
+        return reduce(lambda x, y: x + y, range(self.max_num + 1))
 
     def test(self):
         ''' test '''
@@ -74,7 +90,9 @@ class SumUp(Solution):
         self.do_something(self.sum1)
         self.do_something(self.sum2)
         self.do_something(self.sum3)
-
+        self.do_something(self.sum4)
+        self.do_something(self.sum5)
+        self.do_something(self.sum6)
 
 def main():
     '''main function'''
