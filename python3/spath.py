b0b855c67ab9f9bfe47844439f810f97c339f379
ericosur <ericosur@gmail.com>
2025-02-18 12:52:29 +0800

    REFACTOR spath.py for different modules to use
    Signed-off-by: ericosur <ericosur@gmail.com>

--- python3/spath.py
+++ python3/spath.py
@@ -14,67 +14,123 @@ for ansi color (optional):
 # single line alias version:
 # alias path='echo $PATH | sed "s/:/\n/g"'
 
+import argparse
 import os
-import sys
+from sysconfig import get_platform
+from typing import Any
 
-DEBUG = False
-USE_RICH = False
-USE_ANSICOLOR = False
-
-def logd(*args, **wargs):
-    ''' logd '''
-    if DEBUG:
-        print(*args, **wargs, file=sys.stderr)
+# while import stage, do not use logd
+DIAG_IMPORT = False
+HAS_RICH = False
+HAS_ANSICOLOR = False
 
 try:
     import rich
-    USE_RICH = True
+    HAS_RICH = True
 except ImportError:
-    logd("no rich, use __pip install rich__")
+    if DIAG_IMPORT:
+        print("no rich, use __pip install rich__")
 
-if not USE_RICH:
-    try:
-        from colorama import Fore
-        from colorama import init as color_init
-        USE_ANSICOLOR = True
-    except ImportError:
-        logd("no colorama, use __pip install colorama__")
-else:
-    Fore = None
-    color_init = None
+try:
+    from colorama import Fore
+    from colorama import init as color_init
+    HAS_ANSICOLOR = True
+except ImportError:
+    if DIAG_IMPORT:
+        print("no colorama, use __pip install colorama__")
 
-logd(f'{USE_RICH=}')
-logd(f'{USE_ANSICOLOR=}')
+try:
+    from loguru import logger
+    HAS_LOGGER = True
+except ImportError:
+    HAS_LOGGER = False
 
 def colorlog(color, msg):
     ''' color log '''
-    if USE_ANSICOLOR:
+    if HAS_ANSICOLOR:
         print(color+msg)
     else:
         print(msg)
+
+def do_nothing(*_args, **_wargs) -> None:
+    ''' do nothing '''
+    return None
 
 class PathLister():
     ''' list path '''
     STR_DUP = '-'*20 + ' duplicated ' + '-'*20
     STR_NG = '-'*20 + ' not found ' + '-'*20
-    def __init__(self):
-        self.dirs = []
-        self.ngs = []
-        self.dups = []
+
+    def __init__(self, args: Any):
+        self.args = args
+        self.alldirs: dict[str, list[str]] = {'dirs': [], 'ngs': [], 'dups': []}
+        if args.debug:
+            self.logd = logger.debug if HAS_LOGGER else print
+        else:
+            self.logd = do_nothing
         self.__check_path__()
+        self.is_windows = None
+        self.is_rdp = None
+        self.__check_platform__()
+        self.use_rich = False
+        self.use_ansi = False
+        self.__determine_color__()
+        if args.debug:
+            self.dump_vars()
+
+    def __determine_color__(self):
+        ''' determine which color library to use
+            use rich first, then colorama, then normal print
+        '''
+        logd = self.logd
+        use_rich = self.args.rprint
+        use_ansi = self.args.ansi
+        if use_rich:
+            if HAS_RICH:
+                logd("has rich, and apply")
+                self.use_rich = True
+            else:
+                logd("request rich, but no rich")
+        if use_ansi:
+            if HAS_ANSICOLOR:
+                logd("has ansi, and apply")
+                self.use_ansi = True
+            else:
+                logd("request ansi, but no ansi to use")
+
+    def __check_platform__(self):
+        ''' check platform '''
+        logd = self.logd
+        platform = get_platform()
+        logd(f'platform: {platform}')
+        self.is_windows = "win" in platform
+        if self.is_windows:
+            _s = os.getenv("SESSIONNAME")
+            if _s:
+                logd(f'session: {_s}')
+                self.is_rdp = "RDP" in _s
+
+    def dump_vars(self):
+        ''' dump vars '''
+        args = self.args
+        logd = self.logd
+        logd(f'args: {args}')
 
     @classmethod
     def run(cls):
         ''' run '''
-        obj = cls()
+        args = do_parser()
+        obj = cls(args)
         obj.action()
 
     def action(self):
         ''' acton '''
-        if USE_RICH:
+        # use rich
+        if self.use_rich:
             self.report_in_rich()
             return
-        if USE_ANSICOLOR:
+        # use colorama
+        if self.use_ansi:
             self.report_in_colorama()
             return
         self.report()
@@ -86,57 +142,69 @@ class PathLister():
             if d == '':
                 continue
             if os.path.isdir(d):
-                if d in self.dirs: # duplicates
-                    self.dups.append(d)
+                if d in self.alldirs['dirs']: # duplicates
+                    self.alldirs['dups'].append(d)
                 else:
-                    self.dirs.append(d)
+                    self.alldirs['dirs'].append(d)
             else:
-                self.ngs.append(d)
+                self.alldirs['ngs'].append(d)
 
     def report(self):
         ''' report '''
-        for d in self.dirs:
+        for d in self.alldirs['dirs']:
             print(d)
-        if self.dups:
+        if self.alldirs['dups']:
             print(PathLister.STR_DUP)
-            for d in self.dups:
+            for d in self.alldirs['dups']:
                 print(d)
-        if self.ngs:
+        if self.alldirs['ngs']:
             print(PathLister.STR_NG)
-            for d in self.ngs:
+            for d in self.alldirs['ngs']:
                 print(d)
 
     def report_in_colorama(self):
         ''' use colorama '''
-        logd('report_in_colorama')
+        #logd = self.logd
         color_init(autoreset=True)
-        for d in self.dirs:
-            print(d)
+        for d in self.alldirs['dirs']:
+            colorlog(Fore.GREEN, d)
         # not found
-        for d in self.ngs:
+        for d in self.alldirs['ngs']:
             colorlog(Fore.RED, f'[NOT FOUND] {d}')
         # warn: duplicated
-        for d in self.dups:
-            colorlog(Fore.YELLOW, f'[DUPLICATED] {d}')
+        if self.alldirs['dups']:
+            colorlog(Fore.YELLOW, PathLister.STR_DUP)
+            for d in self.alldirs['dups']:
+                colorlog(Fore.YELLOW, f'[DUPLICATED] {d}')
 
     def report_in_rich(self):
         ''' in rich '''
+        logd = self.logd
         logd('will use rich')
         rp = rich.print
-        for d in self.dirs:
-            rp(d)
-        if self.dups:
-            rp('[bold yellow]'+self.STR_DUP)
-            for d in self.dups:
+        for d in self.alldirs['dirs']:
+            rp('[green]'+d)
+        if self.alldirs['dups']:
+            rp('[yellow]'+self.STR_DUP)
+            for d in self.alldirs['dups']:
                 rp(d)
-        if self.ngs and len(self.ngs)>0:
+        if self.alldirs['ngs'] and len(self.alldirs['ngs'])>0:
             rp('[bold red]'+self.STR_NG)
-            for d in self.ngs:
+            for d in self.alldirs['ngs']:
                 rp(d)
 
-def main():
-    ''' main '''
-    PathLister.run()
+def do_parser() -> Any:
+    ''' make parser '''
+    parser = argparse.ArgumentParser(description='chataes demo encrypt/decrypt')
+    # nargs like regexp, '*' means 0+, '+' means 1+
+    parser.add_argument("-d", "--debug", dest='debug', action='store_true',
+        default=False, help='debug mode')
+    parser.add_argument("-r", "--rprint", dest='rprint', action='store_true',
+        default=False, help='requst rich')
+    parser.add_argument("-a", "--ansi", dest='ansi', action='store_true',
+        default=False, help='request ansi')
+    args = parser.parse_args()
+    return args
 
 if __name__ == '__main__':
-    main()
+    PathLister.run()
