6464942949579120cd74ed25ebc75107dccda0f4
ericosur <ericosur@gmail.com>
2025-03-07 13:46:44 +0800

    UPDATE list_old_folder.py
    - refactor
    - lint
    - change the output
    
    Signed-off-by: ericosur <ericosur@gmail.com>

--- python3/datetime/list_old_folder.py
+++ python3/datetime/list_old_folder.py
@@ -12,56 +12,72 @@ import glob
 import os
 import sys
 import time
-
+sys.path.insert(0, '.')
+sys.path.insert(0, '..')
+from myutil import prt
 
 # arg: 0 is list, 1 is detail
-def showlist(folders, arg=1):
+def showlist(folders: list, days: int=90, verbose: bool=False) -> None:
     '''
     input: a list with name of folders
     output: it would output the folders which is older than 30 days
     '''
     # timedelta for 30 days
-    offset = datetime.timedelta(days=30)
-    #print offset
+    offset = datetime.timedelta(days=days)
 
     # get today
     today = datetime.datetime.fromtimestamp(time.time())
-    # get datetime for 30 days ago
+    # get datetime for nn days ago
     ago = today - offset
-    print('30 days ago = ', ago)
+    prt(f'The date before {days} days ago: {ago}')
+    # print the epoch from a datetime object
+    #prt(ago.timestamp())
+    prt('NOTE: here list the older folders')
+    prt('=' * 56)
 
-    ctime = 0
-    foldertime = 0
-    for xx in folders:
-        ctime = os.path.getctime(xx)
-        foldertime = datetime.datetime.fromtimestamp(ctime)
-        if foldertime <= ago:
-            if arg == 1:
-                print(f"!!! {xx} is one month ago ({foldertime})")
-            else:
-                print(xx)
-        else:
-            if arg == 1:
-                print(f'{xx} is newer ({foldertime})')
-        #print xx, ":", time.strftime('%c', time.localtime(ctime))
+    # collect data for each folder
+    datas = []
+    for f in folders:
+        entry = {}
+        entry['folder'] = f
+        ctime = os.path.getctime(f)
+        entry['ctime'] = ctime
+        #entry['ftime'] = datetime.datetime.fromtimestamp(ctime)
+        datas.append(entry)
+
+    # grep the folder which is older than 30 days
+    for d in datas:
+        f = d['folder']
+        ctime = d['ctime']
+        if ctime <= ago.timestamp():
+            ftime = datetime.datetime.fromtimestamp(ctime)
+            prt(f"[yellow]{f}[/]: ({ftime})")
+    #     ctime = os.path.getctime(f)
+    #     foldertime = datetime.datetime.fromtimestamp(ctime)
+
+    #     if foldertime <= ago:
+    #         prt(f"[yellow]{f}[/]: ({foldertime})")
+    #     else:
+    #         if verbose:
+    #             prt(f'{f} is newer ({foldertime})')
+
 
 def main():
     '''main function'''
     # get file list within current folder
-    alllist = glob.glob('*')
-    dirlist = []
+    dirs = []
 
-    for x in alllist:
+    for d in glob.glob('*'):
         # only sub-directory kept
-        if os.path.isdir(x):
-            #print x
-            dirlist.append(x)
-    detail_arg = 1
-    if (len(sys.argv) > 1) and (sys.argv[1] == '-l'):
-        detail_arg = 0
-    #print len(dirlist)
-    showlist(dirlist, detail_arg)
+        if not os.path.isdir(d):
+            continue
+        # skip the hidden folder
+        if d[0] == '.' or d[0:2] == '__':
+            continue
+        dirs.append(d)
 
+    dirs = sorted(dirs)
+    showlist(dirs)
 
 if __name__ == '__main__':
     main()
