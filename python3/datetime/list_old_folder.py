433cfe9571b79044baacef558b03cb8f98869faa
ericosur <ericosur@gmail.com>
2025-03-24 11:01:28 +0800

    REFACTOR list_old_folder.py
    - refactor as class ShowDirList
    - remove usage of datedelta
    - use epoch to do basic date calculation
    
    Signed-off-by: ericosur <ericosur@gmail.com>

--- python3/datetime/list_old_folder.py
+++ python3/datetime/list_old_folder.py
@@ -7,8 +7,8 @@
 
 from __future__ import print_function
 
-import datetime
-import glob
+from datetime import datetime
+from glob import glob
 import os
 import sys
 import time
@@ -16,68 +16,78 @@ sys.path.insert(0, '.')
 sys.path.insert(0, '..')
 from myutil import prt
 
-# arg: 0 is list, 1 is detail
-def showlist(folders: list, days: int=90, verbose: bool=False) -> None:
-    '''
-    input: a list with name of folders
-    output: it would output the folders which is older than 30 days
-    '''
-    # timedelta for 30 days
-    offset = datetime.timedelta(days=days)
+class ShowDirList():
+    ''' show the folder list by epoch '''
+    SEP_LEN = 60
+    DEFAULT_DAYS = 30
 
-    # get today
-    today = datetime.datetime.fromtimestamp(time.time())
-    # get datetime for nn days ago
-    ago = today - offset
-    prt(f'The date before {days} days ago: {ago}')
-    # print the epoch from a datetime object
-    #prt(ago.timestamp())
-    prt('NOTE: here list the older folders')
-    prt('=' * 56)
+    def __init__(self):
+        self.dirs = []
+        self.__collect_dirs__()
 
-    # collect data for each folder
-    datas = []
-    for f in folders:
-        entry = {}
-        entry['folder'] = f
-        ctime = os.path.getctime(f)
-        entry['ctime'] = ctime
-        #entry['ftime'] = datetime.datetime.fromtimestamp(ctime)
-        datas.append(entry)
+    def __collect_dirs__(self):
+        ''' collect the folder list and sort it '''
+        for d in glob('*'):
+            if not os.path.isdir(d):
+                continue
+            if d[0] == '.' or d[0:2] == '__':
+                continue
+            self.dirs.append(d)
+        self.dirs = sorted(self.dirs)
 
-    # grep the folder which is older than 30 days
-    for d in datas:
-        f = d['folder']
-        ctime = d['ctime']
-        if ctime <= ago.timestamp():
-            ftime = datetime.datetime.fromtimestamp(ctime)
-            prt(f"[yellow]{f}[/]: ({ftime})")
-    #     ctime = os.path.getctime(f)
-    #     foldertime = datetime.datetime.fromtimestamp(ctime)
+    @staticmethod
+    def get_current_epoch() -> int:
+        ''' get current epoch '''
+        return int(time.time())
 
-    #     if foldertime <= ago:
-    #         prt(f"[yellow]{f}[/]: ({foldertime})")
-    #     else:
-    #         if verbose:
-    #             prt(f'{f} is newer ({foldertime})')
+    @staticmethod
+    def get_older_epoch(days: int) -> int:
+        ''' get epoch for days ago '''
+        return ShowDirList.get_current_epoch() - days * 24 * 3600
 
+    @staticmethod
+    def get_newer_epoch(days: int) -> int:
+        ''' get epoch for days later '''
+        return ShowDirList.get_current_epoch() + days * 24 * 3600
+
+    @staticmethod
+    def get_date_str_from_epoch(epoch: int) -> str:
+        ''' get date string from epoch '''
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(epoch))
+
+    @staticmethod
+    def sep() -> None:
+        ''' print a separator '''
+        prt('-' * ShowDirList.SEP_LEN)
+
+    def show_older_dirs(self, days: int=DEFAULT_DAYS) -> None:
+        ''' show the older folders '''
+        ago = ShowDirList.get_older_epoch(days)
+        prt(f'> The folders are older than {days} days ago: {datetime.fromtimestamp(ago)}')
+        ShowDirList.sep()
+        for d in self.dirs:
+            ctime = os.path.getctime(d)
+            if ctime <= ago:
+                ftime = ShowDirList.get_date_str_from_epoch(ctime)
+                prt(f"[yellow]{d:20s}[/]: ({ftime})")
+
+    def show_newer_dirs(self, days: int=DEFAULT_DAYS) -> None:
+        ''' show the newer folders '''
+        ago = ShowDirList.get_older_epoch(days)
+        prt(f'> The folders are newer, within {days} days: {datetime.fromtimestamp(ago)}')
+        ShowDirList.sep()
+        for d in self.dirs:
+            ctime = os.path.getctime(d)
+            if ctime >= ago:
+                ftime = ShowDirList.get_date_str_from_epoch(ctime)
+                prt(f"[yellow]{d:20s}[/]: ({ftime})")
 
 def main():
-    '''main function'''
-    # get file list within current folder
-    dirs = []
-
-    for d in glob.glob('*'):
-        # only sub-directory kept
-        if not os.path.isdir(d):
-            continue
-        # skip the hidden folder
-        if d[0] == '.' or d[0:2] == '__':
-            continue
-        dirs.append(d)
-
-    dirs = sorted(dirs)
-    showlist(dirs)
+    ''' main '''
+    obj = ShowDirList()
+    DAYS_AGO = 60
+    obj.show_older_dirs(DAYS_AGO)
+    obj.show_newer_dirs()
 
 if __name__ == '__main__':
     main()
