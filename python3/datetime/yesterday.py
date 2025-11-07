9c79171f68d1ec592774fdbebbe142be8f14c5a9
ericosur <ericosur@gmail.com>
2025-11-07 14:35:48 +0800

    UPDATE yesterday.py

--- python3/datetime/yesterday.py
+++ python3/datetime/yesterday.py
@@ -2,30 +2,36 @@
 # -*- coding: utf-8 -*-
 
 '''
-demo datetime:
-yesterday = today - timedelta
+demo datetime, and timedelta to get yesterday's date
+  * get today
+  * define timedelta 1 day
+  * compute yesterday = today - timedelta
 '''
 
 from __future__ import print_function
 
 import time
 from datetime import datetime, timedelta
-
+try:
+    from rich import print as pprint
+except ImportError:
+    pprint = print
+prt = pprint
 
 def main():
     '''main function'''
     # get current time
     today = datetime.fromtimestamp(time.time())
     today = today.replace(microsecond=0)  # remove microsecond part
-    print(f"current time from time stamp: {today}")
+    prt(f"current time from time stamp: {today}")
 
     # define offset 1 day
     offset = timedelta(days=1)
-    print(f"timedelta: {offset}")
+    prt(f"timedelta: {offset}")
 
     # get yesterday
     today -= offset
-    print(f'current - timedelta = "{today}"')
+    prt(f'current - timedelta = "{today}"')
 
 if __name__ == '__main__':
     main()
