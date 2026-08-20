2c88b8dd3a8d4dc1276cf51a05cdb2d5fa5eab5d
ericosur <ericosur@gmail.com>
2026-08-20 11:34:24 +0800

    UPDATE scripts under random/

---
+++ python3/random/fisher_yates_shuffle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+''' fisher-yates shuffle '''
+
+import random
+
+# implement fisher-yates shuffle in python
+
+def shuffle_array(arr, debug=False):
+    '''shuffle array using fisher'''
+    n = len(arr)
+    while n > 1:
+        k = random.randint(0, n-1)
+        n = n - 1
+        arr[n], arr[k] = arr[k], arr[n]
+        if debug:
+            print(f'k:{k} exchange:{arr[n]} and {arr[k]}')
+            show_array(arr)
+
+def get_array(max_size=20):
+    '''fill array with index, [0, 1, 2, ... max-size-1] '''
+    # R1721: Unnecessary use of a comprehension (unnecessary-comprehension)
+    #return [i for i in range(0, max_size)]
+    return list(range(max_size))
+
+def show_array(arr):
+    '''dump array'''
+    print(arr)
+
+def main():
+    '''main function'''
+    ARRAY_SIZE = 17
+    REPEAT = 3
+
+    for _ in range(REPEAT):
+        arr = get_array(ARRAY_SIZE)
+        copy1 = arr.copy()
+        copy2 = arr.copy()
+        #show_array(arr)
+        shuffle_array(copy1)
+        #print "result:",
+        show_array(copy1)
+        random.shuffle(copy2)
+        show_array(copy2)
+        print()
+
+
+if __name__ == '__main__':
+    main()
